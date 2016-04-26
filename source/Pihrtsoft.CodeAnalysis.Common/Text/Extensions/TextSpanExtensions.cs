c745ad337f758be8956d31e90093f3a4d2859005
Josef Pihrt <josef.pihrt@outlook.com>
2016-04-26 23:28:52 +0200

    TextSpanExtensions class added

---
+++ source/Pihrtsoft.CodeAnalysis.Common/Text/Extensions/TextSpanExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Pihrtsoft.CodeAnalysis.Text
+{
+    public static class TextSpanExtensions
+    {
+        public static bool IsMultiline(this TextSpan span, SyntaxTree tree)
+        {
+            if (tree == null)
+                throw new ArgumentNullException(nameof(tree));
+
+            return !IsSingleline(span, tree);
+        }
+
+        public static bool IsSingleline(this TextSpan span, SyntaxTree tree)
+        {
+            if (tree == null)
+                throw new ArgumentNullException(nameof(tree));
+
+            FileLinePositionSpan positionSpan = tree.GetLineSpan(span);
+
+            return positionSpan.StartLinePosition.Line == positionSpan.EndLinePosition.Line;
+        }
+    }
+}
