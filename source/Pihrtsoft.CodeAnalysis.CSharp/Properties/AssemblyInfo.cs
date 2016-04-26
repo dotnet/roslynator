aab225094cafe546eff7f0f8d4f9017fab76b4f5
Josef Pihrt <josef.pihrt@outlook.com>
2016-04-27 00:48:38 +0200

    version changed to 0.9.1

--- source/Pihrtsoft.CodeAnalysis.CSharp/Properties/AssemblyInfo.cs
+++ source/Pihrtsoft.CodeAnalysis.CSharp/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.9.0")]
+[assembly: AssemblyVersion("0.9.1")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
