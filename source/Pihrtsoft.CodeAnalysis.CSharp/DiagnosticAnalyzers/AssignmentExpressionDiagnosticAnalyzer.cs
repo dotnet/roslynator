68d9d063158ce2fa364b85762bef5790626c856c
Josef Pihrt <josef.pihrt@outlook.com>
2016-05-13 05:26:27 +0200

    fixes #2: bug fixed in "SimplifyAssignmentExpression" analyzer

--- source/Pihrtsoft.CodeAnalysis.CSharp/DiagnosticAnalyzers/AssignmentExpressionDiagnosticAnalyzer.cs
+++ source/Pihrtsoft.CodeAnalysis.CSharp/DiagnosticAnalyzers/AssignmentExpressionDiagnosticAnalyzer.cs
@@ -27,41 +27,114 @@ namespace Pihrtsoft.CodeAnalysis.CSharp.DiagnosticAnalyzers
             if (context == null)
                 throw new ArgumentNullException(nameof(context));
 
-            context.RegisterSyntaxNodeAction(f => AnalyzeSyntaxNode(f), SyntaxKind.SimpleAssignmentExpression);
+            context.RegisterSyntaxNodeAction(f => AnalyzeSimpleAssignment(f), SyntaxKind.SimpleAssignmentExpression);
         }
 
-        private void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+        private void AnalyzeSimpleAssignment(SyntaxNodeAnalysisContext context)
         {
             if (GeneratedCodeAnalyzer?.IsGeneratedCode(context) == true)
                 return;
 
             var assignment = (AssignmentExpressionSyntax)context.Node;
 
-            if (assignment.Left != null
-                && assignment.Right != null
+            if (assignment.Left?.IsMissing == false
+                && assignment.Right?.IsMissing == false
                 && IsBinaryExpression(assignment.Right))
             {
                 var binaryExpression = (BinaryExpressionSyntax)assignment.Right;
 
-                if (binaryExpression.Left != null)
+                if (binaryExpression.Left?.IsMissing == false
+                    && binaryExpression.Right?.IsMissing == false
+                    && AreExpressionsEqual(assignment.Left, binaryExpression.Left, context))
                 {
-                    ISymbol symbol = context.SemanticModel.GetSymbolInfo(assignment.Left, context.CancellationToken).Symbol;
+                    context.ReportDiagnostic(
+                        DiagnosticDescriptors.SimplifyAssignmentExpression,
+                        assignment.GetLocation());
 
-                    if (symbol != null)
+                    DiagnosticHelper.FadeOutNode(context, binaryExpression.Left, DiagnosticDescriptors.SimplifyAssignmentExpressionFadeOut);
+                }
+            }
+        }
+
+        private static bool AreExpressionsEqual(
+            ExpressionSyntax expression,
+            ExpressionSyntax expression2,
+            SyntaxNodeAnalysisContext context)
+        {
+            if (expression != null && expression2 != null)
+            {
+                if (expression.IsKind(SyntaxKind.IdentifierName)
+                    && expression2.IsKind(SyntaxKind.IdentifierName))
+                {
+                    ISymbol symbol = context.SemanticModel.GetSymbolInfo(expression, context.CancellationToken).Symbol;
+
+                    if (symbol != null && symbol.Kind != SymbolKind.ErrorType)
                     {
-                        ISymbol symbol2 = context.SemanticModel.GetSymbolInfo(binaryExpression.Left, context.CancellationToken).Symbol;
+                        ISymbol symbol2 = context.SemanticModel.GetSymbolInfo(expression2, context.CancellationToken).Symbol;
 
-                        if (symbol.Equals(symbol2))
+                        return symbol.Equals(symbol2);
+                    }
+                }
+                else if (expression.IsKind(SyntaxKind.ElementAccessExpression)
+                    && expression2.IsKind(SyntaxKind.ElementAccessExpression))
+                {
+                    var elementAccess = (ElementAccessExpressionSyntax)expression;
+                    var elementAccess2 = (ElementAccessExpressionSyntax)expression2;
+
+                    return AreExpressionsEqual(elementAccess.Expression, elementAccess2.Expression, context)
+                        && AreArgumentsEqual(elementAccess, elementAccess2, context);
+                }
+            }
+
+            return false;
+        }
+
+        private static bool AreArgumentsEqual(ElementAccessExpressionSyntax elementAccess, ElementAccessExpressionSyntax elementAccess2, SyntaxNodeAnalysisContext context)
+        {
+            ExpressionSyntax expression = GetExpression(elementAccess);
+
+            if (expression != null)
+            {
+                ExpressionSyntax expression2 = GetExpression(elementAccess2);
+
+                if (expression2 != null)
+                {
+                    if (expression.IsKind(SyntaxKind.IdentifierName))
+                    {
+                        return AreExpressionsEqual(expression, expression2, context);
+                    }
+                    else if (expression2 is LiteralExpressionSyntax)
+                    {
+                        SyntaxToken token = ((LiteralExpressionSyntax)expression).Token;
+                        SyntaxToken token2 = ((LiteralExpressionSyntax)expression2).Token;
+
+                        if (!token.IsMissing
+                            && (token.Kind() == token2.Kind()))
                         {
-                            context.ReportDiagnostic(
-                                DiagnosticDescriptors.SimplifyAssignmentExpression,
-                                context.Node.GetLocation());
-
-                            DiagnosticHelper.FadeOutNode(context, binaryExpression.Left, DiagnosticDescriptors.SimplifyAssignmentExpressionFadeOut);
+                            return token.Value.Equals(token2.Value);
                         }
                     }
                 }
             }
+
+            return false;
+        }
+
+        private static ExpressionSyntax GetExpression(ElementAccessExpressionSyntax elementAccess)
+        {
+            if (elementAccess.ArgumentList?.Arguments.Count == 1)
+            {
+                ArgumentSyntax argument = elementAccess.ArgumentList.Arguments[0];
+
+                if (argument.NameColon == null
+                    && argument.Expression != null
+                    && (argument.Expression.IsKind(SyntaxKind.IdentifierName) || argument.Expression is LiteralExpressionSyntax))
+                {
+                    return argument.Expression;
+                }
+            }
+
+            return null;
         }
 
         private static bool IsBinaryExpression(ExpressionSyntax expression)
