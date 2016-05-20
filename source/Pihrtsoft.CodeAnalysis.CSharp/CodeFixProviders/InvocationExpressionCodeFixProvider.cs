81ff46f6bed282380591a4d46a1f5e978acf37bc
Josef Pihrt <josef.pihrt@outlook.com>
2016-05-20 19:16:24 +0200

    using directive added

--- source/Pihrtsoft.CodeAnalysis.CSharp/CodeFixProviders/InvocationExpressionCodeFixProvider.cs
+++ source/Pihrtsoft.CodeAnalysis.CSharp/CodeFixProviders/InvocationExpressionCodeFixProvider.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Immutable;
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
 using System.Composition;
 using System.Threading;
 using System.Threading.Tasks;
