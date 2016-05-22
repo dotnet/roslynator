d15dd25bb6e5400ff2c2693ef31d9667f2cdeab0
Josef Pihrt <josef.pihrt@outlook.com>
2016-05-23 00:28:06 +0200

    code split into more files

---
+++ source/Pihrtsoft.CodeAnalysis.CSharp/CodeFixProviders/CodeFixes/UseCountOrLengthPropertyInsteadOfAnyMethodCodeFix.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Pihrtsoft.CodeAnalysis.CSharp.CodeFixProviders.CodeFixes
+{
+    internal static class UseCountOrLengthPropertyInsteadOfAnyMethodCodeFix
+    {
+        public static void Register(
+            CodeFixContext context,
+            Diagnostic diagnostic,
+            InvocationExpressionSyntax invocation)
+        {
+            string propertyName = diagnostic.Properties["PropertyName"];
+            string sign = (invocation.Parent?.IsKind(SyntaxKind.LogicalNotExpression) == true) ? "==" : ">";
+
+            CodeAction codeAction = CodeAction.Create(
+                $"Replace 'Any' with '{propertyName} {sign} 0'",
+                cancellationToken =>
+                {
+                    return RefactorAsync(
+                        context.Document,
+                        invocation,
+                        propertyName,
+                        cancellationToken);
+                },
+                diagnostic.Id + BaseCodeFixProvider.EquivalenceKeySuffix);
+
+            context.RegisterCodeFix(codeAction, diagnostic);
+        }
+
+        private static async Task<Document> RefactorAsync(
+            Document document,
+            InvocationExpressionSyntax invocation,
+            string propertyName,
+            CancellationToken cancellationToken)
+        {
+            SyntaxNode oldRoot = await document.GetSyntaxRootAsync(cancellationToken);
+
+            var memberAccess = (MemberAccessExpressionSyntax)invocation.Expression;
+
+            memberAccess = memberAccess
+                .WithName(IdentifierName(propertyName).WithTriviaFrom(memberAccess.Name));
+
+            SyntaxNode newRoot = null;
+
+            if (invocation.Parent?.IsKind(SyntaxKind.LogicalNotExpression) == true)
+            {
+                BinaryExpressionSyntax binaryExpression = BinaryExpression(
+                    SyntaxKind.EqualsExpression,
+                    memberAccess,
+                    LiteralExpression(
+                        SyntaxKind.NumericLiteralExpression,
+                        Literal(0)));
+
+                newRoot = oldRoot.ReplaceNode(
+                    invocation.Parent,
+                    binaryExpression.WithTriviaFrom(invocation.Parent));
+            }
+            else
+            {
+                BinaryExpressionSyntax binaryExpression = BinaryExpression(
+                    SyntaxKind.GreaterThanExpression,
+                    memberAccess,
+                    LiteralExpression(
+                        SyntaxKind.NumericLiteralExpression,
+                        Literal(0)));
+
+                newRoot = oldRoot.ReplaceNode(
+                    invocation,
+                    binaryExpression.WithTriviaFrom(invocation));
+            }
+
+            return document.WithSyntaxRoot(newRoot);
+        }
+    }
+}
--- source/Pihrtsoft.CodeAnalysis.CSharp/CodeFixProviders/InvocationExpressionCodeFixProvider.cs
+++ source/Pihrtsoft.CodeAnalysis.CSharp/CodeFixProviders/InvocationExpressionCodeFixProvider.cs
@@ -2,14 +2,11 @@
 
 using System.Collections.Immutable;
 using System.Composition;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+using Pihrtsoft.CodeAnalysis.CSharp.CodeFixProviders.CodeFixes;
 
 namespace Pihrtsoft.CodeAnalysis.CSharp.CodeFixProviders
 {
@@ -43,111 +40,16 @@ namespace Pihrtsoft.CodeAnalysis.CSharp.CodeFixProviders
                 {
                     case DiagnosticIdentifiers.SimplifyLinqMethodChain:
                         {
-                            CodeAction codeAction = CodeAction.Create(
-                                "Simplify method chain",
-                                cancellationToken =>
-                                {
-                                    return SimplifyMethodChainAsync(
-                                        context.Document,
-                                        invocation,
-                                        cancellationToken);
-                                },
-                                diagnostic.Id + EquivalenceKeySuffix);
-
-                            context.RegisterCodeFix(codeAction, diagnostic);
+                            SimplifyLinqMethodChainCodeFix.Register(context, diagnostic, invocation);
                             break;
                         }
                     case DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod:
                         {
-                            string propertyName = diagnostic.Properties["PropertyName"];
-                            string sign = (invocation.Parent?.IsKind(SyntaxKind.LogicalNotExpression) == true) ? "==" : ">";
-
-                            CodeAction codeAction = CodeAction.Create(
-                                $"Replace 'Any' with '{propertyName} {sign} 0'",
-                                cancellationToken =>
-                                {
-                                    return ReplaceAnyMethodWithCountOrLengthPropertyAsync(
-                                        context.Document,
-                                        invocation,
-                                        propertyName,
-                                        cancellationToken);
-                                },
-                                diagnostic.Id + EquivalenceKeySuffix);
-
-                            context.RegisterCodeFix(codeAction, diagnostic);
+                            UseCountOrLengthPropertyInsteadOfAnyMethodCodeFix.Register(context, diagnostic, invocation);
                             break;
                         }
                 }
             }
-        }
-
-        private static async Task<Document> SimplifyMethodChainAsync(
-            Document document,
-            InvocationExpressionSyntax invocation,
-            CancellationToken cancellationToken)
-        {
-            SyntaxNode oldRoot = await document.GetSyntaxRootAsync(cancellationToken);
-
-            var memberAccess = (MemberAccessExpressionSyntax)invocation.Expression;
-
-            var invocation2 = (InvocationExpressionSyntax)invocation.Parent.Parent;
-
-            var memberAccess2 = (MemberAccessExpressionSyntax)invocation2.Expression;
-
-            memberAccess = memberAccess
-                .WithName(memberAccess2.Name.WithTriviaFrom(memberAccess.Name));
-
-            invocation = invocation.WithExpression(memberAccess)
-                .WithTrailingTrivia(invocation2.GetTrailingTrivia());
-
-            SyntaxNode newRoot = oldRoot.ReplaceNode(invocation2, invocation);
-
-            return document.WithSyntaxRoot(newRoot);
-        }
-
-        private static async Task<Document> ReplaceAnyMethodWithCountOrLengthPropertyAsync(
-            Document document,
-            InvocationExpressionSyntax invocation,
-            string propertyName,
-            CancellationToken cancellationToken)
-        {
-            SyntaxNode oldRoot = await document.GetSyntaxRootAsync(cancellationToken);
-
-            var memberAccess = (MemberAccessExpressionSyntax)invocation.Expression;
-
-            memberAccess = memberAccess
-                .WithName(IdentifierName(propertyName).WithTriviaFrom(memberAccess.Name));
-
-            SyntaxNode newRoot = null;
-
-            if (invocation.Parent?.IsKind(SyntaxKind.LogicalNotExpression) == true)
-            {
-                BinaryExpressionSyntax binaryExpression = BinaryExpression(
-                    SyntaxKind.EqualsExpression,
-                    memberAccess,
-                    LiteralExpression(
-                        SyntaxKind.NumericLiteralExpression,
-                        Literal(0)));
-
-                newRoot = oldRoot.ReplaceNode(
-                    invocation.Parent,
-                    binaryExpression.WithTriviaFrom(invocation.Parent));
-            }
-            else
-            {
-                BinaryExpressionSyntax binaryExpression = BinaryExpression(
-                    SyntaxKind.GreaterThanExpression,
-                    memberAccess,
-                    LiteralExpression(
-                        SyntaxKind.NumericLiteralExpression,
-                        Literal(0)));
-
-                newRoot = oldRoot.ReplaceNode(
-                    invocation,
-                    binaryExpression.WithTriviaFrom(invocation));
-            }
-
-            return document.WithSyntaxRoot(newRoot);
         }
     }
 }
