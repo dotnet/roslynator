248ba6209034bbed0ce25c71411935c602369234
Josef Pihrt <josef.pihrt@outlook.com>
2016-05-23 04:02:09 +0200

    "Rename parameter according to type name" refactoring improved

--- source/Pihrtsoft.CodeAnalysis.CSharp.Refactorings/Refactoring/RenameParameterAccordingToTypeNameRefactoring.cs
+++ source/Pihrtsoft.CodeAnalysis.CSharp.Refactorings/Refactoring/RenameParameterAccordingToTypeNameRefactoring.cs
@@ -1,9 +1,14 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Text;
 
 namespace Pihrtsoft.CodeAnalysis.CSharp.Refactoring
 {
@@ -23,26 +28,65 @@ namespace Pihrtsoft.CodeAnalysis.CSharp.Refactoring
             if (parameter.Type == null)
                 return;
 
-            if (!parameter.Identifier.Span.Contains(context.Span))
-                return;
+            if (parameter.Identifier.IsMissing)
+            {
+                if (TextSpan.FromBounds(parameter.Type.Span.End, parameter.Span.End).Contains(context.Span))
+                {
+                    string name = CreateParameterName(parameter, semanticModel);
+                    if (name != null)
+                    {
+                        context.RegisterRefactoring(
+                            $"Add parameter name '{name}'",
+                            cancellationToken => AddParameterNameAccordingToTypeNameRefactorAsync(context.Document, parameter, name, cancellationToken));
+                    }
+                }
+            }
+            else
+            {
+                if (parameter.Identifier.Span.Contains(context.Span))
+                {
+                    string name = CreateParameterName(parameter, semanticModel);
+                    if (name != null)
+                    {
+                        ISymbol symbol = semanticModel.GetDeclaredSymbol(parameter, context.CancellationToken);
 
-            string newName = NamingHelper.CreateIdentifierName(
+                        context.RegisterRefactoring(
+                            $"Rename parameter to '{name}'",
+                            cancellationToken => symbol.RenameAsync(name, context.Document, cancellationToken));
+                    }
+                }
+            }
+        }
+
+        private static string CreateParameterName(ParameterSyntax parameter, SemanticModel semanticModel)
+        {
+            string name = NamingHelper.CreateIdentifierName(
                 parameter.Type,
                 semanticModel,
                 firstCharToLower: true);
 
-            if (string.Equals(parameter.Identifier.ToString(), newName, StringComparison.Ordinal))
-                return;
+            if (string.Equals(parameter.Identifier.ToString(), name, StringComparison.Ordinal))
+                return null;
 
-            ISymbol symbol = semanticModel.GetDeclaredSymbol(parameter, context.CancellationToken);
+            return name;
+        }
 
-            string title = (parameter.Identifier.IsMissing)
-                ? $"Add parameter name '{newName}'"
-                : $"Rename parameter to '{newName}'";
+        private static async Task<Document> AddParameterNameAccordingToTypeNameRefactorAsync(
+            Document document,
+            ParameterSyntax parameter,
+            string name,
+            CancellationToken cancellationToken)
+        {
+            SyntaxNode oldRoot = await document.GetSyntaxRootAsync(cancellationToken);
 
-            context.RegisterRefactoring(
-                title,
-                cancellationToken => symbol.RenameAsync(newName, context.Document, cancellationToken));
+            ParameterSyntax newParameter = parameter
+                .WithType(parameter.Type.WithoutTrailingTrivia())
+                .WithIdentifier(SyntaxFactory.Identifier(name).WithTrailingTrivia(parameter.Type.GetTrailingTrivia()))
+                .WithAdditionalAnnotations(Formatter.Annotation);
+
+            SyntaxNode newRoot = oldRoot.ReplaceNode(parameter, newParameter);
+
+            return document.WithSyntaxRoot(newRoot);
         }
     }
 }
