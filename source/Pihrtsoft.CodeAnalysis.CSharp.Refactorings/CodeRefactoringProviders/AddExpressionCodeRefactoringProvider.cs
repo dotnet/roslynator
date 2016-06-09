f6d0f96de579bef5abada30fcfe561bb187344f1
Josef Pihrt <josef.pihrt@outlook.com>
2016-06-09 18:05:00 +0200

    "Merge string literals (into multiline string literal)" refactorings added

---
+++ source/Pihrtsoft.CodeAnalysis.CSharp.Refactorings/CodeRefactoringProviders/AddExpressionCodeRefactoringProvider.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace Pihrtsoft.CodeAnalysis.CSharp.CodeRefactoringProviders
+{
+    [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = nameof(AddExpressionCodeRefactoringProvider))]
+    public class AddExpressionCodeRefactoringProvider : CodeRefactoringProvider
+    {
+        public override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+        {
+            SyntaxNode root = await context.Document.GetSyntaxRootAsync(context.CancellationToken);
+
+            BinaryExpressionSyntax node = root
+                .FindNode(context.Span, getInnermostNodeForTie: true)?
+                .FirstAncestorOrSelf<BinaryExpressionSyntax>();
+
+            if (node == null)
+                return;
+
+            if (node.IsKind(SyntaxKind.AddExpression)
+                && node.Span.Equals(context.Span)
+                && StringLiteralChain.IsStringLiteralChain(node))
+            {
+                context.RegisterRefactoring(
+                    "Merge string literals",
+                    cancellationToken => MergeStringLiteralsAsync(context.Document, node, cancellationToken));
+
+                if (node
+                    .DescendantTrivia(node.Span)
+                    .Any(f => f.IsKind(SyntaxKind.EndOfLineTrivia)))
+                {
+                    context.RegisterRefactoring(
+                        "Merge string literals into multiline string literal",
+                        cancellationToken => MergeStringLiteralsAsync(context.Document, node, cancellationToken, multiline: true));
+                }
+            }
+        }
+
+        private static async Task<Document> MergeStringLiteralsAsync(
+            Document document,
+            BinaryExpressionSyntax binaryExpression,
+            CancellationToken cancellationToken = default(CancellationToken),
+            bool multiline = false)
+        {
+            SyntaxNode root = await document.GetSyntaxRootAsync(cancellationToken);
+
+            var chain = new StringLiteralChain(binaryExpression);
+
+            LiteralExpressionSyntax newNode = (multiline)
+                ? chain.MergeMultiline()
+                : chain.Merge();
+
+            root = root.ReplaceNode(
+                binaryExpression,
+                newNode.WithAdditionalAnnotations(Formatter.Annotation));
+
+            return document.WithSyntaxRoot(root);
+        }
+    }
+}
---
+++ source/Pihrtsoft.CodeAnalysis.CSharp.Refactorings/StringLiteralChain.cs
@@ -0,0 +1,162 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Pihrtsoft.CodeAnalysis.CSharp
+{
+    internal class StringLiteralChain
+    {
+        private const string Quote = "\"";
+        private const string AmpersandQuote = "@" + Quote;
+
+        public StringLiteralChain(BinaryExpressionSyntax addExpression)
+        {
+            AddExpression = addExpression;
+
+            List<LiteralExpressionSyntax> literals = GetLiterals(addExpression);
+
+            for (int i = 0; i < literals.Count; i++)
+            {
+                if (literals[i].IsVerbatimStringLiteral())
+                    IsAnyVerbatim = true;
+                else
+                    IsAllVerbatim = false;
+            }
+
+            if (IsAllVerbatim)
+                IsAnyVerbatim = false;
+
+            Literals = ImmutableArray.CreateRange(literals);
+        }
+
+        private static List<LiteralExpressionSyntax> GetLiterals(BinaryExpressionSyntax addExpression)
+        {
+            var literals = new List<LiteralExpressionSyntax>();
+
+            var addExpressions = new Stack<BinaryExpressionSyntax>();
+            addExpressions.Push(addExpression);
+
+            while (addExpressions.Count > 0)
+            {
+                addExpression = addExpressions.Pop();
+
+                literals.Add((LiteralExpressionSyntax)addExpression.Right);
+
+                if (addExpression.Left.IsKind(SyntaxKind.StringLiteralExpression))
+                    literals.Add((LiteralExpressionSyntax)addExpression.Left);
+                else
+                    addExpressions.Push((BinaryExpressionSyntax)addExpression.Left);
+            }
+
+            return literals;
+        }
+
+        public ImmutableArray<LiteralExpressionSyntax> Literals { get; }
+
+        public BinaryExpressionSyntax AddExpression { get; }
+
+        public bool IsAllVerbatim { get; } = true;
+
+        public bool IsAnyVerbatim { get; }
+
+        public static bool IsStringLiteralChain(BinaryExpressionSyntax binaryExpression)
+        {
+            if (binaryExpression.IsKind(SyntaxKind.AddExpression)
+                && binaryExpression.Right?.IsKind(SyntaxKind.StringLiteralExpression) == true)
+            {
+                switch (binaryExpression.Left?.Kind())
+                {
+                    case SyntaxKind.StringLiteralExpression:
+                        return true;
+                    case SyntaxKind.AddExpression:
+                        return IsStringLiteralChain((BinaryExpressionSyntax)binaryExpression.Left);
+                }
+            }
+
+            return false;
+        }
+
+        public LiteralExpressionSyntax Merge()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = Literals.Length - 1; i >= 0; i--)
+            {
+                if (IsAnyVerbatim && Literals[i].IsVerbatimStringLiteral())
+                {
+                    sb.Append(EscapeQuotes(Literals[i].Token.ValueText));
+                }
+                else
+                {
+                    sb.Append(GetInnerText(Literals[i].Token.Text));
+                }
+            }
+
+            return (LiteralExpressionSyntax)SyntaxFactory.ParseExpression(
+                ((IsAllVerbatim) ? AmpersandQuote : Quote) +
+                sb.ToString() +
+                Quote);
+        }
+
+        public LiteralExpressionSyntax MergeMultiline()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = Literals.Length - 1; i >= 0; i--)
+            {
+                string s = DoubleQuotes(Literals[i].Token.ValueText);
+
+                int charCount = 0;
+
+                if (s.Length > 0
+                    && s[s.Length - 1] == '\n')
+                {
+                    if (s.Length > 1
+                        && s[s.Length - 2] == '\r')
+                    {
+                        charCount = 2;
+                    }
+                    else
+                    {
+                        charCount = 1;
+                    }
+                }
+
+                sb.Append(s, 0, s.Length - charCount);
+
+                if (charCount > 0
+                    || (i > 0
+                        && Literals[i].GetSpanEndLine() != Literals[i - 1].GetSpanStartLine()))
+                {
+                    sb.Append("\r\n");
+                }
+            }
+
+            return (LiteralExpressionSyntax)SyntaxFactory.ParseExpression(AmpersandQuote + sb.ToString() + Quote);
+        }
+
+        private static string EscapeQuotes(string value)
+        {
+            return value.Replace(Quote, @"\" + Quote);
+        }
+
+        private static string DoubleQuotes(string value)
+        {
+            return value.Replace(Quote, Quote + Quote);
+        }
+
+        private static string GetInnerText(string s)
+        {
+            if (s[0] == '@')
+                return s.Substring(2, s.Length - 3);
+
+            return s.Substring(1, s.Length - 2);
+        }
+    }
+}
