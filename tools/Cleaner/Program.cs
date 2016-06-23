891503adcc9f84cd00638e394b2ac0ec78dd6d84
Josef Pihrt <josef.pihrt@outlook.com>
2016-06-23 14:25:02 +0200

    add project "Cleaner"

---
+++ tools/Cleaner/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Cleaner
+{
+    internal class Program
+    {
+#if DEBUG
+        private static readonly string _currentDirectory = @"D:\Documents\Visual Studio 2015\Projects\Pihrtsoft.CodeAnalysis";
+#else
+        private static readonly string _currentDirectory = Path.GetDirectoryName(Environment.CurrentDirectory);
+#endif
+
+        private static void Main(string[] args)
+        {
+            CleanProjects(new string[]
+            {
+                "source",
+                "vsix"
+            });
+
+            Console.WriteLine("*** FINISHED ***");
+            Console.ReadKey();
+        }
+
+        private static string GetDirectoryPath(string directoryName)
+        {
+            return Path.Combine(_currentDirectory, directoryName);
+        }
+
+        private static void CleanProjects(IEnumerable<string> projectDirectories)
+        {
+            foreach (string projectDirectory in projectDirectories)
+            {
+                foreach (string directory in Directory.EnumerateDirectories(
+                    Path.Combine(_currentDirectory, projectDirectory),
+                    "*",
+                    SearchOption.TopDirectoryOnly))
+                {
+                    CleanProject(directory);
+                }
+            }
+        }
+
+        private static void CleanProject(string projectDirectory)
+        {
+            if (!Directory.EnumerateFiles(projectDirectory, "*.csproj", SearchOption.TopDirectoryOnly).Any())
+                return;
+
+            CleanDirectory(Path.Combine(projectDirectory, "bin"));
+            CleanDirectory(Path.Combine(projectDirectory, "obj"));
+        }
+
+        private static void CleanDirectory(string directory)
+        {
+            if (!Directory.Exists(directory))
+                return;
+
+            foreach (string item in Directory.EnumerateDirectories(directory, "*", SearchOption.TopDirectoryOnly))
+                DeleteDirectory(item);
+        }
+
+        private static void DeleteDirectory(string path)
+        {
+            Console.WriteLine($"{GetRelativePath(path)}");
+
+            try
+            {
+                Directory.Delete(path, recursive: true);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.GetBaseException().Message);
+            }
+        }
+
+        private static string GetRelativePath(string path)
+        {
+            return path.Replace(_currentDirectory, "");
+        }
+    }
+}
