a0c0138ffeacabf5abc976b39e8bc0bc4feeaf3d
Josef Pihrt <josef.pihrt@outlook.com>
2016-07-01 20:57:05 +0200

    add project "Pihrtsoft.CodeAnalysis.Metadata"

---
+++ tools/Pihrtsoft.CodeAnalysis.Metadata/AnalyzerInfo.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Pihrtsoft.CodeAnalysis.Metadata
+{
+    public class AnalyzerInfo
+    {
+        private AnalyzerInfo(
+            string identifier,
+            string title,
+            string id,
+            string category,
+            string severity,
+            string extensionVersion,
+            string nuGetVersion,
+            bool isEnabledByDefault,
+            bool supportsFadeOut,
+            bool supportsFadeOutAnalyzer)
+        {
+            Identifier = identifier;
+            Title = title;
+            Id = id;
+            Category = category;
+            Severity = severity;
+            ExtensionVersion = extensionVersion;
+            NuGetVersion = nuGetVersion;
+            IsEnabledByDefault = isEnabledByDefault;
+            SupportsFadeOut = supportsFadeOut;
+            SupportsFadeOutAnalyzer = supportsFadeOutAnalyzer;
+        }
+
+        public static IEnumerable<AnalyzerInfo> LoadFromFile(string filePath)
+        {
+            XDocument doc = XDocument.Load(filePath);
+
+            foreach (XElement element in doc.Root.Elements())
+            {
+                yield return new AnalyzerInfo(
+                    element.Attribute("Identifier").Value,
+                    element.Element("Title").Value,
+                    element.Element("Id").Value,
+                    element.Element("Category").Value,
+                    element.Element("Severity").Value,
+                    element.Attribute("ExtensionVersion").Value,
+                    element.Attribute("NuGetVersion").Value,
+                    bool.Parse(element.Element("IsEnabledByDefault").Value),
+                    bool.Parse(element.Element("SupportsFadeOut").Value),
+                    bool.Parse(element.Element("SupportsFadeOutAnalyzer").Value));
+            }
+        }
+
+        public string Identifier { get; }
+
+        public string Title { get; }
+
+        public string ExtensionVersion { get; }
+
+        public string NuGetVersion { get; }
+
+        public string Id { get; }
+
+        public string Category { get; }
+
+        public string Severity { get; }
+
+        public bool IsEnabledByDefault { get; }
+
+        public bool SupportsFadeOut { get; }
+
+        public bool SupportsFadeOutAnalyzer { get; }
+    }
+}
---
+++ tools/Pihrtsoft.CodeAnalysis.Metadata/RefactoringInfo.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace Pihrtsoft.CodeAnalysis.Metadata
+{
+    public class RefactoringInfo
+    {
+        private RefactoringInfo(
+            string identifier,
+            string title,
+            bool isEnabledByDefault,
+            string extensionVersion,
+            string scope,
+            IList<SyntaxInfo> syntaxes,
+            IList<ImageInfo> images)
+        {
+            Identifier = identifier;
+            Title = title;
+            IsEnabledByDefault = isEnabledByDefault;
+            ExtensionVersion = extensionVersion;
+            Scope = scope;
+            Syntaxes = new ReadOnlyCollection<SyntaxInfo>(syntaxes);
+            Images = new ReadOnlyCollection<ImageInfo>(images);
+        }
+
+        public static IEnumerable<RefactoringInfo> LoadFromFile(string filePath)
+        {
+            XDocument doc = XDocument.Load(filePath);
+
+            foreach (XElement element in doc.Root.Elements())
+            {
+                yield return new RefactoringInfo(
+                    element.Attribute("Id").Value,
+                    element.Attribute("Title").Value,
+                    (element.Attribute("IsEnabledByDefault") != null)
+                        ? bool.Parse(element.Attribute("IsEnabledByDefault").Value)
+                        : true,
+                    element.Attribute("ExtensionVersion").Value,
+                    (element.Element("Scope") != null) ? element.Element("Scope").Value : null,
+                    element.Element("Syntaxes")
+                        .Elements("Syntax")
+                        .Select(f => new SyntaxInfo(f.Value))
+                        .ToList(),
+                    (element.Element("Images") != null)
+                        ? element.Element("Images")?
+                            .Elements("Image")
+                            .Select(f => new ImageInfo(f.Value))
+                            .ToList()
+                        : new List<ImageInfo>());
+            }
+        }
+
+        public string Identifier { get; }
+
+        public string Title { get; }
+
+        public string Scope { get; }
+
+        public bool IsEnabledByDefault { get; }
+
+        public string ExtensionVersion { get; }
+
+        public ReadOnlyCollection<SyntaxInfo> Syntaxes { get; }
+
+        public ReadOnlyCollection<ImageInfo> Images { get; }
+
+        public string GetGitHubHref()
+        {
+            string s = Title.TrimEnd('.').ToLowerInvariant();
+
+            s = Regex.Replace(s, @"[^a-zA-Z0-9\ \-]", "");
+            s = Regex.Replace(s, @"\ ", "-");
+
+            return s;
+        }
+    }
+}
