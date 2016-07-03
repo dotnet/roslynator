0296fc5483674b45aa83f0ccd1c3b14068b2b851
Josef Pihrt <josef.pihrt@outlook.com>
2016-07-03 21:43:35 +0200

    fix #4: enable/disable refactorings in visual studio UI (options page)

---
+++ vsix/Pihrtsoft.CodeAnalysis.CSharp.Vsix/GeneralOptionsPage.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.ComponentModel;
+using Microsoft.VisualStudio.Shell;
+using Pihrtsoft.CodeAnalysis.CSharp.Refactoring;
+using Pihrtsoft.CodeAnalysis.VisualStudio.TypeConverters;
+
+namespace Pihrtsoft.CodeAnalysis.VisualStudio
+{
+    public partial class GeneralOptionsPage : DialogPage
+    {
+        [Category("General")]
+        [DisplayName("Prefix field identifier with underscore")]
+        [Description("")]
+        [TypeConverter(typeof(YesNoConverter))]
+        public bool PrefixFieldIdentifierWithUnderscore { get; set; }
+
+        public void Apply()
+        {
+            DefaultCodeRefactoringProvider.DefaultSettings.PrefixFieldIdentifierWithUnderscore = PrefixFieldIdentifierWithUnderscore;
+        }
+
+        protected override void OnApply(PageApplyEventArgs e)
+        {
+            if (e.ApplyBehavior == ApplyKind.Apply)
+                Apply();
+
+            base.OnApply(e);
+        }
+    }
+}
