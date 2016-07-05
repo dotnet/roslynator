7e652da402d83058260f94498a797f41bd001514
Josef Pihrt <josef.pihrt@outlook.com>
2016-07-05 21:26:45 +0200

    improve refactoring "AddCastExpression"

--- source/Pihrtsoft.CodeAnalysis.CSharp.Refactorings/Refactoring/ArgumentRefactoring.cs
+++ source/Pihrtsoft.CodeAnalysis.CSharp.Refactorings/Refactoring/ArgumentRefactoring.cs
@@ -1,5 +1,9 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -20,16 +24,86 @@ namespace Pihrtsoft.CodeAnalysis.CSharp.Refactoring
 
                 if (typeSymbol?.IsKind(SymbolKind.ErrorType) == false)
                 {
-                    foreach (IParameterSymbol parameterSymbol in argument.DetermineParameters(semanticModel, context.CancellationToken))
+                    foreach (ITypeSymbol parameterTypeSymbol in DetermineParameters(argument, semanticModel, context.CancellationToken).Distinct())
                     {
-                        if (parameterSymbol.Type != null
-                            && !typeSymbol.Equals(parameterSymbol.Type))
+                        if (parameterTypeSymbol?.IsKind(SymbolKind.ErrorType) == false
+                            && !typeSymbol.Equals(parameterTypeSymbol))
                         {
-                            AddCastExpressionRefactoring.RegisterRefactoring(context, argument.Expression, parameterSymbol.Type);
+                            AddCastExpressionRefactoring.RegisterRefactoring(context, argument.Expression, parameterTypeSymbol);
                         }
                     }
                 }
             }
+        }
+
+        private static IEnumerable<ITypeSymbol> DetermineParameters(
+            ArgumentSyntax argument,
+            SemanticModel semanticModel,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var argumentList = argument.Parent as BaseArgumentListSyntax;
+
+            if (argumentList != null)
+            {
+                var invocableExpression = argumentList.Parent as ExpressionSyntax;
+
+                if (invocableExpression != null)
+                {
+                    SymbolInfo symbolInfo = semanticModel.GetSymbolInfo(invocableExpression, cancellationToken);
+
+                    ISymbol symbol = symbolInfo.Symbol;
+
+                    if (symbol == null)
+                    {
+                        foreach (ISymbol candidateSymbol in symbolInfo.CandidateSymbols)
+                            yield return DetermineParameter(candidateSymbol, argument, argumentList);
+                    }
+                    else
+                    {
+                        yield return DetermineParameter(symbol, argument, argumentList);
+                    }
+                }
+            }
+        }
+
+        private static ITypeSymbol DetermineParameter(
+            ISymbol symbol,
+            ArgumentSyntax argument,
+            BaseArgumentListSyntax argumentList)
+        {
+            ImmutableArray<IParameterSymbol> parameters = symbol.GetParameters();
+
+            if (argument.NameColon != null
+                && !argument.NameColon.IsMissing)
+            {
+                string name = argument.NameColon.Name.Identifier.ValueText;
+
+                foreach (IParameterSymbol parameterSymbol in parameters)
+                {
+                    if (parameterSymbol.Name == name)
+                        return parameterSymbol.Type;
+                }
+
+                return null;
+            }
+
+            int index = argumentList.Arguments.IndexOf(argument);
+
+            if (index >= 0
+                && index < parameters.Length)
+            {
+                IParameterSymbol parameterSymbol = parameters[index];
+
+                if (parameterSymbol.IsParams)
+                {
+                    var arrayTypeSymbol = (IArrayTypeSymbol)parameterSymbol.Type;
+                    return arrayTypeSymbol.ElementType;
+                }
+
+                return parameterSymbol.Type;
+            }
+
+            return null;
         }
     }
 }
--- source/Pihrtsoft.CodeAnalysis.Common/CSharp/Extensions/ArgumentSyntaxExtensions.cs
+++ source/Pihrtsoft.CodeAnalysis.Common/CSharp/Extensions/ArgumentSyntaxExtensions.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
@@ -76,68 +75,6 @@ namespace Pihrtsoft.CodeAnalysis.CSharp
 
                 if (lastParameter.IsParams)
                     return lastParameter;
-            }
-
-            return null;
-        }
-
-        public static IEnumerable<IParameterSymbol> DetermineParameters(
-            this ArgumentSyntax argument,
-            SemanticModel semanticModel,
-            CancellationToken cancellationToken = default(CancellationToken))
-        {
-            if (argument == null)
-                throw new ArgumentNullException(nameof(argument));
-
-            if (semanticModel == null)
-                throw new ArgumentNullException(nameof(semanticModel));
-
-            var argumentList = argument.Parent as BaseArgumentListSyntax;
-
-            if (argumentList == null)
-                yield break;
-
-            var invocableExpression = argumentList.Parent as ExpressionSyntax;
-
-            if (invocableExpression == null)
-                yield break;
-
-            SymbolInfo symbolInfo = semanticModel.GetSymbolInfo(invocableExpression, cancellationToken);
-
-            ISymbol symbol = symbolInfo.Symbol;
-
-            if (symbol == null)
-            {
-                foreach (ISymbol candidateSymbol in symbolInfo.CandidateSymbols)
-                    yield return DetermineParameter(candidateSymbol, argument, argumentList);
-            }
-            else
-            {
-                yield return DetermineParameter(symbol, argument, argumentList);
-            }
-        }
-
-        private static IParameterSymbol DetermineParameter(
-            ISymbol symbol,
-            ArgumentSyntax argument,
-            BaseArgumentListSyntax argumentList)
-        {
-            ImmutableArray<IParameterSymbol> parameters = symbol.GetParameters();
-
-            if (argument.NameColon != null
-                && !argument.NameColon.IsMissing)
-            {
-                string name = argument.NameColon.Name.Identifier.ValueText;
-
-                return parameters.FirstOrDefault(p => p.Name == name);
-            }
-
-            int index = argumentList.Arguments.IndexOf(argument);
-
-            if (index >= 0
-                && index < parameters.Length)
-            {
-                return parameters[index];
             }
 
             return null;
