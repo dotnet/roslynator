69b0716d3e603fe134c56ea3be33723f337bb4a9
Josef Pihrt <josef.pihrt@outlook.com>
2016-07-11 22:25:13 +0200

    change version to 0.91.0

--- source/Pihrtsoft.CodeAnalysis.VisualStudio/Properties/AssemblyInfo.cs
+++ source/Pihrtsoft.CodeAnalysis.VisualStudio/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.9.90")]
+[assembly: AssemblyVersion("0.91.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
