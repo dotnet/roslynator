d57a1d9e0187bfdae40f56a7f8e4856563354679
Josef Pihrt <josef.pihrt@outlook.com>
2016-07-15 03:52:39 +0200

    refactor refactoring "Uncomment"

--- source/Pihrtsoft.CodeAnalysis.CSharp.Refactorings/Refactoring/UncommentRefactoring.cs
+++ source/Pihrtsoft.CodeAnalysis.CSharp.Refactorings/Refactoring/UncommentRefactoring.cs
@@ -1,25 +1,16 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Text.RegularExpressions;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
 
 namespace Pihrtsoft.CodeAnalysis.CSharp.Refactoring
 {
     internal static class UncommentRefactoring
     {
-        private static readonly Regex _uncommentManyCommentsRegex = new Regex(@"
-            (?<=
-                ^
-                [\s-[\r\n]]*
-            )
-            //",
-            RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
-
-        private static readonly Regex _uncommentSingleCommentRegex = new Regex("//");
-
         public static async Task<Document> RefactorAsync(
             Document document,
             SyntaxTrivia comment,
@@ -43,54 +34,26 @@ namespace Pihrtsoft.CodeAnalysis.CSharp.Refactoring
                 triviaList = token.TrailingTrivia;
             }
 
-            string text = GetNewText(oldRoot.ToFullString(), triviaList, index);
+            SourceText sourceText = await document.GetTextAsync(cancellationToken);
 
-            SyntaxTree tree = CSharpSyntaxTree.ParseText(text);
+            SourceText newSourceText = sourceText.WithChanges(GetTextChanges(triviaList, index));
 
-            SyntaxNode newRoot = await tree.GetRootAsync();
-
-            return document.WithSyntaxRoot(newRoot);
+            return document.WithText(newSourceText);
         }
 
-        private static string GetNewText(string input, SyntaxTriviaList triviaList, int index)
+        private static IEnumerable<TextChange> GetTextChanges(SyntaxTriviaList triviaList, int index)
         {
-            int firstIndex = FindFirstTriviaToRemove(triviaList, index);
-            int lastIndex = FindLastTriviaToRemove(triviaList, index);
-            int count = GetSingleLineCommentCount(triviaList, firstIndex, lastIndex);
-
-            Regex regex = (count == 1) ? _uncommentSingleCommentRegex : _uncommentManyCommentsRegex;
-
-            return regex.Replace(
-                input,
-                string.Empty,
-                count,
-                triviaList[firstIndex].SpanStart);
-        }
-
-        private static int GetSingleLineCommentCount(SyntaxTriviaList triviaList, int firstIndex, int lastIndex)
-        {
-            int count = 0;
-            for (int i = firstIndex; i <= lastIndex; i++)
+            int i = index;
+            while (i >= 0)
             {
-                if (triviaList[i].IsKind(SyntaxKind.SingleLineCommentTrivia))
-                    count++;
-            }
+                SyntaxTrivia trivia = triviaList[i];
 
-            return count;
-        }
-
-        private static int FindFirstTriviaToRemove(SyntaxTriviaList triviaList, int index)
-        {
-            int firstIndex = index;
-
-            while (index > 0)
-            {
-                if (IsAllowedTrivia(triviaList[index - 1]))
+                if (IsAllowedTrivia(trivia))
                 {
-                    index--;
+                    if (trivia.IsKind(SyntaxKind.SingleLineCommentTrivia))
+                        yield return new TextChange(trivia.Span, trivia.ToString().Substring(2));
 
-                    if (triviaList[index].IsKind(SyntaxKind.SingleLineCommentTrivia))
-                        firstIndex = index;
+                    i--;
                 }
                 else
                 {
@@ -98,29 +61,23 @@ namespace Pihrtsoft.CodeAnalysis.CSharp.Refactoring
                 }
             }
 
-            return firstIndex;
-        }
-
-        private static int FindLastTriviaToRemove(SyntaxTriviaList triviaList, int index)
-        {
-            int lastIndex = index;
-
-            while (index < triviaList.Count - 1)
+            i = index + 1;
+            while (i < triviaList.Count)
             {
-                if (IsAllowedTrivia(triviaList[index + 1]))
-                {
-                    index++;
+                SyntaxTrivia trivia = triviaList[i];
 
-                    if (triviaList[index].IsKind(SyntaxKind.SingleLineCommentTrivia))
-                        lastIndex = index;
+                if (IsAllowedTrivia(trivia))
+                {
+                    if (trivia.IsKind(SyntaxKind.SingleLineCommentTrivia))
+                        yield return new TextChange(trivia.Span, trivia.ToString().Substring(2));
+
+                    i++;
                 }
                 else
                 {
                     break;
                 }
             }
-
-            return lastIndex;
         }
 
         private static bool IsAllowedTrivia(SyntaxTrivia trivia)
@@ -128,9 +85,7 @@ namespace Pihrtsoft.CodeAnalysis.CSharp.Refactoring
             switch (trivia.Kind())
             {
                 case SyntaxKind.WhitespaceTrivia:
-                    return true;
                 case SyntaxKind.EndOfLineTrivia:
-                    return true;
                 case SyntaxKind.SingleLineCommentTrivia:
                     return true;
                 default:
