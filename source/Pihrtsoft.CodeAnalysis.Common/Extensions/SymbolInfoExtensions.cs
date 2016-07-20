cfdf31511f73c6fc0e3207953ff0c7525238d0b9
Josef Pihrt <josef.pihrt@outlook.com>
2016-07-20 22:53:48 +0200

    add extension method

---
+++ source/Pihrtsoft.CodeAnalysis.Common/Extensions/SymbolInfoExtensions.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+
+namespace Pihrtsoft.CodeAnalysis
+{
+    public static class SymbolInfoExtensions
+    {
+        public static bool IsEmpty(this SymbolInfo symbolInfo)
+        {
+            return symbolInfo.Symbol == null
+                && symbolInfo.CandidateSymbols.Length == 0;
+        }
+    }
+}
