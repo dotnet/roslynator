3d587efbe17dfa1cbfd06862ddbae6f033741f04
Josef Pihrt <josef.pihrt@outlook.com>
2016-08-11 06:17:24 +0200

    add extension methods

---
+++ source/Common/Extensions/IMethodSymbolExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.CodeAnalysis;
+
+namespace Pihrtsoft.CodeAnalysis
+{
+    public static class IMethodSymbolExtensions
+    {
+        public static bool IsOrdinary(this IMethodSymbol methodSymbol)
+        {
+            if (methodSymbol == null)
+                throw new ArgumentNullException(nameof(methodSymbol));
+
+            return methodSymbol.MethodKind == MethodKind.Ordinary;
+        }
+
+        public static bool IsReducedExtension(this IMethodSymbol methodSymbol)
+        {
+            if (methodSymbol == null)
+                throw new ArgumentNullException(nameof(methodSymbol));
+
+            return methodSymbol.MethodKind == MethodKind.ReducedExtension;
+        }
+    }
+}
