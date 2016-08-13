cb0e39b3b34ecabcdf125689b03b6cdb8eeb4eb0
Josef Pihrt <josef.pihrt@outlook.com>
2016-08-13 16:19:23 +0200

    Change version to 0.98.0

--- source/VisualStudio.Refactorings/Properties/AssemblyInfo.cs
+++ source/VisualStudio.Refactorings/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.97.1")]
+[assembly: AssemblyVersion("0.98.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
