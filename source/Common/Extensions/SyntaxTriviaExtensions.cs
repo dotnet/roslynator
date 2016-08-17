88ff4754b535eb956f5cfeddc349cac7fef1037e
Josef Pihrt <josef.pihrt@outlook.com>
2016-08-17 08:55:27 +0200

    Add extension methods

--- source/Common/Extensions/SyntaxTriviaExtensions.cs
+++ source/Common/Extensions/SyntaxTriviaExtensions.cs
@@ -48,6 +48,48 @@ namespace Pihrtsoft.CodeAnalysis
                 || kind == kind2;
         }
 
+        public static bool IsKind(this SyntaxTrivia trivia, SyntaxKind kind1, SyntaxKind kind2, SyntaxKind kind3)
+        {
+            SyntaxKind kind = trivia.Kind();
+
+            return kind == kind1
+                || kind == kind2
+                || kind == kind3;
+        }
+
+        public static bool IsKind(this SyntaxTrivia trivia, SyntaxKind kind1, SyntaxKind kind2, SyntaxKind kind3, SyntaxKind kind4)
+        {
+            SyntaxKind kind = trivia.Kind();
+
+            return kind == kind1
+                || kind == kind2
+                || kind == kind3
+                || kind == kind4;
+        }
+
+        public static bool IsKind(this SyntaxTrivia trivia, SyntaxKind kind1, SyntaxKind kind2, SyntaxKind kind3, SyntaxKind kind4, SyntaxKind kind5)
+        {
+            SyntaxKind kind = trivia.Kind();
+
+            return kind == kind1
+                || kind == kind2
+                || kind == kind3
+                || kind == kind4
+                || kind == kind5;
+        }
+
+        public static bool IsKind(this SyntaxTrivia trivia, SyntaxKind kind1, SyntaxKind kind2, SyntaxKind kind3, SyntaxKind kind4, SyntaxKind kind5, SyntaxKind kind6)
+        {
+            SyntaxKind kind = trivia.Kind();
+
+            return kind == kind1
+                || kind == kind2
+                || kind == kind3
+                || kind == kind4
+                || kind == kind5
+                || kind == kind6;
+        }
+
         public static bool IsWhitespaceTrivia(this SyntaxTrivia trivia)
         {
             return Microsoft.CodeAnalysis.CSharpExtensions.IsKind(trivia, SyntaxKind.WhitespaceTrivia);
@@ -85,16 +127,18 @@ namespace Pihrtsoft.CodeAnalysis
 
         public static bool IsDocumentationCommentTrivia(this SyntaxTrivia trivia)
         {
-            return trivia.IsSingleLineDocumentationCommentTrivia()
-                || trivia.IsMultiLineDocumentationCommentTrivia();
+            return trivia.IsKind(
+                SyntaxKind.SingleLineDocumentationCommentTrivia,
+                SyntaxKind.MultiLineDocumentationCommentTrivia);
         }
 
         public static bool IsCommentTrivia(this SyntaxTrivia trivia)
         {
-            return trivia.IsSingleLineCommentTrivia()
-                || trivia.IsSingleLineDocumentationCommentTrivia()
-                || trivia.IsMultiLineCommentTrivia()
-                || trivia.IsMultiLineDocumentationCommentTrivia();
+            return trivia.IsKind(
+                SyntaxKind.SingleLineCommentTrivia,
+                SyntaxKind.SingleLineDocumentationCommentTrivia,
+                SyntaxKind.MultiLineCommentTrivia,
+                SyntaxKind.MultiLineDocumentationCommentTrivia);
         }
     }
 }
