5379be80590fd2cf405c6ba601566464fcdd0db7
Josef Pihrt <josef.pihrt@outlook.com>
2016-08-19 10:16:19 +0200

    Full line(s) must be selected

--- source/Refactorings/Refactorings/WrapSelectedLines/SelectedLinesRefactoring.cs
+++ source/Refactorings/Refactorings/WrapSelectedLines/SelectedLinesRefactoring.cs
@@ -17,14 +17,38 @@ namespace Pihrtsoft.CodeAnalysis.CSharp.Refactorings.WrapSelectedLines
 
         public static async Task<bool> CanRefactorAsync(RefactoringContext context, SyntaxNode node)
         {
-            if (node
-                .DescendantTrivia(context.Span, descendIntoTrivia: true)
-                .Any(f => f.IsKind(SyntaxKind.EndOfLineTrivia)))
+            SyntaxNode root = context.Root;
+            TextSpan span = context.Span;
+
+            if (IsValidSpan(root, span))
             {
                 SourceText sourceText = await context.Document.GetTextAsync(context.CancellationToken).ConfigureAwait(false);
 
-                if (GetSelectedLines(sourceText, context.Span).Any())
+                if (IsFullLineSelection(sourceText, span))
                     return true;
+            }
+
+            return false;
+        }
+
+        private static bool IsValidSpan(SyntaxNode root, TextSpan span)
+        {
+            if (!span.IsEmpty)
+            {
+                int start = span.Start;
+                int end = span.End;
+
+                if (start == 0
+                    || root
+                        .FindTrivia(start - 1, findInsideTrivia: true)
+                        .IsKind(SyntaxKind.EndOfLineTrivia)
+                    || root
+                        .FindToken(start - 1, findInsideTrivia: true)
+                        .IsKind(SyntaxKind.XmlTextLiteralNewLineToken))
+                {
+                    if (!root.FindTrivia(span.End).IsKind(SyntaxKind.MultiLineCommentTrivia))
+                        return true;
+                }
             }
 
             return false;
@@ -49,12 +73,50 @@ namespace Pihrtsoft.CodeAnalysis.CSharp.Refactorings.WrapSelectedLines
 
         private static IEnumerable<TextLine> GetSelectedLines(SourceText sourceText, TextSpan span)
         {
-            foreach (TextLine line in sourceText
+            return sourceText
                 .Lines
                 .SkipWhile(f => span.Start > f.Start)
-                .TakeWhile(f => span.End >= f.EndIncludingLineBreak))
+                .TakeWhile(f => span.End >= f.End);
+        }
+
+        private static bool IsFullLineSelection(SourceText sourceText, TextSpan span)
+        {
+            return IsFullLineSelection(sourceText, span.Start, span.End);
+        }
+
+        private static bool IsFullLineSelection(SourceText sourceText, int start, int end)
+        {
+            using (TextLineCollection.Enumerator en = sourceText.Lines.GetEnumerator())
             {
-                yield return line;
+                while (en.MoveNext())
+                {
+                    if (start > en.Current.Start)
+                    {
+                        continue;
+                    }
+                    else if (start == en.Current.Start)
+                    {
+                        do
+                        {
+                            if (end < en.Current.End)
+                            {
+                                return false;
+                            }
+                            else if (end == en.Current.End
+                                || end == en.Current.EndIncludingLineBreak)
+                            {
+                                return true;
+                            }
+
+                        } while (en.MoveNext());
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                return false;
             }
         }
     }
