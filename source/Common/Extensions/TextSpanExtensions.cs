9f799edcdea51c8c932b9c56dd7e5a12800402a1
Josef Pihrt <josef.pihrt@outlook.com>
2016-08-19 18:13:05 +0200

    Add extension method

---
+++ source/Common/Extensions/TextSpanExtensions.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Pihrtsoft.CodeAnalysis
+{
+    public static class TextSpanExtensions
+    {
+        public static bool IsBetweenSpans(this TextSpan span, SyntaxNode node)
+        {
+            if (node == null)
+                throw new ArgumentNullException(nameof(node));
+
+            TextSpan nodeSpan = node.Span;
+            TextSpan nodeFullSpan = node.FullSpan;
+
+            return span.Start >= nodeFullSpan.Start
+                && span.Start <= nodeSpan.Start
+                && span.End >= nodeSpan.End
+                && span.End <= nodeFullSpan.End;
+        }
+    }
+}
