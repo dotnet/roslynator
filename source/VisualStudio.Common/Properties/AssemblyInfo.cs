85912427790bd251f4ea1fed24cbe034111ec7f8
Josef Pihrt <josef.pihrt@outlook.com>
2016-12-10 00:56:44 +0100

    Change version to 1.1.81

--- source/VisualStudio.Common/Properties/AssemblyInfo.cs
+++ source/VisualStudio.Common/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.8")]
+[assembly: AssemblyVersion("1.1.81")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
