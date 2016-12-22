6a4a5d146138808e82a61650e90a771d0f7ee4e6
Josef Pihrt <josef.pihrt@outlook.com>
2016-12-22 13:54:01 +0100

    Change version to 1.1.92

--- source/VisualStudio/Properties/AssemblyInfo.cs
+++ source/VisualStudio/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.91")]
+[assembly: AssemblyVersion("1.1.92")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
