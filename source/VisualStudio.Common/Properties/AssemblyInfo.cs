a21d5bfd61745ab34d7b96c57dd438d9173110d7
Josef Pihrt <josef.pihrt@outlook.com>
2016-12-28 15:31:12 +0100

    Change version to 1.1.93

--- source/VisualStudio.Common/Properties/AssemblyInfo.cs
+++ source/VisualStudio.Common/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.92")]
+[assembly: AssemblyVersion("1.1.93")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
--- source/VisualStudio.Refactorings/Properties/AssemblyInfo.cs
+++ source/VisualStudio.Refactorings/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.92")]
+[assembly: AssemblyVersion("1.1.93")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
