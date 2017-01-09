ddddb268a1289f54459c59dc2c15e6ac7e6aed91
Josef Pihrt <josef.pihrt@outlook.com>
2017-01-09 04:53:50 +0100

    Rename project 'Utilities' to 'Core'.

--- source/Core/Properties/AssemblyInfo.cs
+++ source/Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Roslynator.Utilities")]
+[assembly: AssemblyTitle("Roslynator.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pihrtsoft")]
-[assembly: AssemblyProduct("Roslynator.Utilities")]
+[assembly: AssemblyProduct("Roslynator.Core")]
 [assembly: AssemblyCopyright("Copyright (c) 2016-2017 Josef Pihrt")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
