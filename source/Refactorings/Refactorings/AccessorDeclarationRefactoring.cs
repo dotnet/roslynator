5558dfad0473435e4eae9b6fb877001575ee0c0c
Josef Pihrt <josef.pihrt@outlook.com>
2017-01-13 01:19:10 +0100

    Improve refactoring FormatAccessorBraces.

--- source/Refactorings/Refactorings/AccessorDeclarationRefactoring.cs
+++ source/Refactorings/Refactorings/AccessorDeclarationRefactoring.cs
@@ -22,9 +22,12 @@ namespace Roslynator.CSharp.Refactorings
             {
                 if (body.IsSingleLine())
                 {
-                    context.RegisterRefactoring(
-                        "Format braces on separate lines",
-                        cancellationToken => CSharpFormatter.ToMultiLineAsync(context.Document, accessor, cancellationToken));
+                    if (accessor.Parent?.IsMultiLine() == true)
+                    {
+                        context.RegisterRefactoring(
+                            "Format braces on separate lines",
+                            cancellationToken => CSharpFormatter.ToMultiLineAsync(context.Document, accessor, cancellationToken));
+                    }
                 }
                 else
                 {
