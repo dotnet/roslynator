2b7879100fb807c2cd4041b4bd320c31b33f9c76
Josef Pihrt <josef.pihrt@outlook.com>
2017-01-13 16:01:45 +0100

    Add missing using directive.

--- source/Refactorings/Refactorings/IntroduceAndInitialize/IntroduceAndInitializeInfo.cs
+++ source/Refactorings/Refactorings/IntroduceAndInitialize/IntroduceAndInitializeInfo.cs
@@ -4,6 +4,7 @@ using System;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Roslynator.Extensions;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 using static Roslynator.CSharp.CSharpFactory;
 
