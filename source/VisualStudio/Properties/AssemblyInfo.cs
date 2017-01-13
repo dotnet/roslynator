960d10e0152b3e868ccc82a37a7f365b007f60b6
Josef Pihrt <josef.pihrt@outlook.com>
2017-01-13 15:25:41 +0100

    Change version to 1.1.970

--- source/VisualStudio/Properties/AssemblyInfo.cs
+++ source/VisualStudio/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.960")]
+[assembly: AssemblyVersion("1.1.970")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
