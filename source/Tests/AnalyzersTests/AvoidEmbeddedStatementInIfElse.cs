164bc35313fba4b33fd8006b4d30fce491305035
Josef Pihrt <josef.pihrt@outlook.com>
2017-01-21 18:22:55 +0100

    Add analyzer AvoidEmbeddedStatementInIfElse.

---
+++ source/Tests/AnalyzersTests/AvoidEmbeddedStatementInIfElse.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace Roslynator.CSharp.Analyzers.Tests
+{
+#pragma warning disable RCS1004, RCS1007, RCS1073
+    internal static class AvoidEmbeddedStatementInIfElse
+    {
+        public static bool Foo()
+        {
+            bool f = false;
+
+            if (f)
+                return true;
+
+            if (f)
+                return true;
+            else
+                return false;
+        }
+    }
+#pragma warning restore RCS1004, RCS1007, RCS1073
+}
