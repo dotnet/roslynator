f596966fcd50af2d054854a35783bbbf0dc295a0
Josef Pihrt <josef.pihrt@outlook.com>
2017-02-02 12:58:57 +0100

    Add analyzer AddEmptyLineAfterClosingBrace.

---
+++ source/Analyzers/CodeFixProviders/AddEmptyLineAfterClosingBraceCodeFixProvider.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Composition;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Roslynator.CSharp.Extensions;
+using Roslynator.Extensions;
+
+namespace Roslynator.CSharp.CodeFixProviders
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AddEmptyLineAfterClosingBraceCodeFixProvider))]
+    [Shared]
+    public class AddEmptyLineAfterClosingBraceCodeFixProvider : BaseCodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(DiagnosticIdentifiers.AddEmptyLineAfterClosingBrace); }
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            SyntaxNode root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            SyntaxTrivia trivia = root.FindTrivia(context.Span.Start);
+
+            Debug.Assert(trivia.IsEndOfLineTrivia(), $"{nameof(trivia)} is not EOF");
+
+            if (trivia == null)
+                return;
+
+            CodeAction codeAction = CodeAction.Create(
+                "Add empty line",
+                cancellationToken => RefactorAsync(context.Document, trivia.Token, cancellationToken),
+                DiagnosticIdentifiers.AddEmptyLineAfterClosingBrace + EquivalenceKeySuffix);
+
+            context.RegisterCodeFix(codeAction, context.Diagnostics);
+        }
+
+        private static Task<Document> RefactorAsync(
+            Document document,
+            SyntaxToken token,
+            CancellationToken cancellationToken)
+        {
+            SyntaxToken newToken = token.AppendToTrailingTrivia(CSharpFactory.NewLineTrivia());
+
+            return document.ReplaceTokenAsync(token, newToken, cancellationToken);
+        }
+    }
+}
