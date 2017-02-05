7d29aedb296d887e6e3df6057669557791892397
Josef Pihrt <josef.pihrt@outlook.com>
2017-02-05 05:48:56 +0100

    Sort members.

--- source/Core/MetadataNames.cs
+++ source/Core/MetadataNames.cs
@@ -4,43 +4,33 @@ namespace Roslynator
 {
     public static class MetadataNames
     {
-        public const string System_FlagsAttribute = "System.FlagsAttribute";
-
+        public const string System_ArgumentException = "System.ArgumentException";
+        public const string System_ArgumentNullException = "System.ArgumentNullException";
         public const string System_Collections = "System.Collections";
-        public const string System_Collections_IEnumerable = "System.Collections.IEnumerable";
-
         public const string System_Collections_Generic = "System.Collections.Generic";
         public const string System_Collections_Generic_IEnumerable_T = "System.Collections.Generic.IEnumerable`1";
         public const string System_Collections_Generic_List_T = "System.Collections.Generic.List`1";
-
+        public const string System_Collections_IEnumerable = "System.Collections.IEnumerable";
         public const string System_Collections_Immutable_ImmutableArray = "System.Collections.Immutable.ImmutableArray";
         public const string System_Collections_Immutable_ImmutableArray_T = "System.Collections.Immutable.ImmutableArray`1";
-
         public const string System_ComponentModel = "System.ComponentModel";
         public const string System_ComponentModel_INotifyPropertyChanged = "System.ComponentModel.INotifyPropertyChanged";
-
         public const string System_EventArgs = "System.EventArgs";
         public const string System_EventHandler = "System.EventHandler";
         public const string System_EventHandler_T = "System.EventHandler`1";
-
         public const string System_Exception = "System.Exception";
-        public const string System_ArgumentException = "System.ArgumentException";
-        public const string System_ArgumentNullException = "System.ArgumentNullException";
-        public const string System_InvalidOperationException = "System.InvalidOperationException";
-        public const string System_NotImplementedException = "System.NotImplementedException";
-
+        public const string System_FlagsAttribute = "System.FlagsAttribute";
         public const string System_Func_T2 = "System.Func`2";
         public const string System_Func_T3 = "System.Func`3";
-
+        public const string System_InvalidOperationException = "System.InvalidOperationException";
         public const string System_Linq_Enumerable = "System.Linq.Enumerable";
         public const string System_Linq_ImmutableArrayExtensions = "System.Linq.ImmutableArrayExtensions";
         public const string System_Linq_IOrderedEnumerable_T = "System.Linq.IOrderedEnumerable`1";
-
+        public const string System_NotImplementedException = "System.NotImplementedException";
+        public const string System_Runtime_CompilerServices_ConfiguredTaskAwaitable_T = "System.Runtime.CompilerServices.ConfiguredTaskAwaitable`1";
         public const string System_String = "System.String";
         public const string System_StringComparison = "System.StringComparison";
-
         public const string System_Threading_Tasks_Task = "System.Threading.Tasks.Task";
         public const string System_Threading_Tasks_Task_T = "System.Threading.Tasks.Task`1";
-        public const string System_Runtime_CompilerServices_ConfiguredTaskAwaitable_T = "System.Runtime.CompilerServices.ConfiguredTaskAwaitable`1";
     }
 }
