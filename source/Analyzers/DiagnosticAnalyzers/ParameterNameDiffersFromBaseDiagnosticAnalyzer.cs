894600ebed5750d53152f89ea16c2926920d7f60
Josef Pihrt <josef.pihrt@outlook.com>
2017-02-19 16:40:51 +0100

    Add analyzer ParameterNameDiffersFromBase.

---
+++ source/Analyzers/DiagnosticAnalyzers/ParameterNameDiffersFromBaseDiagnosticAnalyzer.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Roslynator.CSharp.Refactorings;
+
+namespace Roslynator.CSharp.DiagnosticAnalyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ParameterNameDiffersFromBaseDiagnosticAnalyzer : BaseDiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get { return ImmutableArray.Create(DiagnosticDescriptors.ParameterNameDiffersFromBase); }
+        }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            context.RegisterSymbolAction(f => AnalyzeMethodSymbol(f), SymbolKind.Method);
+            context.RegisterSymbolAction(f => AnalyzePropertySymbol(f), SymbolKind.Property);
+        }
+
+        private void AnalyzeMethodSymbol(SymbolAnalysisContext context)
+        {
+            if (GeneratedCodeAnalyzer?.IsGeneratedCode(context) == true)
+                return;
+
+            ParameterNameDiffersFromBaseRefactoring.AnalyzeMethodSymbol(context);
+        }
+
+        private void AnalyzePropertySymbol(SymbolAnalysisContext context)
+        {
+            if (GeneratedCodeAnalyzer?.IsGeneratedCode(context) == true)
+                return;
+
+            ParameterNameDiffersFromBaseRefactoring.AnalyzePropertySymbol(context);
+        }
+    }
+}
