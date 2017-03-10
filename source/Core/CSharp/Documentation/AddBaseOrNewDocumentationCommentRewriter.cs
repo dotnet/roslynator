339894bd6ab87a21f549e237b6520c6bfb577212
Josef Pihrt <josef.pihrt@outlook.com>
2017-03-10 15:19:31 +0100

    Refactor DocumentationCommentGenerator and related items.

---
+++ source/Core/CSharp/Documentation/AddBaseOrNewDocumentationCommentRewriter.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Roslynator.CSharp.Documentation
+{
+    internal class AddBaseOrNewDocumentationCommentRewriter : AddNewDocumentationCommentRewriter
+    {
+        public AddBaseOrNewDocumentationCommentRewriter(DocumentationCommentGeneratorSettings settings, SemanticModel semanticModel, CancellationToken cancellationToken = default(CancellationToken))
+            : base(settings)
+        {
+            SemanticModel = semanticModel;
+            CancellationToken = cancellationToken;
+        }
+
+        public SemanticModel SemanticModel { get; }
+        public CancellationToken CancellationToken { get; }
+
+        protected override MemberDeclarationSyntax AddDocumentationComment(MemberDeclarationSyntax memberDeclaration)
+        {
+            return DocumentationCommentGenerator.AddNewDocumentationComment(memberDeclaration, Settings, SemanticModel, CancellationToken);
+        }
+    }
+}
