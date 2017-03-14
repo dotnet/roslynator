bb2d7520a03d90418eef09940ac3cb09792e3504
Josef Pihrt <josef.pihrt@outlook.com>
2017-03-14 21:52:59 +0100

    Add copyright banner.

--- source/Refactorings/GlobalSuppressions.cs
+++ source/Refactorings/GlobalSuppressions.cs
@@ -1,4 +1,5 @@
-﻿
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 // This file is used by Code Analysis to maintain SuppressMessage 
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given 
