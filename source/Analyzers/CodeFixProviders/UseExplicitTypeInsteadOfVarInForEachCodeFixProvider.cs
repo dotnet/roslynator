df31d2a99daf81256317339b28cec4739961114a
Josef Pihrt <josef.pihrt@outlook.com>
2017-03-25 04:13:01 +0100

    Fix ReplaceVarWithExplicitTypeInForEachCodeFixProvider and rename to UseExplicitTypeInsteadOfVarInForEachCodeFixProvider.

--- source/Analyzers/CodeFixProviders/UseExplicitTypeInsteadOfVarInForEachCodeFixProvider.cs
+++ source/Analyzers/CodeFixProviders/UseExplicitTypeInsteadOfVarInForEachCodeFixProvider.cs
@@ -2,11 +2,11 @@
 
 using System.Collections.Immutable;
 using System.Composition;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Roslynator.CodeFixes.Extensions;
 using Roslynator.CSharp.Extensions;
@@ -14,9 +14,9 @@ using Roslynator.CSharp.Refactorings;
 
 namespace Roslynator.CSharp.CodeFixProviders
 {
-    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(ReplaceVarWithExplicitTypeInForEachCodeFixProvider))]
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(UseExplicitTypeInsteadOfVarInForEachCodeFixProvider))]
     [Shared]
-    public class ReplaceVarWithExplicitTypeInForEachCodeFixProvider : BaseCodeFixProvider
+    public class UseExplicitTypeInsteadOfVarInForEachCodeFixProvider : BaseCodeFixProvider
     {
         public sealed override ImmutableArray<string> FixableDiagnosticIds
         {
@@ -31,6 +31,8 @@ namespace Roslynator.CSharp.CodeFixProviders
                 .FindNode(context.Span, getInnermostNodeForTie: true)?
                 .FirstAncestorOrSelf<ForEachStatementSyntax>();
 
+            Debug.Assert(forEachStatement != null, $"{nameof(forEachStatement)} is null");
+
             if (forEachStatement == null)
                 return;
 
@@ -40,15 +42,12 @@ namespace Roslynator.CSharp.CodeFixProviders
 
             ITypeSymbol typeSymbol = semanticModel.GetTypeSymbol(type, context.CancellationToken);
 
-            if (typeSymbol != null)
-            {
-                CodeAction codeAction = CodeAction.Create(
-                    $"Change type to '{SymbolDisplay.GetMinimalString(typeSymbol, semanticModel, type.SpanStart)}'",
-                    cancellationToken => ChangeTypeRefactoring.ChangeTypeAsync(context.Document, type, typeSymbol, cancellationToken),
-                    DiagnosticIdentifiers.UseExplicitTypeInsteadOfVarInForEach + EquivalenceKeySuffix);
+            CodeAction codeAction = CodeAction.Create(
+                $"Change type to '{SymbolDisplay.GetMinimalString(typeSymbol, semanticModel, type.SpanStart)}'",
+                cancellationToken => ChangeTypeRefactoring.ChangeTypeAsync(context.Document, type, typeSymbol, cancellationToken),
+                DiagnosticIdentifiers.UseExplicitTypeInsteadOfVarInForEach + EquivalenceKeySuffix);
 
-                context.RegisterCodeFix(codeAction, context.Diagnostics);
-            }
+            context.RegisterCodeFix(codeAction, context.Diagnostics);
         }
     }
 }
