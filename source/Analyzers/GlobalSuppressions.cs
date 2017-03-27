e4457853b03b7411dc442bc88587f1d89294bc15
Josef Pihrt <josef.pihrt@outlook.com>
2017-03-27 07:52:59 +0200

    Update GlobalSuppressions.

--- source/Analyzers/GlobalSuppressions.cs
+++ source/Analyzers/GlobalSuppressions.cs
@@ -5,6 +5,3 @@
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "RCS1146:Use conditional access.", Justification = "<Pending>", Scope = "member", Target = "~M:Roslynator.CSharp.Refactorings.UseElementAccessInsteadOfFirstRefactoring.Analyze(Microsoft.CodeAnalysis.Diagnostics.SyntaxNodeAnalysisContext,Microsoft.CodeAnalysis.CSharp.Syntax.InvocationExpressionSyntax,Microsoft.CodeAnalysis.CSharp.Syntax.MemberAccessExpressionSyntax)")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "RCS1146:Use conditional access.", Justification = "<Pending>", Scope = "member", Target = "~M:Roslynator.CSharp.Refactorings.UseElementAccessInsteadOfElementAtRefactoring.Analyze(Microsoft.CodeAnalysis.Diagnostics.SyntaxNodeAnalysisContext,Microsoft.CodeAnalysis.CSharp.Syntax.InvocationExpressionSyntax,Microsoft.CodeAnalysis.CSharp.Syntax.MemberAccessExpressionSyntax)")]
-
