7630a592c2b6ade561cf767199baa60e9cc03a09
Josef Pihrt <josef.pihrt@outlook.com>
2017-03-27 07:52:59 +0200

    Update GlobalSuppressions.

--- source/Core/GlobalSuppressions.cs
+++ source/Core/GlobalSuppressions.cs
@@ -5,6 +5,4 @@
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Simplification", "RCS1136:Merge switch sections with equivalent content.", Justification = "<Pending>", Scope = "member", Target = "~M:Roslynator.CSharp.Refactorings.ExpandExpressionBodyRefactoring.Refactor(Microsoft.CodeAnalysis.SyntaxNode,Microsoft.CodeAnalysis.CSharp.Syntax.ExpressionSyntax)~Microsoft.CodeAnalysis.SyntaxNode")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "RCS1146:Use conditional access.", Justification = "<Pending>", Scope = "member", Target = "~M:Roslynator.CSharp.StringExpressionChain.IsStringExpression(Microsoft.CodeAnalysis.CSharp.Syntax.ExpressionSyntax,Microsoft.CodeAnalysis.SemanticModel,System.Threading.CancellationToken)~System.Boolean")]
 
