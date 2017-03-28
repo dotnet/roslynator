c60d5fc4c0441e2c2810106324920a6c753b3b77
Josef Pihrt <josef.pihrt@outlook.com>
2017-03-28 10:44:16 +0200

    Replace property grid with WPF control.

--- source/VisualStudio.Core/GeneralOptionsPage.cs
+++ source/VisualStudio.Core/GeneralOptionsPage.cs
@@ -1,17 +1,25 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.ComponentModel;
+using System.Windows;
 using Microsoft.VisualStudio.Shell;
 using Roslynator.CSharp.Refactorings;
 using Roslynator.VisualStudio.TypeConverters;
 
 namespace Roslynator.VisualStudio
 {
-    public class GeneralOptionsPage : DialogPage
+    public class GeneralOptionsPage : UIElementDialogPage
     {
         public GeneralOptionsPage()
         {
             PrefixFieldIdentifierWithUnderscore = true;
+        }
+
+        private readonly GeneralOptionsPageControl _control = new GeneralOptionsPageControl();
+
+        protected override UIElement Child
+        {
+            get { return _control; }
         }
 
         [Category("General")]
@@ -29,10 +37,20 @@ namespace Roslynator.VisualStudio
             settings.PrefixFieldIdentifierWithUnderscore = PrefixFieldIdentifierWithUnderscore;
         }
 
+        protected override void OnActivate(CancelEventArgs e)
+        {
+            base.OnActivate(e);
+
+            _control.PrefixFieldIdentifierWithUnderscore = PrefixFieldIdentifierWithUnderscore;
+        }
+
         protected override void OnApply(PageApplyEventArgs e)
         {
             if (e.ApplyBehavior == ApplyKind.Apply)
+            {
+                PrefixFieldIdentifierWithUnderscore = _control.PrefixFieldIdentifierWithUnderscore;
                 ApplyTo(RefactoringSettings.Current);
+            }
 
             base.OnApply(e);
         }
---
+++ source/VisualStudio.Core/GeneralOptionsPageControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace Roslynator.VisualStudio
+{
+    public partial class GeneralOptionsPageControl : UserControl, INotifyPropertyChanged
+    {
+        private bool _prefixFieldIdentifierWithUnderscore;
+
+        public GeneralOptionsPageControl()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+
+        public bool PrefixFieldIdentifierWithUnderscore
+        {
+            get { return _prefixFieldIdentifierWithUnderscore; }
+
+            set
+            {
+                if (_prefixFieldIdentifierWithUnderscore != value)
+                {
+                    _prefixFieldIdentifierWithUnderscore = value;
+                    OnPropertyChanged(nameof(PrefixFieldIdentifierWithUnderscore));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
