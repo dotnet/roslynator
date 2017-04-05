919e891004f9726994f5e9b35817f1b0406f6136
Josef Pihrt <josef.pihrt@outlook.com>
2017-04-05 15:47:35 +0200

    Improve analyzer SimplifyMethodChain - Merge combination of Where and Any.

---
+++ source/Analyzers/Refactorings/CombineEnumerableWhereAndAnyRefactoring.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
+using Roslynator.CSharp.Extensions;
+using Roslynator.CSharp.Syntax;
+using Roslynator.Diagnostics.Extensions;
+using Roslynator.Extensions;
+
+namespace Roslynator.CSharp.Refactorings
+{
+    internal static class CombineEnumerableWhereAndAnyRefactoring
+    {
+        internal static void AnalyzeInvocationExpression(SyntaxNodeAnalysisContext context)
+        {
+            var invocationExpression = (InvocationExpressionSyntax)context.Node;
+
+            if (!invocationExpression.ContainsDiagnostics
+                && !invocationExpression.SpanContainsDirectives())
+            {
+                MemberInvocationWithSingleParameter invocation;
+                if (MemberInvocationWithSingleParameter.TryCreate(invocationExpression, out invocation)
+                    && invocation.NameText == "Any")
+                {
+                    MemberInvocationWithSingleParameter invocation2;
+                    if (MemberInvocationWithSingleParameter.TryCreate(invocation.Expression, out invocation2)
+                        && invocation2.NameText == "Where")
+                    {
+                        SemanticModel semanticModel = context.SemanticModel;
+                        CancellationToken cancellationToken = context.CancellationToken;
+
+                        if (semanticModel.GetExtensionMethodInfo(invocationExpression, cancellationToken).IsLinqExtensionOfIEnumerableOfTWithPredicate("Any")
+                            && semanticModel.GetExtensionMethodInfo(invocation2.InvocationExpression, cancellationToken).IsLinqWhere(allowImmutableArrayExtension: true))
+                        {
+                            LambdaWithSingleParameter lambda;
+                            if (LambdaWithSingleParameter.TryCreate(invocation.Argument.Expression, out lambda)
+                                && lambda.Body is ExpressionSyntax)
+                            {
+                                LambdaWithSingleParameter lambda2;
+                                if (LambdaWithSingleParameter.TryCreate(invocation2.Argument.Expression, out lambda2)
+                                    && lambda2.Body is ExpressionSyntax
+                                    && lambda.ParameterName.Equals(lambda2.ParameterName, StringComparison.Ordinal))
+                                {
+                                    context.ReportDiagnostic(
+                                        DiagnosticDescriptors.SimplifyLinqMethodChain,
+                                        Location.Create(context.SyntaxTree(), TextSpan.FromBounds(invocation2.Name.SpanStart, invocationExpression.Span.End)));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static Task<Document> RefactorAsync(
+            Document document,
+            InvocationExpressionSyntax invocationExpression,
+            CancellationToken cancellationToken)
+        {
+            MemberInvocationWithSingleParameter invocation = MemberInvocationWithSingleParameter.Create(invocationExpression);
+            MemberInvocationWithSingleParameter invocation2 = MemberInvocationWithSingleParameter.Create((InvocationExpressionSyntax)invocation.Expression);
+
+            LambdaWithSingleParameter lambda = LambdaWithSingleParameter.Create((LambdaExpressionSyntax)invocation.Argument.Expression);
+            LambdaWithSingleParameter lambda2 = LambdaWithSingleParameter.Create((LambdaExpressionSyntax)invocation2.Argument.Expression);
+
+            BinaryExpressionSyntax logicalAnd = CSharpFactory.LogicalAndExpression(
+                ((ExpressionSyntax)lambda2.Body).Parenthesize(moveTrivia: true).WithSimplifierAnnotation(),
+                ((ExpressionSyntax)lambda.Body).Parenthesize(moveTrivia: true).WithSimplifierAnnotation());
+
+            InvocationExpressionSyntax newNode = invocation2.InvocationExpression
+                .ReplaceNode(invocation2.Name, invocation.Name.WithTriviaFrom(invocation2.Name))
+                .WithArgumentList(invocation2.ArgumentList.ReplaceNode((ExpressionSyntax)lambda2.Body, logicalAnd));
+
+            return document.ReplaceNodeAsync(invocationExpression, newNode, cancellationToken);
+        }
+    }
+}
---
+++ source/Core/CSharp/Syntax/MemberInvocationWithSingleParameter.cs
@@ -0,0 +1,134 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Roslynator.CSharp.Extensions;
+using Roslynator.Extensions;
+
+namespace Roslynator.CSharp.Syntax
+{
+    internal struct MemberInvocationWithSingleParameter : IEquatable<MemberInvocationWithSingleParameter>
+    {
+        public MemberInvocationWithSingleParameter(ExpressionSyntax expression, SimpleNameSyntax name, ArgumentSyntax argument)
+        {
+            Expression = expression;
+            Name = name;
+            Argument = argument;
+        }
+
+        public ExpressionSyntax Expression { get; }
+        public SimpleNameSyntax Name { get; }
+        public ArgumentSyntax Argument { get; }
+
+        public InvocationExpressionSyntax InvocationExpression
+        {
+            get { return (InvocationExpressionSyntax)Node; }
+        }
+
+        public MemberAccessExpressionSyntax MemberAccessExpression
+        {
+            get { return (MemberAccessExpressionSyntax)Expression?.Parent; }
+        }
+
+        public string NameText
+        {
+            get { return Name?.Identifier.ValueText; }
+        }
+
+        private SyntaxNode Node
+        {
+            get { return Argument?.Parent?.Parent; }
+        }
+
+        public ArgumentListSyntax ArgumentList
+        {
+            get { return (ArgumentListSyntax)Argument?.Parent; }
+        }
+
+        public static MemberInvocationWithSingleParameter Create(InvocationExpressionSyntax invocationExpression)
+        {
+            if (invocationExpression == null)
+                throw new ArgumentNullException(nameof(invocationExpression));
+
+            var memberAccess = (MemberAccessExpressionSyntax)invocationExpression.Expression;
+
+            ArgumentListSyntax argumentList = invocationExpression.ArgumentList;
+
+            if (argumentList == null)
+                throw new ArgumentException("", nameof(invocationExpression));
+
+            SeparatedSyntaxList<ArgumentSyntax> arguments = argumentList.Arguments;
+
+            if (arguments.Count != 1)
+                throw new ArgumentException("", nameof(invocationExpression));
+
+            return new MemberInvocationWithSingleParameter(
+                memberAccess.Expression,
+                memberAccess.Name,
+                arguments[0]);
+        }
+
+        public static bool TryCreate(SyntaxNode invocationExpression, out MemberInvocationWithSingleParameter result)
+        {
+            if (invocationExpression?.IsKind(SyntaxKind.InvocationExpression) == true)
+                return TryCreate((InvocationExpressionSyntax)invocationExpression, out result);
+
+            result = default(MemberInvocationWithSingleParameter);
+            return false;
+        }
+
+        public static bool TryCreate(InvocationExpressionSyntax invocationExpression, out MemberInvocationWithSingleParameter result)
+        {
+            ArgumentListSyntax argumentList = invocationExpression?.ArgumentList;
+
+            if (argumentList != null)
+            {
+                SeparatedSyntaxList<ArgumentSyntax> arguments = argumentList.Arguments;
+
+                if (arguments.Count == 1)
+                {
+                    ExpressionSyntax expression = invocationExpression.Expression;
+
+                    if (expression?.IsKind(SyntaxKind.SimpleMemberAccessExpression) == true)
+                    {
+                        var memberAccessExpression = (MemberAccessExpressionSyntax)expression;
+
+                        result = new MemberInvocationWithSingleParameter(memberAccessExpression.Expression, memberAccessExpression.Name, arguments[0]);
+                        return true;
+                    }
+                }
+            }
+
+            result = default(MemberInvocationWithSingleParameter);
+            return false;
+        }
+
+        public bool Equals(MemberInvocationWithSingleParameter other)
+        {
+            return Node == other.Node;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is MemberInvocationWithSingleParameter
+                && Equals((MemberInvocationWithSingleParameter)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Node?.GetHashCode() ?? 0;
+        }
+
+        public static bool operator ==(MemberInvocationWithSingleParameter left, MemberInvocationWithSingleParameter right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(MemberInvocationWithSingleParameter left, MemberInvocationWithSingleParameter right)
+        {
+            return !left.Equals(right);
+        }
+    }
+}
