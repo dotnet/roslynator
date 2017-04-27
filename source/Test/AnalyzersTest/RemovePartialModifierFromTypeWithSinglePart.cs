ee741c7ccfd94a62dcef7b7fda3eea0c3932b3a2
Josef Pihrt <josef.pihrt@outlook.com>
2017-04-27 22:11:39 +0200

    Fix analyzer RemovePartialModifierFromTypeWithSinglePart (RCS1043) - Do not report type with partial method.

--- source/Test/AnalyzersTest/RemovePartialModifierFromTypeWithSinglePart.cs
+++ source/Test/AnalyzersTest/RemovePartialModifierFromTypeWithSinglePart.cs
@@ -7,8 +7,115 @@ namespace Roslynator.CSharp.Analyzers.Test
 {
     public static class RemovePartialModifierFromTypeWithSinglePart
     {
-        private partial class Foo
+        private static class Foo
         {
+            private partial class FooClass
+            {
+            }
+
+            private partial interface FooInterface
+            {
+            }
+
+            private partial struct FooStruct
+            {
+            }
+        }
+
+        private static class Foo2
+        {
+            private static partial class FooClass
+            {
+                private partial class FooClass2
+                {
+                }
+
+                private partial class FooClass2
+                {
+                }
+            }
+
+            private partial struct FooStruct
+            {
+                private partial class FooClass2
+                {
+                }
+
+                private partial class FooClass2
+                {
+                }
+            }
+        }
+
+        private static class Foo3
+        {
+            private static partial class FooClass
+            {
+                private partial interface FooInterface
+                {
+                }
+
+                private partial interface FooInterface
+                {
+                }
+            }
+
+            private partial struct FooStruct
+            {
+                private partial interface FooInterface
+                {
+                }
+
+                private partial interface FooInterface
+                {
+                }
+            }
+        }
+
+        private static class Foo4
+        {
+            private static partial class FooClass
+            {
+                private partial class FooClass2
+                {
+                }
+
+                private partial class FooClass2
+                {
+                }
+            }
+
+            private partial struct FooStruct
+            {
+                private partial class FooClass2
+                {
+                }
+
+                private partial class FooClass2
+                {
+                }
+            }
+        }
+
+        private static class Foo5
+        {
+            private partial class FooClass
+            {
+                partial void FooMethod();
+
+                partial void FooMethod()
+                {
+                }
+            }
+
+            private partial struct FooStruct
+            {
+                partial void FooMethod();
+
+                partial void FooMethod()
+                {
+                }
+            }
         }
     }
 }
