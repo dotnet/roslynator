a78231d948339c23403f471ea61d66dc1bff5989
Josef Pihrt <josef.pihrt@outlook.com>
2017-05-03 18:18:07 +0200

    Fix analyzer RemoveEmptyInitializer (RCS1041).

--- source/Analyzers/Refactorings/RemoveEmptyInitializerRefactoring.cs
+++ source/Analyzers/Refactorings/RemoveEmptyInitializerRefactoring.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
 
 namespace Roslynator.CSharp.Refactorings
 {
@@ -14,40 +16,66 @@ namespace Roslynator.CSharp.Refactorings
     {
         public static void Analyze(SyntaxNodeAnalysisContext context, ObjectCreationExpressionSyntax objectCreationExpression)
         {
-            TypeSyntax type = objectCreationExpression.Type;
-            InitializerExpressionSyntax initializer = objectCreationExpression.Initializer;
-
-            if (type?.IsMissing == false
-                && initializer?.Expressions.Any() == false
-                && initializer.OpenBraceToken.TrailingTrivia.All(f => f.IsWhitespaceOrEndOfLineTrivia())
-                && initializer.CloseBraceToken.LeadingTrivia.All(f => f.IsWhitespaceOrEndOfLineTrivia()))
+            if (!objectCreationExpression.ContainsDiagnostics)
             {
-                context.ReportDiagnostic(
-                    DiagnosticDescriptors.RemoveEmptyInitializer,
-                    initializer);
+                TypeSyntax type = objectCreationExpression.Type;
+
+                if (type?.IsMissing == false)
+                {
+                    InitializerExpressionSyntax initializer = objectCreationExpression.Initializer;
+
+                    if (initializer?.Expressions.Any() == false
+                        && initializer.OpenBraceToken.TrailingTrivia.All(f => f.IsWhitespaceOrEndOfLineTrivia())
+                        && initializer.CloseBraceToken.LeadingTrivia.All(f => f.IsWhitespaceOrEndOfLineTrivia()))
+                    {
+                        context.ReportDiagnostic(DiagnosticDescriptors.RemoveEmptyInitializer, initializer);
+                    }
+                }
             }
         }
 
         public static Task<Document> RefactorAsync(
             Document document,
-            InitializerExpressionSyntax initializer,
+            ObjectCreationExpressionSyntax objectCreationExpression,
             CancellationToken cancellationToken)
         {
-            var objectCreationExpression = (ObjectCreationExpressionSyntax)initializer.Parent;
+            ArgumentListSyntax argumentList = objectCreationExpression.ArgumentList;
+            InitializerExpressionSyntax initializer = objectCreationExpression.Initializer;
 
-            ObjectCreationExpressionSyntax newNode = objectCreationExpression
-                .WithInitializer(null);
-
-            if (newNode.ArgumentList == null)
+            ObjectCreationExpressionSyntax newNode = objectCreationExpression.WithInitializer(null);
+            if (argumentList == null)
             {
+                TypeSyntax type = objectCreationExpression.Type;
+
+                SyntaxTriviaList trailingTrivia = type.GetTrailingTrivia();
+
+                ArgumentListSyntax newArgumentList = SyntaxFactory.ArgumentList();
+
+                IEnumerable<SyntaxTrivia> trivia = objectCreationExpression.DescendantTrivia(TextSpan.FromBounds(type.Span.End, initializer.Span.End));
+
+                if (trivia.Any(f => !f.IsWhitespaceOrEndOfLineTrivia()))
+                    newArgumentList = newArgumentList.WithTrailingTrivia(trivia);
+
                 newNode = newNode
-                    .WithArgumentList(
-                        SyntaxFactory.ArgumentList()
-                            .WithTrailingTrivia(objectCreationExpression.Type.GetTrailingTrivia()));
+                    .WithType(type.WithoutTrailingTrivia())
+                    .WithArgumentList(newArgumentList);
+            }
+            else
+            {
+                IEnumerable<SyntaxTrivia> trivia = objectCreationExpression.DescendantTrivia(TextSpan.FromBounds(argumentList.Span.End, initializer.Span.End));
+
+                if (trivia.Any(f => !f.IsWhitespaceOrEndOfLineTrivia()))
+                {
+                    newNode = newNode.WithTrailingTrivia(trivia);
+                }
+                else
+                {
+                    newNode = newNode.WithoutTrailingTrivia();
+                }
             }
 
             newNode = newNode
-                .WithTriviaFrom(objectCreationExpression)
+                .AppendToTrailingTrivia(initializer.GetTrailingTrivia())
                 .WithFormatterAnnotation();
 
             return document.ReplaceNodeAsync(objectCreationExpression, newNode, cancellationToken);
