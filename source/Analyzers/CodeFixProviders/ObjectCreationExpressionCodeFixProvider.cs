cb7a2cf205b30ed415736f61dc4f3153ade68fd3
Josef Pihrt <josef.pihrt@outlook.com>
2017-05-06 03:09:18 +0200

    Fix #84: Report expressions in initializer as a whole and not one by one (RCS1095).

---
+++ source/Analyzers/CodeFixProviders/ObjectCreationExpressionCodeFixProvider.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Composition;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Roslynator.CSharp.Refactorings;
+
+namespace Roslynator.CSharp.CodeFixProviders
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(ObjectCreationExpressionCodeFixProvider))]
+    [Shared]
+    public class ObjectCreationExpressionCodeFixProvider : BaseCodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(DiagnosticIdentifiers.UseCSharp6DictionaryInitializer); }
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            SyntaxNode root = await context.GetSyntaxRootAsync().ConfigureAwait(false);
+
+            ObjectCreationExpressionSyntax objectCreationExpression = root
+                .FindNode(context.Span, getInnermostNodeForTie: true)?
+                .FirstAncestorOrSelf<ObjectCreationExpressionSyntax>();
+
+            Debug.Assert(objectCreationExpression != null, $"{nameof(objectCreationExpression)} is null");
+
+            if (objectCreationExpression == null)
+                return;
+
+            foreach (Diagnostic diagnostic in context.Diagnostics)
+            {
+                switch (diagnostic.Id)
+                {
+                    case DiagnosticIdentifiers.UseCSharp6DictionaryInitializer:
+                        {
+                            CodeAction codeAction = CodeAction.Create(
+                                "Use C# 6.0 dictionary initializer",
+                                cancellationToken => UseCSharp6DictionaryInitializerRefactoring.RefactorAsync(context.Document, objectCreationExpression.Initializer, cancellationToken),
+                                diagnostic.Id + EquivalenceKeySuffix);
+
+                            context.RegisterCodeFix(codeAction, diagnostic);
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
