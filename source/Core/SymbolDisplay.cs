50f48e116a790da907eb2f1e97b4f579fd1fd12f
Josef Pihrt <josef.pihrt@outlook.com>
2017-05-12 00:25:47 +0200

    Change parameter type from ITypeSymbol to ISymbol.

--- source/Core/SymbolDisplay.cs
+++ source/Core/SymbolDisplay.cs
@@ -6,14 +6,14 @@ namespace Roslynator
 {
     internal static class SymbolDisplay
     {
-        public static string GetString(ITypeSymbol typeSymbol)
+        public static string GetString(ISymbol symbol)
         {
-            return typeSymbol.ToDisplayString(Format);
+            return symbol.ToDisplayString(Format);
         }
 
-        public static string GetMinimalString(ITypeSymbol typeSymbol, SemanticModel semanticModel, int position)
+        public static string GetMinimalString(ISymbol symbol, SemanticModel semanticModel, int position)
         {
-            return typeSymbol.ToMinimalDisplayString(semanticModel, position, Format);
+            return symbol.ToMinimalDisplayString(semanticModel, position, Format);
         }
 
         public static SymbolDisplayFormat Format { get; } = new SymbolDisplayFormat(
