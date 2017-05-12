5cb4ab8c9b6c1be6d1277cd2ac7694e7fe0b7729
Josef Pihrt <josef.pihrt@outlook.com>
2017-05-12 02:58:28 +0200

    Fix analyzer UsePredefinedType (RCS1013).

--- source/Test/AnalyzersTest/UsePredefinedType.cs
+++ source/Test/AnalyzersTest/UsePredefinedType.cs
@@ -1,9 +1,18 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+#pragma warning disable RCS1078, RCS1056, RCS1176
+
 using System;
 using s = System.String;
 
 namespace Roslynator.CSharp.Analyzers.Test
 {
+    /// <summary>
+    /// <see cref="String"/>
+    /// <see cref="System.String"/>
+    /// <see cref="global::System.String"/>
+    /// <see cref="s"/>
+    /// </summary>
     internal static class UsePredefinedType
     {
         public static void MethodName()
