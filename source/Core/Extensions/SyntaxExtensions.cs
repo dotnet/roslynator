cfe94536bd3d1b4c7cc5da15669c86e25b69f186
Josef Pihrt <josef.pihrt@outlook.com>
2017-05-12 02:51:19 +0200

    Add new extension methods.

--- source/Core/Extensions/SyntaxExtensions.cs
+++ source/Core/Extensions/SyntaxExtensions.cs
@@ -487,6 +487,34 @@ namespace Roslynator
         {
             return tokenList.Replace(tokenList[index], newToken);
         }
+
+        public static bool Any(this SyntaxTokenList list, Func<SyntaxToken, bool> predicate)
+        {
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            foreach (SyntaxToken token in list)
+            {
+                if (predicate(token))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool All(this SyntaxTokenList list, Func<SyntaxToken, bool> predicate)
+        {
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            foreach (SyntaxToken token in list)
+            {
+                if (!predicate(token))
+                    return false;
+            }
+
+            return true;
+        }
         #endregion SyntaxTokenList
 
         #region SyntaxTrivia
@@ -571,6 +599,34 @@ namespace Roslynator
         public static SyntaxTriviaList ReplaceAt(this SyntaxTriviaList triviaList, int index, SyntaxTrivia newTrivia)
         {
             return triviaList.Replace(triviaList[index], newTrivia);
+        }
+
+        public static bool Any(this SyntaxTriviaList list, Func<SyntaxTrivia, bool> predicate)
+        {
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            foreach (SyntaxTrivia trivia in list)
+            {
+                if (predicate(trivia))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool All(this SyntaxTriviaList list, Func<SyntaxTrivia, bool> predicate)
+        {
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            foreach (SyntaxTrivia trivia in list)
+            {
+                if (!predicate(trivia))
+                    return false;
+            }
+
+            return true;
         }
         #endregion SyntaxTriviaList
     }
