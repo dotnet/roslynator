f47b2dfc83cf3a3faa6ddf21a6822bc7b3a3d15a
Josef Pihrt <josef.pihrt@outlook.com>
2017-06-27 13:38:45 +0200

    Improve refactoring UseListInsteadOfYield (RR0183).

--- source/Refactorings/Refactorings/YieldStatementRefactoring.cs
+++ source/Refactorings/Refactorings/YieldStatementRefactoring.cs
@@ -101,7 +101,11 @@ namespace Roslynator.CSharp.Refactorings
             }
 
             if (context.IsRefactoringEnabled(RefactoringIdentifiers.UseListInsteadOfYield))
-                UseListInsteadOfYieldRefactoring.ComputeRefactoring(context, yieldStatement);
+            {
+                SemanticModel semanticModel = await context.GetSemanticModelAsync().ConfigureAwait(false);
+
+                UseListInsteadOfYieldRefactoring.ComputeRefactoring(context, yieldStatement, semanticModel);
+            }
         }
     }
 }
