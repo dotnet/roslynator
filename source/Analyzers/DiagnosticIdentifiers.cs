1cc686f6280eaf8ff709be207419f4e178da1977
Josef Pihrt <josef.pihrt@outlook.com>
2017-07-11 20:16:09 +0200

    Add DiagnosticIdentifiers.Deprecated

--- source/Analyzers/DiagnosticIdentifiers.cs
+++ source/Analyzers/DiagnosticIdentifiers.cs
@@ -2,7 +2,7 @@
 
 namespace Roslynator.CSharp
 {
-    public static class DiagnosticIdentifiers
+    public static partial class DiagnosticIdentifiers
     {
         public const string Prefix = "RCS";
 
@@ -16,7 +16,6 @@ namespace Roslynator.CSharp
         public const string UseExplicitTypeInsteadOfVarWhenTypeIsNotObvious = Prefix + "1008";
         public const string UseExplicitTypeInsteadOfVarInForEach = Prefix + "1009";
         public const string UseVarInsteadOfExplicitTypeWhenTypeIsObvious = Prefix + "1010";
-        //public const string UseVarInsteadOfExplicitTypeInForEach = Prefix + "1011";
         public const string UseExplicitTypeInsteadOfVarWhenTypeIsObvious = Prefix + "1012";
         public const string UsePredefinedType = Prefix + "1013";
         public const string AvoidImplicitlyTypedArray = Prefix + "1014";
@@ -36,7 +35,6 @@ namespace Roslynator.CSharp
         public const string FormatSwitchSectionStatementOnSeparateLine = Prefix + "1028";
         public const string FormatBinaryOperatorOnNextLine = Prefix + "1029";
         public const string AddEmptyLineAfterEmbeddedStatement = Prefix + "1030";
-        //public const string RemoveRedundantBraces = Prefix + "1031";
         public const string RemoveRedundantParentheses = Prefix + "1032";
         public const string RemoveRedundantBooleanLiteral = Prefix + "1033";
         public const string RemoveRedundantSealedModifier = Prefix + "1034";
@@ -58,8 +56,6 @@ namespace Roslynator.CSharp
         public const string AddConstructorArgumentList = Prefix + "1050";
         public const string ParenthesizeConditionInConditionalExpression = Prefix + "1051";
         public const string DeclareEachAttributeSeparately = Prefix + "1052";
-        //public const string ReplaceForEachWithFor = Prefix + "1053";
-        //public const string MergeLocalDeclarationWithReturnStatement = Prefix + "1054";
         public const string AvoidSemicolonAtEndOfDeclaration = Prefix + "1055";
         public const string AvoidUsageOfUsingAliasDirective = Prefix + "1056";
         public const string AddEmptyLineBetweenDeclarations = Prefix + "1057";
@@ -120,44 +116,32 @@ namespace Roslynator.CSharp
         public const string CombineEnumerableWhereMethodChain = Prefix + "1112";
         public const string UseStringIsNullOrEmptyMethod = Prefix + "1113";
         public const string RemoveRedundantDelegateCreation = Prefix + "1114";
-        //public const string ReplaceReturnStatementWithExpressionStatement = Prefix + "1115";
-        //public const string AddBreakStatementToSwitchSection = Prefix + "1116";
-        //public const string AddReturnStatementThatReturnsDefaultValue = Prefix + "1117";
         public const string MarkLocalVariableAsConst = Prefix + "1118";
         public const string CallFindMethodInsteadOfFirstOrDefaultMethod = Prefix + "1119";
         public const string UseElementAccessInsteadOfElementAt = Prefix + "1120";
         public const string UseElementAccessInsteadOfFirst = Prefix + "1121";
-        //public const string AddMissingSemicolon = Prefix + "1122";
         public const string AddParenthesesAccordingToOperatorPrecedence = Prefix + "1123";
         public const string InlineLocalVariable = Prefix + "1124";
-        //public const string MarkMemberAsStatic = Prefix + "1125";
         public const string AvoidEmbeddedStatementInIfElse = Prefix + "1126";
         public const string MergeLocalDeclarationWithAssignment = Prefix + "1127";
         public const string UseCoalesceExpression = Prefix + "1128";
         public const string RemoveRedundantFieldInitialization = Prefix + "1129";
         public const string BitwiseOperationOnEnumWithoutFlagsAttribute = Prefix + "1130";
-        //public const string ReplaceReturnWithYieldReturn = Prefix + "1131";
         public const string RemoveRedundantOverridingMember = Prefix + "1132";
         public const string RemoveRedundantDisposeOrCloseCall = Prefix + "1133";
         public const string RemoveRedundantContinueStatement = Prefix + "1134";
         public const string DeclareEnumMemberWithZeroValue = Prefix + "1135";
         public const string MergeSwitchSectionsWithEquivalentContent = Prefix + "1136";
-        //public const string AddDocumentationComment = Prefix + "1137";
         public const string AddSummaryToDocumentationComment = Prefix + "1138";
         public const string AddSummaryElementToDocumentationComment = Prefix + "1139";
         public const string AddExceptionToDocumentationComment = Prefix + "1140";
         public const string AddParameterToDocumentationComment = Prefix + "1141";
         public const string AddTypeParameterToDocumentationComment = Prefix + "1142";
         public const string SimplifyCoalesceExpression = Prefix + "1143";
-        //public const string MarkContainingClassAsAbstract = Prefix + "1144";
         public const string RemoveRedundantAsOperator = Prefix + "1145";
         public const string UseConditionalAccess = Prefix + "1146";
-        //public const string RemoveInapplicableModifier = Prefix + "1147";
-        //public const string RemoveUnreachableCode = Prefix + "1148";
-        //public const string RemoveImplementationFromAbstractMember = Prefix + "1149";
         public const string CallStringConcatInsteadOfStringJoin = Prefix + "1150";
         public const string RemoveRedundantCast = Prefix + "1151";
-        //public const string MemberTypeMustMatchOverriddenMemberType = Prefix + "1152";
         public const string AddEmptyLineAfterClosingBrace = Prefix + "1153";
         public const string SortEnumMembers = Prefix + "1154";
         public const string UseStringComparison = Prefix + "1155";
@@ -172,7 +156,6 @@ namespace Roslynator.CSharp
         public const string UnusedTypeParameter = Prefix + "1164";
         public const string UnconstrainedTypeParameterCheckedForNull = Prefix + "1165";
         public const string ValueTypeObjectIsNeverEqualToNull = Prefix + "1166";
-        //public const string OverridingMemberCannotChangeAccessModifiers = Prefix + "1167";
         public const string ParameterNameDiffersFromBase = Prefix + "1168";
         public const string MarkFieldAsReadOnly = Prefix + "1169";
         public const string UseReadOnlyAutoProperty = Prefix + "1170";
