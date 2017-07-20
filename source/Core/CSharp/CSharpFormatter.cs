ce78bf905342965dea33c46883f05992d47d32df
Josef Pihrt <josef.pihrt@outlook.com>
2017-07-20 15:19:53 +0200

    Fix #120: Rewrite method CSharpFormatter.GetIndentation

--- source/Core/CSharp/CSharpFormatter.cs
+++ source/Core/CSharp/CSharpFormatter.cs
@@ -360,19 +360,33 @@ namespace Roslynator.CSharp
         {
             SyntaxTree tree = node.SyntaxTree;
 
-            if (tree == null)
-                return default(SyntaxTrivia);
+            if (tree != null)
+            {
+                TextSpan span = node.Span;
 
-            TextSpan span = node.Span;
+                int lineStartIndex = span.Start - tree.GetLineSpan(span, cancellationToken).StartLinePosition.Character;
 
-            int lineStartIndex = span.Start - tree.GetLineSpan(span, cancellationToken).StartLinePosition.Character;
+                while (!node.FullSpan.Contains(lineStartIndex))
+                    node = node.Parent;
 
-            while (!node.FullSpan.Contains(lineStartIndex))
-                node = node.Parent;
+                SyntaxToken token = node.FindToken(lineStartIndex);
 
-            SyntaxTriviaList trivia = node.GetLeadingTrivia();
+                if (!token.IsKind(SyntaxKind.None))
+                {
+                    SyntaxTriviaList leadingTrivia = token.LeadingTrivia;
 
-            return (trivia.Any()) ? trivia.Last() : default(SyntaxTrivia);
+                    if (leadingTrivia.Any()
+                        && leadingTrivia.FullSpan.Contains(lineStartIndex))
+                    {
+                        SyntaxTrivia trivia = leadingTrivia.Last();
+
+                        if (trivia.IsWhitespaceTrivia())
+                            return trivia;
+                    }
+                }
+            }
+
+            return EmptyWhitespace();
         }
 
         private static SyntaxTriviaList GetIncreasedIndentation(SyntaxNode node, CancellationToken cancellationToken = default(CancellationToken))
