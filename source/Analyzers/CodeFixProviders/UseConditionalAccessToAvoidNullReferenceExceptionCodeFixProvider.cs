073426c70fb4e471257f3cb7b9fab381a55be8f9
Josef Pihrt <josef.pihrt@outlook.com>
2017-07-31 20:01:09 +0200

    Fix #110: Add analyzer UseConditionalAccessToAvoidNullReferenceException (RCS1202).

---
+++ source/Analyzers/CodeFixProviders/UseConditionalAccessToAvoidNullReferenceExceptionCodeFixProvider.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Composition;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Roslynator.CSharp.Refactorings;
+
+namespace Roslynator.CSharp.CodeFixProviders
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(UseConditionalAccessToAvoidNullReferenceExceptionCodeFixProvider))]
+    [Shared]
+    public class UseConditionalAccessToAvoidNullReferenceExceptionCodeFixProvider : BaseCodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(DiagnosticIdentifiers.UseConditionalAccessToAvoidNullReferenceException); }
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            SyntaxNode root = await context.GetSyntaxRootAsync().ConfigureAwait(false);
+
+            //TODO: TryFindFirstAncestorOrSelf
+            SyntaxNode node = root
+                .FindNode(context.Span, getInnermostNodeForTie: true)?
+                .FirstAncestorOrSelf(SyntaxKind.SimpleMemberAccessExpression, SyntaxKind.ElementAccessExpression);
+
+            if (node == null)
+                return;
+
+            ExpressionSyntax expression = null;
+
+            switch (node.Kind())
+            {
+                case SyntaxKind.SimpleMemberAccessExpression:
+                    {
+                        expression = ((MemberAccessExpressionSyntax)node).Expression;
+                        break;
+                    }
+                case SyntaxKind.ElementAccessExpression:
+                    {
+                        expression = ((ElementAccessExpressionSyntax)node).Expression;
+                        break;
+                    }
+            }
+
+            CodeAction codeAction = CodeAction.Create(
+                "Use conditional access",
+                cancellationToken => UseConditionalAccessToAvoidNullReferenceExceptionRefactoring.RefactorAsync(context.Document, expression, cancellationToken),
+                DiagnosticIdentifiers.UseConditionalAccessToAvoidNullReferenceException + EquivalenceKeySuffix);
+
+            context.RegisterCodeFix(codeAction, context.Diagnostics);
+        }
+    }
+}
---
+++ source/Analyzers/Refactorings/UseConditionalAccessToAvoidNullReferenceExceptionRefactoring.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
+using Roslynator.CSharp.Syntax;
+
+namespace Roslynator.CSharp.Refactorings
+{
+    internal static class UseConditionalAccessToAvoidNullReferenceExceptionRefactoring
+    {
+        public static void Analyze(SyntaxNodeAnalysisContext context, MemberInvocationExpression memberInvocation)
+        {
+            switch (memberInvocation.NameText)
+            {
+                case "ElementAtOrDefault":
+                case "FirstOrDefault":
+                case "LastOrDefault":
+                    {
+                        InvocationExpressionSyntax invocationExpression = memberInvocation.InvocationExpression;
+
+                        ExpressionSyntax expression = invocationExpression.WalkUpParentheses();
+
+                        if (IsExpressionOfAccessExpression(expression))
+                        {
+                            MethodInfo methodInfo;
+                            if (context.SemanticModel.TryGetMethodInfo(invocationExpression, out methodInfo, context.CancellationToken)
+                                && methodInfo.ReturnType.IsReferenceType
+                                && methodInfo.IsContainingType(MetadataNames.System_Linq_Enumerable))
+                            {
+                                ReportDiagnostic(context, expression);
+                            }
+                        }
+
+                        break;
+                    }
+            }
+        }
+
+        public static void AnalyzeAsExpression(SyntaxNodeAnalysisContext context)
+        {
+            var asExpression = (BinaryExpressionSyntax)context.Node;
+
+            if (asExpression.IsParentKind(SyntaxKind.ParenthesizedExpression))
+            {
+                var expression = (ExpressionSyntax)asExpression.Parent;
+
+                expression = expression.WalkUpParentheses();
+
+                if (IsExpressionOfAccessExpression(expression))
+                    ReportDiagnostic(context, expression);
+            }
+        }
+
+        private static bool IsExpressionOfAccessExpression(ExpressionSyntax expression)
+        {
+            SyntaxNode parent = expression.Parent;
+
+            switch (parent?.Kind())
+            {
+                case SyntaxKind.SimpleMemberAccessExpression:
+                    return expression == ((MemberAccessExpressionSyntax)parent).Expression;
+                case SyntaxKind.ElementAccessExpression:
+                    return expression == ((ElementAccessExpressionSyntax)parent).Expression;
+            }
+
+            return false;
+        }
+
+        private static void ReportDiagnostic(SyntaxNodeAnalysisContext context, ExpressionSyntax expression)
+        {
+            context.ReportDiagnostic(
+                DiagnosticDescriptors.UseConditionalAccessToAvoidNullReferenceException,
+                Location.Create(expression.SyntaxTree, new TextSpan(expression.Span.End, 1)));
+        }
+
+        public static Task<Document> RefactorAsync(
+            Document document,
+            ExpressionSyntax expression,
+            CancellationToken cancellationToken)
+        {
+            var textChange = new TextChange(new TextSpan(expression.Span.End, 0), "?");
+
+            return document.WithTextChangeAsync(textChange, cancellationToken);
+        }
+    }
+}
