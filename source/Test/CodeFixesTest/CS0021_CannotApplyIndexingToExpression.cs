eb1e7e5ef3fbc8bf2f816a3bc4d303a0590634dd
Josef Pihrt <josef.pihrt@outlook.com>
2017-08-31 19:24:53 +0200

    Add code fix for CS0021.

---
+++ source/Test/CodeFixesTest/CS0021_CannotApplyIndexingToExpression.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Roslynator.CSharp.CodeFixes.Test
+{
+    internal static class CS0021_CannotApplyIndexingToExpression
+    {
+        private static void Foo()
+        {
+            int i = 0;
+
+            string s = i.ToString["f"];
+        }
+    }
+}
