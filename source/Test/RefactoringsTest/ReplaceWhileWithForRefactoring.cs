bad8943217c76b706da441d5907103b70e143f7b
Josef Pihrt <josef.pihrt@outlook.com>
2017-09-15 03:31:19 +0200

    Refactor refactoring ReplaceWhileWithFor (RR0151).

--- source/Test/RefactoringsTest/ReplaceWhileWithForRefactoring.cs
+++ source/Test/RefactoringsTest/ReplaceWhileWithForRefactoring.cs
@@ -16,6 +16,22 @@ namespace Roslynator.CSharp.Refactorings.Test
                 items[i] = null;
                 i++;
             }
+
+            xxx j = 0;
+            while (j < items.Count)
+            {
+                items[j] = null;
+            }
+
+            int k = 0;
+
+            Foo();
+            Foo();
+            while (k < items.Count)
+            {
+                items[k] = null;
+                k++;
+            }
         }
 
         public static void Foo2()
@@ -29,6 +45,76 @@ namespace Roslynator.CSharp.Refactorings.Test
                 items[i] = null;
                 i++;
                 j++;
+            }
+        }
+
+        //n
+
+        public static void Foo3()
+        {
+            var items = new List<string>();
+
+            int j = 0;
+            Foo();
+            while (j < items.Count)
+            {
+                items[j] = null;
+                j++;
+            }
+        }
+
+        public static void Foo4()
+        {
+            var items = new List<string>();
+
+            int i = 0;
+            long j = 0;
+            while (i < items.Count)
+            {
+                items[i] = null;
+                i++;
+            }
+        }
+
+        public static void Foo5()
+        {
+            var items = new List<string>();
+
+            int i = 0;
+            while (i < items.Count)
+            {
+                items[i] = null;
+                i++;
+            }
+
+            i = 0;
+        }
+
+        public static void Foo6()
+        {
+            var items = new List<string>();
+
+            Foo();
+            int i = 0;
+            while (i < items.Count)
+            {
+                items[i] = null;
+                i++;
+            }
+        }
+
+        public static void Foo7()
+        {
+            int i = 0;
+
+            var items = new List<string>();
+
+            Foo();
+            i += 2;
+            while (i < items.Count)
+            {
+                items[i] = null;
+                i++;
             }
         }
     }
