6f36f1c8f6303de1f4e238a2aa08175b20765ae8
Josef Pihrt <josef.pihrt@outlook.com>
2017-09-22 09:00:02 +0200

    Change version to 1.5.0

--- source/VisualStudio.Refactorings/Properties/AssemblyInfo.cs
+++ source/VisualStudio.Refactorings/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.58")]
+[assembly: AssemblyVersion("1.5.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
