6c18e89485b1dd22162107b211cf8a33f822feaa
Karlen Simonyan <szkarlen@gmail.com>
2017-12-10 17:05:49 +0100

    Move usings after file header

--- source/Analyzers/Refactorings/ImplementExceptionConstructorsRefactoring.cs
+++ source/Analyzers/Refactorings/ImplementExceptionConstructorsRefactoring.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections.Immutable;
-// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
