a2d6a4c9b3454e3b4e145a7c9875ab00c27cd357
Hake <lzl1918@outlook.com>
2018-01-30 20:40:50 +0800

    add rules for windows runtime interfaces

--- source/Analyzers/Refactorings/NonAsynchronousMethodNameShouldNotEndWithAsyncRefactoring.cs
+++ source/Analyzers/Refactorings/NonAsynchronousMethodNameShouldNotEndWithAsyncRefactoring.cs
@@ -1,5 +1,16 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+// The original rule does not consider the following four return types for Universal Windows Platform:
+//      Windows.Foundation.IAsyncAction
+//      Windows.Foundation.IAsyncOperation<TResult>
+//      Windows.Foundation.IAsyncActionWithProgress<TProgress>
+//      Windows.Foundation.IAsyncOperationWithProgress<TResult, TProgress>
+
+// Meanwhile, the rule ignores names of those abstract methods.
+// In my personal viewpoint, these methods should also get included into considerations.
+
+#define DO_NOT_PASS_ABSTRACT
+
 using System;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -13,14 +24,52 @@ namespace Roslynator.CSharp.Refactorings
     {
         private const string AsyncSuffix = "Async";
 
+        private static bool ShouldAddAsyncSuffix(this ITypeSymbol type, SemanticModel model)
+        {
+            // skip some common types
+            if (type.IsVoid()
+                || type.IsString()
+                || type.IsArrayType()
+                || type.IsPredefinedValueType()
+                || type.IsTupleType
+                || type.IsEnum()
+                || type.MetadataName.Length <= 0)
+                return false;
+
+            if (type.IsTaskOrInheritsFromTask(model))
+                return true;
+
+            // check if the return type implements Windows.Foundation.IAsyncAction
+            INamedTypeSymbol symbol = model.GetTypeByMetadataName("Windows.Foundation.IAsyncAction");
+            if (type.EqualsOrInheritsFrom(symbol))
+                return true;
+
+            // then we will check if the type is directly one of the remaining three mentioned interfaces
+            // notice that, it is quite rare that we create types which implement those interfaces
+            // for effciency, we will not check the derived parent types
+            if (type is INamedTypeSymbol namedType
+                && namedType.IsInterface()
+                && namedType.IsGenericType)
+            {
+                if (namedType.IsConstructedFrom(model.GetTypeByMetadataName("Windows.Foundation.IAsyncOperation`1"))
+                    || namedType.IsConstructedFrom(model.GetTypeByMetadataName("Windows.Foundation.IAsyncActionWithProgress`1"))
+                    || namedType.IsConstructedFrom(model.GetTypeByMetadataName("Windows.Foundation.IAsyncOperationWithProgress`2")))
+                    return true;
+            }
+
+            return false;
+        }
+
         public static void Analyze(SyntaxNodeAnalysisContext context, MethodDeclarationSyntax methodDeclaration)
         {
             IMethodSymbol methodSymbol = context.SemanticModel.GetDeclaredSymbol(methodDeclaration, context.CancellationToken);
 
             if (methodSymbol?.IsAsync == false
+#if !DO_NOT_PASS_ABSTRACT
                 && !methodSymbol.IsAbstract
+#endif
                 && methodSymbol.Name.EndsWith(AsyncSuffix, StringComparison.Ordinal)
-                && !methodSymbol.ReturnType.IsTaskOrInheritsFromTask(context.SemanticModel))
+                && !methodSymbol.ReturnType.ShouldAddAsyncSuffix(context.SemanticModel))
             {
                 SyntaxToken identifier = methodDeclaration.Identifier;
 
