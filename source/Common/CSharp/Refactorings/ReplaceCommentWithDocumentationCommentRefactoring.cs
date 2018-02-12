3e8643758518727e0b4abaf63810e5ef38a89ea3
Josef Pihrt <josef.pihrt@outlook.com>
2018-02-12 12:02:55 +0100

    Fix #272: Improve analyzer ReplaceCommentWithDocumentationComment (RCS1181).

--- source/Common/CSharp/Refactorings/ReplaceCommentWithDocumentationCommentRefactoring.cs
+++ source/Common/CSharp/Refactorings/ReplaceCommentWithDocumentationCommentRefactoring.cs
@@ -20,113 +20,196 @@ namespace Roslynator.CSharp.Refactorings
 
         private static readonly Regex _leadingSlashesRegex = new Regex(@"^//\s*");
 
-        public static bool IsFixable(SyntaxTrivia trivia)
+        public static TextSpan GetFixableSpan(SyntaxTrivia trivia)
         {
             if (trivia.Kind() != SyntaxKind.SingleLineCommentTrivia)
-                return false;
+                return default(TextSpan);
 
             if (!(trivia.Token.Parent is MemberDeclarationSyntax memberDeclaration))
-                return false;
+                return default(TextSpan);
 
-            if (trivia.SpanStart >= memberDeclaration.SpanStart)
-                return false;
+            TextSpan span = trivia.Span;
 
-            SyntaxTriviaList leadingTrivia = memberDeclaration.GetLeadingTrivia();
-
-            int i = leadingTrivia.IndexOf(trivia);
-
-            Debug.Assert(i != -1, trivia.ToString());
-
-            if (i == -1)
-                return false;
-
-            i++;
-
-            while (i < leadingTrivia.Count)
+            if (memberDeclaration.LeadingTriviaSpan().Contains(span)
+                || memberDeclaration.TrailingTriviaSpan().Contains(span))
             {
-                if (!leadingTrivia[i].IsKind(
-                    SyntaxKind.WhitespaceTrivia,
-                    SyntaxKind.EndOfLineTrivia,
-                    SyntaxKind.SingleLineCommentTrivia))
-                {
-                    return false;
-                }
+                (TextSpan fixableSpan, bool hasDocumentationComment) = AnalyzeLeadingTrivia(memberDeclaration);
 
-                i++;
-            }
-
-            return true;
-        }
-
-        public static TextSpan GetFixableSpan(MemberDeclarationSyntax memberDeclaration)
-        {
-            return GetFixableSpan(memberDeclaration.GetLeadingTrivia());
-        }
-
-        private static TextSpan GetFixableSpan(SyntaxTriviaList leadingTrivia)
-        {
-            if (leadingTrivia.Any())
-            {
-                var span = default(TextSpan);
-
-                int i = leadingTrivia.Count - 1;
-                while (i >= 0)
-                {
-                    if (leadingTrivia[i].IsWhitespaceTrivia())
-                        i--;
-
-                    if (i < 0)
-                        break;
-
-                    if (!leadingTrivia[i].IsEndOfLineTrivia())
-                        break;
-
-                    i--;
-
-                    if (i < 0)
-                        break;
-
-                    SyntaxTrivia trivia = leadingTrivia[i];
-
-                    if (!trivia.IsKind(SyntaxKind.SingleLineCommentTrivia))
-                        break;
-
-                    span = (span == default(TextSpan))
-                        ? trivia.Span
-                        : TextSpan.FromBounds(trivia.SpanStart, span.End);
-
-                    i--;
-                }
-
-                return span;
+                if (fixableSpan.Contains(span))
+                    return fixableSpan;
             }
 
             return default(TextSpan);
         }
 
+        public static (TextSpan span, bool hasDocumentationComment) AnalyzeLeadingTrivia(SyntaxNode declaration)
+        {
+            SyntaxTriviaList leadingTrivia = declaration.GetLeadingTrivia();
+
+            SyntaxTriviaList.Reversed.Enumerator en = leadingTrivia.Reverse().GetEnumerator();
+
+            var span = default(TextSpan);
+
+            while (en.MoveNext())
+            {
+                if (en.Current.Kind() == SyntaxKind.WhitespaceTrivia
+                    && !en.MoveNext())
+                {
+                    break;
+                }
+
+                if (en.Current.Kind() == SyntaxKind.EndOfLineTrivia)
+                {
+                    if (!en.MoveNext())
+                        break;
+
+                    if (en.Current.Kind() == SyntaxKind.SingleLineCommentTrivia)
+                    {
+                        span = (span == default(TextSpan))
+                            ? en.Current.Span
+                            : TextSpan.FromBounds(en.Current.SpanStart, span.End);
+                    }
+                }
+                else
+                {
+                    do
+                    {
+                        if (SyntaxFacts.IsDocumentationCommentTrivia(en.Current.Kind()))
+                            return (default(TextSpan), true);
+
+                    } while (en.MoveNext());
+
+                    break;
+                }
+            }
+
+            return (span, false);
+        }
+
+        public static (TextSpan span, bool containsEndOfLine) AnalyzeTrailingTrivia(SyntaxNode declaration)
+        {
+            if (declaration == null)
+                return (default(TextSpan), false);
+
+            return AnalyzeTrailingTrivia(declaration.GetTrailingTrivia());
+        }
+
+        public static (TextSpan span, bool containsEndOfLine) AnalyzeTrailingTrivia<TNode>(SyntaxList<TNode> nodes) where TNode : SyntaxNode
+        {
+            TNode node = nodes.FirstOrDefault();
+
+            if (node == null)
+                return (default(TextSpan), false);
+
+            return AnalyzeTrailingTrivia(node.GetTrailingTrivia());
+        }
+
+        public static (TextSpan span, bool containsEndOfLine) AnalyzeTrailingTrivia(SyntaxToken token)
+        {
+            return AnalyzeTrailingTrivia(token.TrailingTrivia);
+        }
+
+        public static (TextSpan span, bool containsEndOfLine) AnalyzeTrailingTrivia(SyntaxTriviaList trailingTrivia)
+        {
+            SyntaxTriviaList.Enumerator en = trailingTrivia.GetEnumerator();
+
+            if (en.MoveNext())
+            {
+                SyntaxKind kind = en.Current.Kind();
+
+                if (kind == SyntaxKind.WhitespaceTrivia)
+                {
+                    if (en.MoveNext())
+                    {
+                        if (en.Current.Kind() == SyntaxKind.SingleLineCommentTrivia)
+                        {
+                            return (en.Current.Span, false);
+                        }
+                        else
+                        {
+                            while (en.MoveNext())
+                            {
+                                if (en.Current.IsEndOfLineTrivia())
+                                    return (default(TextSpan), true);
+                            }
+                        }
+                    }
+                }
+                else if (kind == SyntaxKind.EndOfLineTrivia)
+                {
+                    return (default(TextSpan), true);
+                }
+                else if (kind == SyntaxKind.SingleLineCommentTrivia)
+                {
+                    return (en.Current.Span, false);
+                }
+                else
+                {
+                    while (en.MoveNext())
+                    {
+                        if (en.Current.IsEndOfLineTrivia())
+                            return (default(TextSpan), true);
+                    }
+                }
+            }
+
+            return (default(TextSpan), false);
+        }
+
         public static Task<Document> RefactorAsync(
             Document document,
-            MemberDeclarationSyntax memberDeclaration,
+            MemberDeclarationSyntax declaration,
+            TextSpan span,
             CancellationToken cancellationToken)
         {
-            SyntaxTriviaList leadingTrivia = memberDeclaration.GetLeadingTrivia();
+            MemberDeclarationSyntax newDeclaration = declaration;
 
-            TextSpan commentsSpan = GetFixableSpan(leadingTrivia);
+            ImmutableArray<string> comments;
 
-            ImmutableArray<string> comments = leadingTrivia
-                .Where(f => commentsSpan.Contains(f.Span) && f.IsKind(SyntaxKind.SingleLineCommentTrivia))
-                .Select(f => _leadingSlashesRegex.Replace(f.ToString(), ""))
-                .ToImmutableArray();
+            SyntaxTriviaList leadingTrivia = declaration.GetLeadingTrivia();
 
-            TextSpan spanToRemove = TextSpan.FromBounds(commentsSpan.Start, memberDeclaration.SpanStart);
+            if (leadingTrivia.Span.Contains(span))
+            {
+                comments = leadingTrivia
+                    .Where(f => span.Contains(f.Span) && f.IsKind(SyntaxKind.SingleLineCommentTrivia))
+                    .Select(f => _leadingSlashesRegex.Replace(f.ToString(), ""))
+                    .ToImmutableArray();
 
-            MemberDeclarationSyntax newNode = memberDeclaration.WithLeadingTrivia(leadingTrivia.Where(f => !spanToRemove.Contains(f.Span)));
+                TextSpan spanToRemove = TextSpan.FromBounds(span.Start, declaration.SpanStart);
+
+                newDeclaration = declaration.WithLeadingTrivia(leadingTrivia.Where(f => !spanToRemove.Contains(f.Span)));
+            }
+            else
+            {
+                SyntaxTrivia trivia = declaration.FindTrivia(span.Start);
+
+                Debug.Assert(trivia != default(SyntaxTrivia));
+
+                SyntaxToken token = trivia.Token;
+
+                SyntaxTriviaList trailingTrivia = token.TrailingTrivia;
+
+                Debug.Assert(trailingTrivia.Contains(trivia));
+
+                for (int i = 0; i < trailingTrivia.Count; i++)
+                {
+                    if (trailingTrivia[i].Span == span)
+                    {
+                        comments = ImmutableArray.Create(_leadingSlashesRegex.Replace(trailingTrivia[i].ToString(), ""));
+
+                        SyntaxToken newToken = token.WithTrailingTrivia(trailingTrivia.Skip(i + 1));
+
+                        newDeclaration = newDeclaration.ReplaceToken(token, newToken);
+                        break;
+                    }
+                }
+            }
 
             var settings = new DocumentationCommentGeneratorSettings(comments);
 
-            newNode = newNode.WithNewSingleLineDocumentationComment(settings);
+            newDeclaration = newDeclaration.WithNewSingleLineDocumentationComment(settings);
 
-            return document.ReplaceNodeAsync(memberDeclaration, newNode, cancellationToken);
+            return document.ReplaceNodeAsync(declaration, newDeclaration, cancellationToken);
         }
     }
 }
