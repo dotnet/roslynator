c77e96584332fef037dc4c30031a4a3fa3eadc58
Josef Pihrt <josef.pihrt@outlook.com>
2018-03-20 14:07:38 +0100

    Change version to 1.8.0.0

--- source/VisualStudio/Properties/AssemblyInfo.cs
+++ source/VisualStudio/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.7.2")]
+[assembly: AssemblyVersion("1.8.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
