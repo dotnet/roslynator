7122eb56d35c017dce1ef0bcaac4fa615fa6c026
Josef Pihrt <josef.pihrt@outlook.com>
2018-10-01 20:09:36 +0200

    Fix code fix for analyzer AvoidBoxingOfValueType (RCS1198).

---
+++ src/Analyzers.CodeFixes/CSharp/CodeFixes/AvoidBoxingOfValueTypeCodeFixProvider.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Roslynator.CodeFixes;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+using static Roslynator.CSharp.CSharpFactory;
+
+namespace Roslynator.CSharp.CodeFixes
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AvoidBoxingOfValueTypeCodeFixProvider))]
+    [Shared]
+    public class AvoidBoxingOfValueTypeCodeFixProvider : BaseCodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(DiagnosticIdentifiers.AvoidBoxingOfValueType); }
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            SyntaxNode root = await context.GetSyntaxRootAsync().ConfigureAwait(false);
+
+            if (!TryFindFirstAncestorOrSelf(root, context.Span, out ExpressionSyntax expression))
+                return;
+
+            foreach (Diagnostic diagnostic in context.Diagnostics)
+            {
+                switch (diagnostic.Id)
+                {
+                    case DiagnosticIdentifiers.AvoidBoxingOfValueType:
+                        {
+                            CodeAction codeAction = CodeAction.Create(
+                                (expression.IsKind(SyntaxKind.CharacterLiteralExpression))
+                                    ? "Use string literal instead of character literal"
+                                    : "Call 'ToString'",
+                                cancellationToken => RefactorAsync(context.Document, expression, cancellationToken),
+                                GetEquivalenceKey(diagnostic));
+
+                            context.RegisterCodeFix(codeAction, diagnostic);
+                            break;
+                        }
+                }
+            }
+        }
+
+        private static async Task<Document> RefactorAsync(
+            Document document,
+            ExpressionSyntax expression,
+            CancellationToken cancellationToken)
+        {
+            ExpressionSyntax newNode = null;
+
+            if (expression.Kind() == SyntaxKind.CharacterLiteralExpression)
+            {
+                var literalExpression = (LiteralExpressionSyntax)expression;
+
+                newNode = StringLiteralExpression(literalExpression.Token.ValueText);
+            }
+            else
+            {
+                ParenthesizedExpressionSyntax newExpression = expression
+                    .WithoutTrivia()
+                    .Parenthesize();
+
+                SemanticModel semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+
+                if (ShouldAddConditionalAccess(semanticModel))
+                {
+                    newNode = ConditionalAccessExpression(
+                        newExpression,
+                        InvocationExpression(MemberBindingExpression(IdentifierName("ToString")), ArgumentList()));
+                }
+                else
+                {
+                    newNode = SimpleMemberInvocationExpression(
+                        newExpression,
+                        IdentifierName("ToString"),
+                        ArgumentList());
+                }
+            }
+
+            newNode = newNode.WithTriviaFrom(expression);
+
+            return await document.ReplaceNodeAsync(expression, newNode, cancellationToken).ConfigureAwait(false);
+
+            bool ShouldAddConditionalAccess(SemanticModel semanticModel)
+            {
+                if (!semanticModel.GetTypeSymbol(expression, cancellationToken).IsNullableType())
+                    return false;
+
+                if (!expression.IsKind(SyntaxKind.ConditionalAccessExpression))
+                    return true;
+
+                var conditionalAccess = (ConditionalAccessExpressionSyntax)expression;
+
+                return semanticModel
+                    .GetTypeSymbol(conditionalAccess.WhenNotNull, cancellationToken)
+                    .IsNullableType();
+            }
+        }
+    }
+}
---
+++ src/Tests/Analyzers.Tests/RCS1198AvoidBoxingOfValueTypeTests.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Roslynator.CSharp.CodeFixes;
+using Xunit;
+
+namespace Roslynator.CSharp.Analysis.Tests
+{
+    public class RCS1198AvoidBoxingOfValueTypeTests : AbstractCSharpCodeFixVerifier
+    {
+        public override DiagnosticDescriptor Descriptor { get; } = DiagnosticDescriptors.AvoidBoxingOfValueType;
+
+        public override DiagnosticAnalyzer Analyzer { get; } = new AvoidBoxingOfValueTypeAnalyzer();
+
+        public override CodeFixProvider FixProvider { get; } = new AvoidBoxingOfValueTypeCodeFixProvider();
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.AvoidBoxingOfValueType)]
+        public async Task Test_Interpolation()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System;
+
+class C
+{
+    public TimeSpan P { get; }
+
+    void M()
+    {
+        var c = new C();
+
+        var x = $""{[|c?.P.TotalMilliseconds|]}"";
+    }
+}
+", @"
+using System;
+
+class C
+{
+    public TimeSpan P { get; }
+
+    void M()
+    {
+        var c = new C();
+
+        var x = $""{(c?.P.TotalMilliseconds).ToString()}"";
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.AvoidBoxingOfValueType)]
+        public async Task Test_Interpolation_NullableType()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+class C
+{
+    void M()
+    {
+        int? i = null;
+
+        string s = $""{[|i|]}"";
+    }
+}
+", @"
+class C
+{
+    void M()
+    {
+        int? i = null;
+
+        string s = $""{i?.ToString()}"";
+    }
+}
+");
+        }
+    }
+}
