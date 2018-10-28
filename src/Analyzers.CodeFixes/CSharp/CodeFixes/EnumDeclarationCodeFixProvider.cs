3c9830966ba91756b88026a1b731f2c380821457
Josef Pihrt <josef.pihrt@outlook.com>
2018-10-28 13:31:12 +0100

    Keep empty lines after the code fix is applied (RCS1154) (fix #452).

--- src/Analyzers.CodeFixes/CSharp/CodeFixes/EnumDeclarationCodeFixProvider.cs
+++ src/Analyzers.CodeFixes/CSharp/CodeFixes/EnumDeclarationCodeFixProvider.cs
@@ -2,13 +2,18 @@
 
 using System.Collections.Immutable;
 using System.Composition;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Roslynator.CodeFixes;
+using Roslynator.CSharp.Comparers;
 using Roslynator.CSharp.Refactorings;
+using static Roslynator.CSharp.CSharpFactory;
 
 namespace Roslynator.CSharp.CodeFixes
 {
@@ -51,7 +56,7 @@ namespace Roslynator.CSharp.CodeFixes
                         {
                             CodeAction codeAction = CodeAction.Create(
                                 $"Sort '{enumDeclaration.Identifier}' members",
-                                cancellationToken => SortEnumMembersRefactoring.RefactorAsync(context.Document, enumDeclaration, cancellationToken),
+                                cancellationToken => SortEnumMembersAsync(context.Document, enumDeclaration, cancellationToken),
                                 GetEquivalenceKey(diagnostic));
 
                             context.RegisterCodeFix(codeAction, diagnostic);
@@ -59,6 +64,84 @@ namespace Roslynator.CSharp.CodeFixes
                         }
                 }
             }
+        }
+
+        private static async Task<Document> SortEnumMembersAsync(
+            Document document,
+            EnumDeclarationSyntax enumDeclaration,
+            CancellationToken cancellationToken)
+        {
+            SemanticModel semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+
+            SpecialType enumSpecialType = semanticModel.GetDeclaredSymbol(enumDeclaration).EnumUnderlyingType.SpecialType;
+
+            SeparatedSyntaxList<EnumMemberDeclarationSyntax> members = enumDeclaration.Members;
+
+            SeparatedSyntaxList<EnumMemberDeclarationSyntax> newMembers = members
+                .OrderBy(f => GetConstantValue(f, semanticModel, cancellationToken), EnumValueComparer.GetInstance(enumSpecialType))
+                .ToSeparatedSyntaxList();
+
+            if (AreSeparatedWithEmptyLine(members))
+            {
+                for (int i = 0; i < newMembers.Count; i++)
+                {
+                    newMembers = newMembers.ReplaceAt(i, newMembers[i].TrimLeadingTrivia());
+                }
+
+                for (int i = 0; i < newMembers.Count - 1; i++)
+                {
+                    SyntaxToken separator = newMembers.GetSeparator(i);
+
+                    newMembers = newMembers.ReplaceSeparator(
+                        separator,
+                        separator.TrimTrailingTrivia().AppendToTrailingTrivia(new SyntaxTrivia[] { NewLine(), NewLine() }));
+                }
+            }
+
+            if (newMembers.SeparatorCount == members.SeparatorCount - 1)
+            {
+                SyntaxNodeOrTokenList newMembersWithSeparators = newMembers.GetWithSeparators();
+
+                newMembersWithSeparators = newMembersWithSeparators.Add(CommaToken());
+
+                newMembers = newMembersWithSeparators.ToSeparatedSyntaxList<EnumMemberDeclarationSyntax>();
+            }
+
+            MemberDeclarationSyntax newNode = enumDeclaration
+                .WithMembers(newMembers)
+                .WithFormatterAnnotation();
+
+            return await document.ReplaceNodeAsync(enumDeclaration, newNode, cancellationToken).ConfigureAwait(false);
+        }
+
+        private static bool AreSeparatedWithEmptyLine(SeparatedSyntaxList<EnumMemberDeclarationSyntax> members)
+        {
+            int count = members.Count;
+
+            if (members.SeparatorCount < count - 1)
+                return false;
+
+            for (int i = 1; i < count; i++)
+            {
+                if (!members[i].GetLeadingTrivia().Any(SyntaxKind.EndOfLineTrivia))
+                    return false;
+            }
+
+            for (int i = 0; i < count - 1; i++)
+            {
+                if (!members.GetSeparator(i).TrailingTrivia.Any(SyntaxKind.EndOfLineTrivia))
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static object GetConstantValue(
+            EnumMemberDeclarationSyntax enumMemberDeclaration,
+            SemanticModel semanticModel,
+            CancellationToken cancellationToken)
+        {
+            return semanticModel.GetDeclaredSymbol(enumMemberDeclaration, cancellationToken)?.ConstantValue;
         }
     }
 }
---
+++ src/Tests/Analyzers.Tests/RCS1154SortEnumMembersTests.cs
@@ -0,0 +1,194 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Roslynator.CSharp.CodeFixes;
+using Xunit;
+
+namespace Roslynator.CSharp.Analysis.Tests
+{
+    public class RCS1154SortEnumMembersTests : AbstractCSharpCodeFixVerifier
+    {
+        public override DiagnosticDescriptor Descriptor { get; } = DiagnosticDescriptors.SortEnumMembers;
+
+        public override DiagnosticAnalyzer Analyzer { get; } = new SortEnumMembersAnalyzer();
+
+        public override CodeFixProvider FixProvider { get; } = new EnumDeclarationCodeFixProvider();
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.SortEnumMembers)]
+        public async Task Test()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+enum [|Foo|]
+{
+    B = 1,
+    A = 0,
+    D = 3,
+    C = 2
+}
+", @"
+enum Foo
+{
+    A = 0,
+    B = 1,
+    C = 2,
+    D = 3
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.SortEnumMembers)]
+        public async Task Test_TrailingSeparator()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+enum [|Foo|]
+{
+    B = 1,
+    A = 0,
+    D = 3,
+    C = 2,
+}
+", @"
+enum Foo
+{
+    A = 0,
+    B = 1,
+    C = 2,
+    D = 3,
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.SortEnumMembers)]
+        public async Task Test_EmptyLines()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+enum [|Foo|]
+{
+    B = 1,
+
+    A = 0,
+
+    D = 3,
+
+    C = 2
+}
+", @"
+enum Foo
+{
+    A = 0,
+
+    B = 1,
+
+    C = 2,
+
+    D = 3
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.SortEnumMembers)]
+        public async Task Test_WithComments()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+enum [|Foo|]
+{
+    /// <summary>B</summary>
+    B = 1,
+    /// <summary>A</summary>
+    A = 0,
+    /// <summary>D</summary>
+    D = 3,
+    /// <summary>C</summary>
+    C = 2,
+}
+", @"
+enum Foo
+{
+    /// <summary>A</summary>
+    A = 0,
+    /// <summary>B</summary>
+    B = 1,
+    /// <summary>C</summary>
+    C = 2,
+    /// <summary>D</summary>
+    D = 3,
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.SortEnumMembers)]
+        public async Task Test_Comments_EmptyLines()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+enum [|Foo|]
+{
+    /// <summary>B</summary>
+    B = 1,
+
+    /// <summary>A</summary>
+    A = 0,
+
+    /// <summary>D</summary>
+    D = 3,
+
+    /// <summary>C</summary>
+    C = 2
+}
+", @"
+enum Foo
+{
+    /// <summary>A</summary>
+    A = 0,
+
+    /// <summary>B</summary>
+    B = 1,
+
+    /// <summary>C</summary>
+    C = 2,
+
+    /// <summary>D</summary>
+    D = 3
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.SortEnumMembers)]
+        public async Task Test_Comments_EmptyLines_TrailingSeparator()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+enum [|Foo|]
+{
+    /// <summary>B</summary>
+    B = 1,
+
+    /// <summary>A</summary>
+    A = 0,
+
+    /// <summary>D</summary>
+    D = 3,
+
+    /// <summary>C</summary>
+    C = 2,
+}
+", @"
+enum Foo
+{
+    /// <summary>A</summary>
+    A = 0,
+
+    /// <summary>B</summary>
+    B = 1,
+
+    /// <summary>C</summary>
+    C = 2,
+
+    /// <summary>D</summary>
+    D = 3,
+}
+");
+        }
+    }
+}
