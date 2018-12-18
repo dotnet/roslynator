b4bd2ebea93f4f68c34cf853042d18d134b16b0e
Josef Pihrt <josef.pihrt@outlook.com>
2018-12-18 14:11:37 +0100

    Fix refactoring MergeIfStatements (RR0075).

---
+++ src/Tests/Refactorings.Tests/RR0075MergeIfStatementsTests.cs
@@ -0,0 +1,118 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Roslynator.CSharp.Refactorings.Tests
+{
+    public class RR0075MergeIfStatementsTests : AbstractCSharpCodeRefactoringVerifier
+    {
+        public override string RefactoringId { get; } = RefactoringIdentifiers.MergeIfStatements;
+
+        [Fact, Trait(Traits.Refactoring, RefactoringIdentifiers.MergeIfStatements)]
+        public async Task Test()
+        {
+            await VerifyRefactoringAsync(@"
+class C
+{
+    bool M()
+    {
+        bool f1 = false;
+        bool f2 = false;
+        bool f3 = false;
+        bool f4 = false;
+
+[|        if (f1)
+        {
+            return true;
+        }
+
+        if (f2)
+            return true;
+
+        if (f3)
+        {
+            return true;
+        }
+
+        if (f4)
+        {
+            return true;
+        }|]
+
+        return false;
+    }
+}
+", @"
+class C
+{
+    bool M()
+    {
+        bool f1 = false;
+        bool f2 = false;
+        bool f3 = false;
+        bool f4 = false;
+
+        if (f1 || f2 || f3 || f4)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
+", equivalenceKey: RefactoringId);
+        }
+
+        [Fact, Trait(Traits.Refactoring, RefactoringIdentifiers.MergeIfStatements)]
+        public async Task Test_SwitcSection()
+        {
+            await VerifyRefactoringAsync(@"
+class C
+{
+    bool M()
+    {
+        bool f1 = false;
+        bool f2 = false;
+
+        switch (0)
+        {
+            case 0:
+[|                if (f1)
+                    return false;
+
+                if (f2)
+                {
+                    return false;
+                }|]
+
+                break;
+        }
+
+        return true;
+    }
+}
+", @"
+class C
+{
+    bool M()
+    {
+        bool f1 = false;
+        bool f2 = false;
+
+        switch (0)
+        {
+            case 0:
+                if (f1 || f2)
+                    return false;
+
+                break;
+        }
+
+        return true;
+    }
+}
+", equivalenceKey: RefactoringId);
+        }
+    }
+}
