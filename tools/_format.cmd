d390cc457800127b934e359bba19e12aa3035ea7
Josef Pihrt <josef.pihrt@outlook.com>
2019-01-06 16:35:32 +0100

    Update roslynator scripts.

--- tools/_format.cmd
+++ tools/_format.cmd
@@ -1,8 +1,11 @@
 @echo off
 
-"C:\Program Files\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild" "..\src\CommandLine.sln" /t:Build /p:Configuration=Debug /v:m /m
+set _msbuildPath="C:\Program Files\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin"
+
+%_msbuildPath%\msbuild "..\src\CommandLine.sln" /t:Build /p:Configuration=Debug /v:m /m
 
 "..\src\CommandLine\bin\Debug\net461\roslynator" format "..\src\Roslynator.sln" ^
+ --msbuild-path %_msbuildPath% ^
  --verbosity d ^
  --file-log "roslynator.log" ^
  --file-log-verbosity diag ^
