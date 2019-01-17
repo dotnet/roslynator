26f916896b93f33d5afe623946f5d0bb7590dfb7
Josef Pihrt <josef.pihrt@outlook.com>
2019-01-17 19:55:12 +0100

    Add class NavigationAnnotation.

---
+++ src/Workspaces.Core/CodeActions/NavigationAnnotation.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+
+#pragma warning disable RCS1102
+
+namespace Roslynator.CodeActions
+{
+    internal class NavigationAnnotation
+    {
+        public const string Kind = "CodeAction_Navigation";
+
+        public static SyntaxAnnotation Annotation { get; } = new SyntaxAnnotation(Kind);
+    }
+}
