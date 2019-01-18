4e2b4e033c1280e020475467c07dee1c919c731f
Josef Pihrt <josef.pihrt@outlook.com>
2019-01-18 16:08:17 +0100

    Deduplicate NodeSymbolInfo.

--- src/Analyzers/CSharp/NodeSymbolInfo.cs
+++ src/Analyzers/CSharp/NodeSymbolInfo.cs
@@ -5,7 +5,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-namespace Roslynator.CSharp.Analysis.UnusedMember
+namespace Roslynator.CSharp
 {
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     internal readonly struct NodeSymbolInfo
