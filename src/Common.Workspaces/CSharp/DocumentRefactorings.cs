640616e88c5deb1f703344ddce53a3c79b041644
Josef Pihrt <josef.pihrt@outlook.com>
2019-01-26 20:27:07 +0100

    Merge CommonRefactorings with DocumentRefactorings.

--- src/Common.Workspaces/CSharp/DocumentRefactorings.cs
+++ src/Common.Workspaces/CSharp/DocumentRefactorings.cs
@@ -91,5 +91,39 @@ namespace Roslynator.CSharp
         {
             return RemoveAsyncAwait.RefactorAsync(document, asyncKeyword, cancellationToken);
         }
+
+        public static Task<Document> SwapBinaryOperandsAsync(
+            Document document,
+            BinaryExpressionSyntax binaryExpression,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            ExpressionSyntax left = binaryExpression.Left;
+            ExpressionSyntax right = binaryExpression.Right;
+            SyntaxToken token = binaryExpression.OperatorToken;
+
+            ExpressionSyntax newBinaryExpressions = binaryExpression.Update(
+                left: right.WithTriviaFrom(left),
+                operatorToken: Token(token.LeadingTrivia, GetOperatorTokenKind(token.Kind()), token.TrailingTrivia),
+                right: left.WithTriviaFrom(right));
+
+            return document.ReplaceNodeAsync(binaryExpression, newBinaryExpressions, cancellationToken);
+
+            SyntaxKind GetOperatorTokenKind(SyntaxKind kind)
+            {
+                switch (kind)
+                {
+                    case SyntaxKind.LessThanToken:
+                        return SyntaxKind.GreaterThanToken;
+                    case SyntaxKind.LessThanEqualsToken:
+                        return SyntaxKind.GreaterThanEqualsToken;
+                    case SyntaxKind.GreaterThanToken:
+                        return SyntaxKind.LessThanToken;
+                    case SyntaxKind.GreaterThanEqualsToken:
+                        return SyntaxKind.LessThanEqualsToken;
+                    default:
+                        return kind;
+                }
+            }
+        }
     }
 }
