528addada372a2e3db21ece524e66ade2ec59901
Josef Pihrt <josef.pihrt@outlook.com>
2019-02-21 14:13:25 +0100

    Improve code fix for CS0120.

---
+++ src/Tests/CodeFixes.Tests/CS0120ObjectReferenceIsRequiredForNonStaticMemberTests.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Xunit;
+
+namespace Roslynator.CSharp.CodeFixes.Tests
+{
+    public class CS0120ObjectReferenceIsRequiredForNonStaticMemberTests : AbstractCSharpCompilerDiagnosticFixVerifier
+    {
+        public override string DiagnosticId { get; } = CompilerDiagnosticIdentifiers.ObjectReferenceIsRequiredForNonStaticMember;
+
+        public override CodeFixProvider FixProvider { get; } = new ObjectReferenceIsRequiredForNonStaticMemberCodeFixProvider();
+
+        [Fact, Trait(Traits.CodeFix, CompilerDiagnosticIdentifiers.ObjectReferenceIsRequiredForNonStaticMember)]
+        public async Task Test_MakeContainingMethodNonStatic()
+        {
+            await VerifyFixAsync(@"
+class A
+{
+    public static void M()
+    {
+        int x = P;
+    }
+
+    public int P => 1;
+}
+", @"
+class A
+{
+    public void M()
+    {
+        int x = P;
+    }
+
+    public int P => 1;
+}
+", equivalenceKey: EquivalenceKey.Create(DiagnosticId, CodeFixIdentifiers.MakeMemberNonStatic));
+        }
+
+        [Fact, Trait(Traits.CodeFix, CompilerDiagnosticIdentifiers.ObjectReferenceIsRequiredForNonStaticMember)]
+        public async Task TestNoFix()
+        {
+            await VerifyNoFixAsync(@"
+class A
+{
+    public void M()
+    {
+        int x = B.P;
+    }
+}
+
+class B
+{
+    public int P => 1;
+}
+", equivalenceKey: EquivalenceKey.Create(DiagnosticId));
+        }
+    }
+}
