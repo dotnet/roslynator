b72fff80961c868f81460c7566f3d3dc0ecb7b1c
Josef Pihrt <josef.pihrt@outlook.com>
2019-03-27 19:46:56 +0100

    Adjust scripts to install/uninstall vsix.

--- tools/vsix.debug.install.cmd
+++ tools/vsix.debug.install.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-"C:\Program Files\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller" ^
+"C:\Program Files\Microsoft Visual Studio\2019\Preview\Common7\IDE\VSIXInstaller" ^
  /q ^
  "..\src\VisualStudio\bin\Debug\Roslynator.VisualStudio.vsix"
 
