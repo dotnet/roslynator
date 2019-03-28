220660875ed9da10c59df64ea8e1a4e55899f70b
Danghor <danghor+github.com@gmail.com>
2019-03-28 21:27:30 +0100

    Add tests for RCS1043 and remove old test file

--- src/Tests/Analyzers.Tests/RCS1043RemovePartialModifierFromTypeWithSinglePartTests.cs
+++ src/Tests/Analyzers.Tests/RCS1043RemovePartialModifierFromTypeWithSinglePartTests.cs
@@ -1,24 +1,27 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Roslynator.CSharp.CodeFixes;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Roslynator.CSharp.Analysis.Tests
 {
     public class RCS1043RemovePartialModifierFromTypeWithSinglePartTests : AbstractCSharpFixVerifier
     {
-        public override DiagnosticDescriptor Descriptor { get; } = DiagnosticDescriptors.RemovePartialModifierFromTypeWithSinglePart;
+        public override DiagnosticDescriptor Descriptor { get; } =
+            DiagnosticDescriptors.RemovePartialModifierFromTypeWithSinglePart;
 
-        public override DiagnosticAnalyzer Analyzer { get; } = new RemovePartialModifierFromTypeWithSinglePartAnalyzer();
+        public override DiagnosticAnalyzer Analyzer { get; } =
+            new RemovePartialModifierFromTypeWithSinglePartAnalyzer();
 
-        public override CodeFixProvider FixProvider { get; } = new RemovePartialModifierFromTypeWithSinglePartCodeFixProvider();
+        public override CodeFixProvider FixProvider { get; } =
+            new RemovePartialModifierFromTypeWithSinglePartCodeFixProvider();
 
         [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.RemovePartialModifierFromTypeWithSinglePart)]
-        public async Task Test()
+        public async Task Test_Class()
         {
             await VerifyDiagnosticAndFixAsync(@"
 public [|partial|] class Foo
@@ -27,6 +30,82 @@ public [|partial|] class Foo
 ", @"
 public class Foo
 {
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.RemovePartialModifierFromTypeWithSinglePart)]
+        public async Task Test_Interface()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+public [|partial|] interface Foo
+{
+}
+", @"
+public interface Foo
+{
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.RemovePartialModifierFromTypeWithSinglePart)]
+        public async Task Test_Struct()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+public [|partial|] struct Foo
+{
+}
+", @"
+public struct Foo
+{
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.RemovePartialModifierFromTypeWithSinglePart)]
+        public async Task Test_MultipleMethodsNestedInClass()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+public [|partial|] class Foo
+{
+    partial void FooMethod();
+
+    partial void FooMethod()
+    {
+    }
+}
+", @"
+public class Foo
+{
+    partial void FooMethod();
+
+    partial void FooMethod()
+    {
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.RemovePartialModifierFromTypeWithSinglePart)]
+        public async Task Test_MultipleMethodsNestedInStruct()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+public [|partial|] struct Foo
+{
+    partial void FooMethod();
+
+    partial void FooMethod()
+    {
+    }
+}
+", @"
+public struct Foo
+{
+    partial void FooMethod();
+
+    partial void FooMethod()
+    {
+    }
 }
 ");
         }
@@ -42,15 +121,97 @@ public class Foo
         }
 
         [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.RemovePartialModifierFromTypeWithSinglePart)]
-        public async Task TestNoDiagnostic_PartialModifierWithMultipleParts()
+        public async Task TestNoDiagnostic_MultipleClassesNestedInStaticClass()
         {
             await VerifyNoDiagnosticAsync(@"
-public partial class Foo
+public static class A
 {
+    public partial class Foo
+    {
+    }
+    
+    public partial class Foo
+    {
+    }  
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.RemovePartialModifierFromTypeWithSinglePart)]
+        public async Task TestNoDiagnostic_MultipleClassesNestedInStruct()
+        {
+            await VerifyNoDiagnosticAsync(@"
+public struct A
+{
+    public partial class Foo
+    {
+    }
+    
+    public partial class Foo
+    {
+    }  
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.RemovePartialModifierFromTypeWithSinglePart)]
+        public async Task TestNoDiagnostic_MultipleInterfacesNestedInStaticClass()
+        {
+            await VerifyNoDiagnosticAsync(@"
+public static class A
+{
+    public partial interface Foo
+    {
+    }
+    
+    public partial interface Foo
+    {
+    }  
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.RemovePartialModifierFromTypeWithSinglePart)]
+        public async Task TestNoDiagnostic_MultipleInterfacesNestedInStruct()
+        {
+            await VerifyNoDiagnosticAsync(@"
+public struct A
+{
+    public partial interface Foo
+    {
+    }
+    
+    public partial interface Foo
+    {
+    }  
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.RemovePartialModifierFromTypeWithSinglePart)]
+        public async Task TestNoDiagnostic_MultipleNestedClassesWithSameName()
+        {
+            await VerifyNoDiagnosticAsync(@"
+public static class Foo
+{
+    private partial class Foo2
+    {
+    }
+
+    private partial class Foo2
+    {
+    }
 }
 
-public partial class Foo
+public struct FooStruct
 {
+    private partial class Foo2
+    {
+    }
+
+    private partial class Foo2
+    {
+    }
 }
 ");
         }
