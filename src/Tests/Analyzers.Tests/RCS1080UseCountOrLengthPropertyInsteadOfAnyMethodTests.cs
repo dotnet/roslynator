c09a1ff65e946f7c7272f00e1aa19cf8ffc11fa2
Danghor <danghor+github.com@gmail.com>
2019-03-29 19:31:33 +0100

    Add tests for RCS1080

--- src/Tests/Analyzers.Tests/RCS1080UseCountOrLengthPropertyInsteadOfAnyMethodTests.cs
+++ src/Tests/Analyzers.Tests/RCS1080UseCountOrLengthPropertyInsteadOfAnyMethodTests.cs
@@ -18,7 +18,7 @@ namespace Roslynator.CSharp.Analysis.Tests
         public override CodeFixProvider FixProvider { get; } = new InvocationExpressionCodeFixProvider();
 
         [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
-        public async Task Test()
+        public async Task Test_List()
         {
             await VerifyDiagnosticAndFixAsync(@"
 using System.Collections.Generic;
@@ -52,7 +52,7 @@ class C
         }
 
         [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
-        public async Task Test_LogicalNot()
+        public async Task Test_ListNot()
         {
             await VerifyDiagnosticAndFixAsync(@"
 using System.Collections.Generic;
@@ -80,6 +80,507 @@ class C
 
         bool any = items.Count == 0 //x
             ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task Test_IList()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        IList<object> items = null;
+
+        bool any = items.[|Any()|] //x
+            ;
+    }
+}
+", @"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        IList<object> items = null;
+
+        bool any = items.Count > 0 //x
+            ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task Test_IListNot()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        IList<object> items = null;
+
+        bool any = !items.[|Any()|] //x
+            ;
+    }
+}
+", @"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        IList<object> items = null;
+
+        bool any = items.Count == 0 //x
+            ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task Test_IReadOnlyList()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        IReadOnlyList<object> items = null;
+
+        bool any = items.[|Any()|] //x
+            ;
+    }
+}
+", @"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        IReadOnlyList<object> items = null;
+
+        bool any = items.Count > 0 //x
+            ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task Test_IReadOnlyListNot()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        IReadOnlyList<object> items = null;
+
+        bool any = !items.[|Any()|] //x
+            ;
+    }
+}
+", @"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        IReadOnlyList<object> items = null;
+
+        bool any = items.Count == 0 //x
+            ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task Test_Collection()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System.Collections.ObjectModel;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        Collection<object> items = null;
+
+        bool any = items.[|Any()|] //x
+            ;
+    }
+}
+", @"
+using System.Collections.ObjectModel;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        Collection<object> items = null;
+
+        bool any = items.Count > 0 //x
+            ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task Test_CollectionNot()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System.Collections.ObjectModel;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        Collection<object> items = null;
+
+        bool any = !items.[|Any()|] //x
+            ;
+    }
+}
+", @"
+using System.Collections.ObjectModel;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        Collection<object> items = null;
+
+        bool any = items.Count == 0 //x
+            ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task Test_ICollection()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        ICollection<object> items = null;
+
+        bool any = items.[|Any()|] //x
+            ;
+    }
+}
+", @"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        ICollection<object> items = null;
+
+        bool any = items.Count > 0 //x
+            ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task Test_ICollectionNot()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        ICollection<object> items = null;
+
+        bool any = !items.[|Any()|] //x
+            ;
+    }
+}
+", @"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        ICollection<object> items = null;
+
+        bool any = items.Count == 0 //x
+            ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task Test_IReadOnlyCollection()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        IReadOnlyCollection<object> items = null;
+
+        bool any = items.[|Any()|] //x
+            ;
+    }
+}
+", @"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        IReadOnlyCollection<object> items = null;
+
+        bool any = items.Count > 0 //x
+            ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task Test_IReadOnlyCollectionNot()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        IReadOnlyCollection<object> items = null;
+
+        bool any = !items.[|Any()|] //x
+            ;
+    }
+}
+", @"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        IReadOnlyCollection<object> items = null;
+
+        bool any = items.Count == 0 //x
+            ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task Test_Array()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        object[] items = null;
+
+        bool any = items.[|Any()|] //x
+            ;
+    }
+}
+", @"
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        object[] items = null;
+
+        bool any = items.Length > 0 //x
+            ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task Test_ArrayNot()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        object[] items = null;
+
+        bool any = !items.[|Any()|] //x
+            ;
+    }
+}
+", @"
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        object[] items = null;
+
+        bool any = items.Length == 0 //x
+            ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task Test_String()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        string items = null;
+
+        bool any = items.[|Any()|] //x
+            ;
+    }
+}
+", @"
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        string items = null;
+
+        bool any = items.Length > 0 //x
+            ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task Test_StringNot()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        string items = null;
+
+        bool any = !items.[|Any()|] //x
+            ;
+    }
+}
+", @"
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        string items = null;
+
+        bool any = items.Length == 0 //x
+            ;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task TestNoDiagnostic_ImmutableArray()
+        {
+            await VerifyNoDiagnosticAsync(@"
+using System.Collections.Immutable;
+
+class C
+{
+    void M()
+    {
+        ImmutableArray<object> items = ImmutableArray<object>.Empty;
+    }
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseCountOrLengthPropertyInsteadOfAnyMethod)]
+        public async Task TestNoDiagnostic_IEnumerable()
+        {
+            await VerifyNoDiagnosticAsync(@"
+using System.Collections.Generic;
+using System.Linq;
+
+class C
+{
+    void M()
+    {
+        IEnumerable<object> items = null;
     }
 }
 ");
