ce91de7497e6d4e9ee5bb6b359bca5a2e68d4cb7
Josef Pihrt <josef.pihrt@outlook.com>
2019-04-20 20:44:40 +0200

    Refactor OptimizeStringBuilderAppendCallAnalysis (RCS1197).

---
+++ src/Analyzers/CSharp/Analysis/AvoidBoxingOfValueTypeAnalysis.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Roslynator.CSharp.Syntax;
+
+namespace Roslynator.CSharp.Analysis
+{
+    internal static class AvoidBoxingOfValueTypeAnalysis
+    {
+        public static void Analyze(SyntaxNodeAnalysisContext context, in SimpleMemberInvocationExpressionInfo invocationInfo)
+        {
+            IMethodSymbol methodSymbol = context.SemanticModel.GetMethodSymbol(invocationInfo.InvocationExpression, context.CancellationToken);
+
+            if (methodSymbol == null)
+                return;
+
+            if (methodSymbol.IsExtensionMethod)
+                return;
+
+            if (methodSymbol.ContainingType?.HasMetadataName(MetadataNames.System_Text_StringBuilder) != true)
+                return;
+
+            ImmutableArray<IParameterSymbol> parameters = methodSymbol.Parameters;
+
+            switch (parameters.Length)
+            {
+                case 1:
+                    {
+                        if (methodSymbol.IsName("Append", "AppendLine"))
+                        {
+                            ArgumentSyntax argument = invocationInfo.Arguments.SingleOrDefault(shouldThrow: false);
+
+                            if (argument != null)
+                            {
+                                ExpressionSyntax expression = argument.Expression;
+
+                                switch (expression.Kind())
+                                {
+                                    case SyntaxKind.InterpolatedStringExpression:
+                                    case SyntaxKind.AddExpression:
+                                        {
+                                            return;
+                                        }
+                                    default:
+                                        {
+                                            if (methodSymbol.IsName("Append")
+                                                && parameters[0].Type.IsObject()
+                                                && context.SemanticModel.GetTypeSymbol(expression, context.CancellationToken).IsValueType)
+                                            {
+                                                DiagnosticHelpers.ReportDiagnostic(context, DiagnosticDescriptors.AvoidBoxingOfValueType, argument);
+                                                return;
+                                            }
+
+                                            break;
+                                        }
+                                }
+                            }
+                        }
+
+                        break;
+                    }
+
+                case 2:
+                    {
+                        if (methodSymbol.IsName("Insert")
+                            && parameters[0].Type.SpecialType == SpecialType.System_Int32
+                            && parameters[1].Type.SpecialType == SpecialType.System_Object)
+                        {
+                            SeparatedSyntaxList<ArgumentSyntax> arguments = invocationInfo.Arguments;
+
+                            if (arguments.Count == 2
+                                && context.SemanticModel
+                                    .GetTypeSymbol(arguments[1].Expression, context.CancellationToken)
+                                    .IsValueType)
+                            {
+                                DiagnosticHelpers.ReportDiagnostic(context, DiagnosticDescriptors.AvoidBoxingOfValueType, arguments[1]);
+                            }
+                        }
+
+                        break;
+                    }
+            }
+        }
+    }
+}
--- src/Analyzers/CSharp/Analysis/OptimizeStringBuilderAppendCallAnalysis.cs
+++ src/Analyzers/CSharp/Analysis/OptimizeStringBuilderAppendCallAnalysis.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Immutable;
-using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -14,9 +13,7 @@ namespace Roslynator.CSharp.Analysis
     {
         public static void Analyze(SyntaxNodeAnalysisContext context, in SimpleMemberInvocationExpressionInfo invocationInfo)
         {
-            InvocationExpressionSyntax invocationExpression = invocationInfo.InvocationExpression;
-
-            IMethodSymbol methodSymbol = context.SemanticModel.GetMethodSymbol(invocationExpression, context.CancellationToken);
+            IMethodSymbol methodSymbol = context.SemanticModel.GetMethodSymbol(invocationInfo.InvocationExpression, context.CancellationToken);
 
             if (methodSymbol == null)
                 return;
@@ -27,147 +24,142 @@ namespace Roslynator.CSharp.Analysis
             if (methodSymbol.ContainingType?.HasMetadataName(MetadataNames.System_Text_StringBuilder) != true)
                 return;
 
-            ImmutableArray<IParameterSymbol> parameters = methodSymbol.Parameters;
-
-            int parameterCount = parameters.Length;
+            int parameterCount = methodSymbol.Parameters.Length;
 
             if (parameterCount == 0)
             {
-                if (methodSymbol.IsName("AppendLine"))
-                {
-                    SimpleMemberInvocationExpressionInfo invocationInfo2 = SyntaxInfo.SimpleMemberInvocationExpressionInfo(invocationInfo.Expression);
+                if (!methodSymbol.IsName("AppendLine"))
+                    return;
 
-                    if (invocationInfo2.Success
-                        && invocationInfo2.NameText == "Append"
-                        && invocationInfo2.Arguments.Count == 1)
-                    {
-                        IMethodSymbol methodInfo2 = context.SemanticModel.GetMethodSymbol(invocationInfo2.InvocationExpression, context.CancellationToken);
+                SimpleMemberInvocationExpressionInfo invocationInfo2 = SyntaxInfo.SimpleMemberInvocationExpressionInfo(invocationInfo.Expression);
 
-                        if (methodInfo2?.IsStatic == false
-                            && methodInfo2.ContainingType?.HasMetadataName(MetadataNames.System_Text_StringBuilder) == true
-                            && methodInfo2.HasSingleParameter(SpecialType.System_String))
-                        {
-                            DiagnosticHelpers.ReportDiagnostic(context, DiagnosticDescriptors.OptimizeStringBuilderAppendCall, invocationInfo.Name, methodSymbol.Name);
-                        }
-                    }
-                }
+                if (invocationInfo2.NameText != "Append")
+                    return;
+
+                if (invocationInfo2.Arguments.Count != 1)
+                    return;
+
+                IMethodSymbol methodInfo2 = context.SemanticModel.GetMethodSymbol(invocationInfo2.InvocationExpression, context.CancellationToken);
+
+                if (methodInfo2?.IsStatic != false)
+                    return;
+
+                if (methodInfo2.ContainingType?.HasMetadataName(MetadataNames.System_Text_StringBuilder) != true)
+                    return;
+
+                if (!methodInfo2.HasSingleParameter(SpecialType.System_String))
+                    return;
+
+                ReportDiagnostic(invocationInfo.Name);
             }
             else if (parameterCount == 1)
             {
-                if (methodSymbol.IsName("Append", "AppendLine"))
+                if (!methodSymbol.IsName("Append", "AppendLine"))
+                    return;
+
+                ArgumentSyntax argument = invocationInfo.Arguments.SingleOrDefault(shouldThrow: false);
+
+                if (argument == null)
+                    return;
+
+                ExpressionSyntax expression = argument.Expression;
+
+                SyntaxKind expressionKind = expression.Kind();
+
+                switch (expressionKind)
                 {
-                    ArgumentSyntax argument = invocationInfo.Arguments.SingleOrDefault(shouldThrow: false);
-
-                    if (argument != null)
-                    {
-                        ExpressionSyntax expression = argument.Expression;
-
-                        SyntaxKind expressionKind = expression.Kind();
-
-                        switch (expressionKind)
+                    case SyntaxKind.InterpolatedStringExpression:
                         {
-                            case SyntaxKind.InterpolatedStringExpression:
-                                {
-                                    DiagnosticHelpers.ReportDiagnostic(context, DiagnosticDescriptors.OptimizeStringBuilderAppendCall, argument, methodSymbol.Name);
-                                    return;
-                                }
-                            case SyntaxKind.AddExpression:
-                                {
-                                    BinaryExpressionInfo binaryExpressionInfo = SyntaxInfo.BinaryExpressionInfo((BinaryExpressionSyntax)expression);
-
-                                    if (binaryExpressionInfo.Success
-                                        && binaryExpressionInfo.AsChain().Reverse().IsStringConcatenation(context.SemanticModel, context.CancellationToken))
-                                    {
-                                        DiagnosticHelpers.ReportDiagnostic(context, DiagnosticDescriptors.OptimizeStringBuilderAppendCall, argument, methodSymbol.Name);
-                                        return;
-                                    }
-
-                                    break;
-                                }
-                            default:
-                                {
-                                    if (expressionKind == SyntaxKind.InvocationExpression
-                                        && IsFixable((InvocationExpressionSyntax)expression, context.SemanticModel, context.CancellationToken))
-                                    {
-                                        DiagnosticHelpers.ReportDiagnostic(context, DiagnosticDescriptors.OptimizeStringBuilderAppendCall, argument, methodSymbol.Name);
-                                        return;
-                                    }
-
-                                    if (methodSymbol.IsName("Append")
-                                        && parameterCount == 1
-                                        && parameters[0].Type.IsObject()
-                                        && context.SemanticModel.GetTypeSymbol(argument.Expression, context.CancellationToken).IsValueType)
-                                    {
-                                        DiagnosticHelpers.ReportDiagnostic(context, DiagnosticDescriptors.AvoidBoxingOfValueType, argument);
-                                        return;
-                                    }
-
-                                    break;
-                                }
+                            ReportDiagnostic(argument);
+                            return;
                         }
-                    }
+                    case SyntaxKind.AddExpression:
+                        {
+                            BinaryExpressionInfo binaryExpressionInfo = SyntaxInfo.BinaryExpressionInfo((BinaryExpressionSyntax)expression);
+
+                            if (binaryExpressionInfo.Success
+                                && binaryExpressionInfo.AsChain().Reverse().IsStringConcatenation(context.SemanticModel, context.CancellationToken))
+                            {
+                                ReportDiagnostic(argument);
+                            }
+
+                            return;
+                        }
                 }
-            }
-            else if (parameterCount == 2)
-            {
-                if (methodSymbol.IsName("Insert")
-                    && parameters[0].Type.SpecialType == SpecialType.System_Int32
-                    && parameters[1].Type.SpecialType == SpecialType.System_Object)
+
+                if (expressionKind != SyntaxKind.InvocationExpression)
+                    return;
+
+                var invocationExpression = (InvocationExpressionSyntax)expression;
+
+                SimpleMemberInvocationExpressionInfo invocationInfo2 = SyntaxInfo.SimpleMemberInvocationExpressionInfo(invocationExpression);
+
+                if (!invocationInfo2.Success)
+                    return;
+
+                IMethodSymbol methodSymbol2 = context.SemanticModel.GetMethodSymbol(invocationInfo2.InvocationExpression, context.CancellationToken);
+
+                if (methodSymbol2 == null)
+                    return;
+
+                if (!methodSymbol2.IsContainingType(SpecialType.System_String))
+                    return;
+
+                if (!methodSymbol2.IsReturnType(SpecialType.System_String))
+                    return;
+
+                switch (methodSymbol2.Name)
                 {
-                    SeparatedSyntaxList<ArgumentSyntax> arguments = invocationInfo.Arguments;
+                    case "Substring":
+                        {
+                            ImmutableArray<IParameterSymbol> parameters = methodSymbol2.Parameters;
 
-                    if (arguments.Count == 2
-                        && context.SemanticModel
-                            .GetTypeSymbol(arguments[1].Expression, context.CancellationToken)
-                            .IsValueType)
-                    {
-                        DiagnosticHelpers.ReportDiagnostic(context, DiagnosticDescriptors.AvoidBoxingOfValueType, arguments[1]);
-                    }
+                            switch (parameters.Length)
+                            {
+                                case 1:
+                                    {
+                                        if (parameters[0].Type.SpecialType == SpecialType.System_Int32
+                                            && invocationInfo2.Expression.IsKind(SyntaxKind.IdentifierName)
+                                            && context.SemanticModel.GetSymbol(invocationInfo2.Expression, context.CancellationToken).IsKind(SymbolKind.Field, SymbolKind.Local, SymbolKind.Parameter))
+                                        {
+                                            ReportDiagnostic(argument);
+                                        }
+
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        if (parameters[0].Type.SpecialType == SpecialType.System_Int32
+                                            && parameters[1].Type.SpecialType == SpecialType.System_Int32)
+                                        {
+                                            ReportDiagnostic(argument);
+                                        }
+
+                                        break;
+                                    }
+                            }
+
+                            break;
+                        }
+                    case "Remove":
+                        {
+                            if (methodSymbol2.HasSingleParameter(SpecialType.System_Int32))
+                                ReportDiagnostic(argument);
+
+                            break;
+                        }
+                    case "Format":
+                        {
+                            ReportDiagnostic(argument);
+                            break;
+                        }
                 }
             }
-        }
 
-        private static bool IsFixable(InvocationExpressionSyntax invocationExpression, SemanticModel semanticModel, CancellationToken cancellationToken)
-        {
-            SimpleMemberInvocationExpressionInfo invocationInfo = SyntaxInfo.SimpleMemberInvocationExpressionInfo(invocationExpression);
-
-            if (!invocationInfo.Success)
-                return false;
-
-            IMethodSymbol methodSymbol = semanticModel.GetMethodSymbol(invocationInfo.InvocationExpression, cancellationToken);
-
-            if (methodSymbol == null)
-                return false;
-
-            if (!methodSymbol.IsContainingType(SpecialType.System_String))
-                return false;
-
-            if (!methodSymbol.IsReturnType(SpecialType.System_String))
-                return false;
-
-            switch (methodSymbol.Name)
+            void ReportDiagnostic(SyntaxNode node)
             {
-                case "Substring":
-                    {
-                        if (methodSymbol.HasTwoParameters(SpecialType.System_Int32, SpecialType.System_Int32))
-                            return true;
-
-                        break;
-                    }
-                case "Remove":
-                    {
-                        if (methodSymbol.HasSingleParameter(SpecialType.System_Int32))
-                            return true;
-
-                        break;
-                    }
-                case "Format":
-                    {
-                        return true;
-                    }
+                DiagnosticHelpers.ReportDiagnostic(context, DiagnosticDescriptors.OptimizeStringBuilderAppendCall, node, methodSymbol.Name);
             }
-
-            return false;
         }
     }
 }
