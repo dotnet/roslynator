d4b487c02d124e638a79398d3d8d98dc6bf2b657
Josef Pihrt <josef.pihrt@outlook.com>
2019-05-01 20:52:10 +0200

    Add well known diagnostic ID prefix 'SA'.

--- src/Core/DiagnosticIdPrefix.cs
+++ src/Core/DiagnosticIdPrefix.cs
@@ -98,6 +98,15 @@ namespace Roslynator
 
                         break;
                     }
+                case 'S':
+                    {
+                        if (HasPrefix(SA))
+                        {
+                            return SA;
+                        }
+
+                        break;
+                    }
                 case 'U':
                     {
                         if (HasPrefix(U2U))
