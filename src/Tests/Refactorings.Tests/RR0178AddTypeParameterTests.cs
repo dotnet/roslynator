38e72fb194e4ba00d7ca3a59dfd57d020834de3a
Josef Pihrt <josef.pihrt@outlook.com>
2019-08-11 18:10:05 +0200

    Fix refactoring AddTypeParameter (RR0178).

---
+++ src/Tests/Refactorings.Tests/RR0178AddTypeParameterTests.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Roslynator.Tests;
+using Xunit;
+
+namespace Roslynator.CSharp.Refactorings.Tests
+{
+    public class RR0178AddTypeParameterTests : AbstractCSharpRefactoringVerifier
+    {
+        public override string RefactoringId { get; } = RefactoringIdentifiers.AddTypeParameter;
+
+        [Fact, Trait(Traits.Refactoring, RefactoringIdentifiers.AddTypeParameter)]
+        public async Task Test()
+        {
+            await VerifyRefactoringAsync(@"
+class C[||]
+{
+}
+", @"
+class C<T>
+{
+}
+", equivalenceKey: RefactoringId);
+        }
+
+        [Fact, Trait(Traits.Refactoring, RefactoringIdentifiers.AddTypeParameter)]
+        public async Task TestNoRefactoring_SpanOnNextLine()
+        {
+            await VerifyNoRefactoringAsync(@"
+class C
+[||]{
+}
+", equivalenceKey: RefactoringId);
+        }
+    }
+}
