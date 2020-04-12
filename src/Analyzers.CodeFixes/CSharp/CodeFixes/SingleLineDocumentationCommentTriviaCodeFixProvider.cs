18213a3923b3c8584f5514a86a80cf1667847ba4
Josef Pihrt <josef.pihrt@outlook.com>
2020-04-12 20:23:17 +0200

    Fix indentation (RCS1101) (fix #663)

--- src/Analyzers.CodeFixes/CSharp/CodeFixes/SingleLineDocumentationCommentTriviaCodeFixProvider.cs
+++ src/Analyzers.CodeFixes/CSharp/CodeFixes/SingleLineDocumentationCommentTriviaCodeFixProvider.cs
@@ -1,15 +1,20 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Immutable;
 using System.Composition;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
 using Roslynator.CodeFixes;
-using Roslynator.CSharp.Refactorings;
 using Roslynator.CSharp.Refactorings.Documentation;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace Roslynator.CSharp.CodeFixes
 {
@@ -17,6 +22,28 @@ namespace Roslynator.CSharp.CodeFixes
     [Shared]
     public class SingleLineDocumentationCommentTriviaCodeFixProvider : BaseCodeFixProvider
     {
+        private static readonly Regex _formatSummaryOnSingleLineRegex = new Regex(
+            @"
+            ^
+            (
+                [\s-[\r\n]]*
+                \r?\n
+                [\s-[\r\n]]*
+                ///
+                [\s-[\r\n]]*
+            )?
+            (?<1>[^\r\n]*)
+            (
+                [\s-[\r\n]]*
+                \r?\n
+                [\s-[\r\n]]*
+                ///
+                [\s-[\r\n]]*
+            )?
+            $
+            ",
+            RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture);
+
         public sealed override ImmutableArray<string> FixableDiagnosticIds
         {
             get
@@ -44,7 +71,7 @@ namespace Roslynator.CSharp.CodeFixes
                         {
                             CodeAction codeAction = CodeAction.Create(
                                 "Format summary on a single line",
-                                cancellationToken => FormatSummaryOnSingleLineRefactoring.RefactorAsync(context.Document, documentationComment, cancellationToken),
+                                ct => FormatSummaryOnSingleLineAsync(context.Document, documentationComment, ct),
                                 GetEquivalenceKey(diagnostic));
 
                             context.RegisterCodeFix(codeAction, diagnostic);
@@ -54,7 +81,7 @@ namespace Roslynator.CSharp.CodeFixes
                         {
                             CodeAction codeAction = CodeAction.Create(
                                 "Format summary on multiple lines",
-                                cancellationToken => FormatSummaryOnMultipleLinesRefactoring.RefactorAsync(context.Document, documentationComment, cancellationToken),
+                                ct => FormatSummaryOnMultipleLinesAsync(context.Document, documentationComment, ct),
                                 GetEquivalenceKey(diagnostic));
 
                             context.RegisterCodeFix(codeAction, diagnostic);
@@ -86,6 +113,73 @@ namespace Roslynator.CSharp.CodeFixes
                         }
                 }
             }
+        }
+
+        private static async Task<Document> FormatSummaryOnSingleLineAsync(
+            Document document,
+            DocumentationCommentTriviaSyntax documentationComment,
+            CancellationToken cancellationToken)
+        {
+            SourceText sourceText = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
+
+            XmlElementSyntax summaryElement = documentationComment.SummaryElement();
+
+            XmlElementStartTagSyntax startTag = summaryElement.StartTag;
+            XmlElementEndTagSyntax endTag = summaryElement.EndTag;
+
+            Match match = _formatSummaryOnSingleLineRegex.Match(
+                summaryElement.ToString(),
+                startTag.Span.End - summaryElement.SpanStart,
+                endTag.SpanStart - startTag.Span.End);
+
+            var textChange = new TextChange(
+                new TextSpan(startTag.Span.End, match.Length),
+                match.Groups[1].Value);
+
+            SourceText newSourceText = sourceText.WithChanges(textChange);
+
+            return document.WithText(newSourceText);
+        }
+
+        private static async Task<Document> FormatSummaryOnMultipleLinesAsync(
+            Document document,
+            DocumentationCommentTriviaSyntax documentationComment,
+            CancellationToken cancellationToken)
+        {
+            SourceText sourceText = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
+
+            XmlElementSyntax summaryElement = documentationComment.SummaryElement();
+
+            string indentation = "";
+
+            SyntaxTrivia parentTrivia = documentationComment.ParentTrivia;
+
+            SyntaxToken token = parentTrivia.Token;
+
+            SyntaxTriviaList leadingTrivia = token.LeadingTrivia;
+
+            int index = leadingTrivia.IndexOf(parentTrivia);
+
+            if (index > 0)
+            {
+                SyntaxTrivia previousTrivia = token.LeadingTrivia[index - 1];
+
+                if (previousTrivia.IsWhitespaceTrivia())
+                    indentation = previousTrivia.ToString();
+            }
+
+            string endOfLine = documentationComment.DescendantTokens().FirstOrDefault(f => f.IsKind(SyntaxKind.XmlTextLiteralNewLineToken)).ToString();
+
+            if (endOfLine.Length == 0)
+                endOfLine = Environment.NewLine;
+
+            string startOfLine = endOfLine + indentation + "/// ";
+
+            SourceText newSourceText = sourceText.WithChanges(
+                new TextChange(new TextSpan(summaryElement.StartTag.Span.End, 0), startOfLine),
+                new TextChange(new TextSpan(summaryElement.EndTag.SpanStart, 0), startOfLine));
+
+            return document.WithText(newSourceText);
         }
     }
 }
---
+++ src/Tests/Analyzers.Tests/RCS1101FormatDocumentationSummaryOnMultipleLinesTests.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Roslynator.CSharp.CodeFixes;
+using Xunit;
+
+namespace Roslynator.CSharp.Analysis.Tests
+{
+    public class RCS1101FormatDocumentationSummaryOnMultipleLinesTests : AbstractCSharpFixVerifier
+    {
+        public override DiagnosticDescriptor Descriptor { get; } = DiagnosticDescriptors.FormatDocumentationSummaryOnMultipleLines;
+
+        public override DiagnosticAnalyzer Analyzer { get; } = new FormatSummaryOnMultipleLinesAnalyzer();
+
+        public override CodeFixProvider FixProvider { get; } = new SingleLineDocumentationCommentTriviaCodeFixProvider();
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.FormatDocumentationSummaryOnMultipleLines)]
+        public async Task Test()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+/// [|<summary>a<code>b</code>c</summary>|]
+class C
+{
+}
+", @"
+/// <summary>
+/// a<code>b</code>c
+/// </summary>
+class C
+{
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.FormatDocumentationSummaryOnMultipleLines)]
+        public async Task Test_EmptySummary()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+/// [|<summary></summary>|]
+class C
+{
+}
+", @"
+/// <summary>
+/// 
+/// </summary>
+class C
+{
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.FormatDocumentationSummaryOnMultipleLines)]
+        public async Task Test_Tab()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+class C
+{
+	/// [|<summary>x</summary>|]
+	void M()
+	{
+	}
+}
+", @"
+class C
+{
+	/// <summary>
+	/// x
+	/// </summary>
+	void M()
+	{
+	}
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.FormatDocumentationSummaryOnMultipleLines)]
+        public async Task TestNoDiagnostic()
+        {
+            await VerifyNoDiagnosticAsync(@"
+/// <summary>
+/// x
+/// </summary>
+class C
+{
+}
+");
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.FormatDocumentationSummaryOnMultipleLines)]
+        public async Task TestNoDiagnostic_EmptySummary()
+        {
+            await VerifyNoDiagnosticAsync(@"
+/// <summary>
+/// </summary>
+class C
+{
+}
+");
+        }
+    }
+}
