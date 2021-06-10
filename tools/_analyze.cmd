67c8a602a673ad8541e30f5e9077cded7f9586ca
Josef Pihrt <josef.pihrt@outlook.com>
2021-06-10 15:06:31 +0200

    Update script analyze.cmd

--- tools/_analyze.cmd
+++ tools/_analyze.cmd
@@ -9,14 +9,16 @@ set _msbuildPath="%_programFiles%\Microsoft Visual Studio\2019\Community\MSBuild
 
 "..\src\CommandLine\bin\Debug\net48\roslynator" analyze "..\src\Roslynator.sln" ^
  --msbuild-path %_msbuildPath% ^
- --analyzer-assemblies "..\src\Analyzers\bin\Debug\netstandard2.0\Roslynator.CSharp.Analyzers.dll" ^
+ --analyzer-assemblies ^
+  "..\src\Analyzers.CodeFixes\bin\Debug\netstandard2.0\Roslynator.CSharp.Analyzers.dll" ^
+  "..\src\CodeAnalysis.Analyzers.CodeFixes\bin\Debug\netstandard2.0\Roslynator.CodeAnalysis.Analyzers.dll" ^
+  "..\src\Formatting.Analyzers.CodeFixes\bin\Debug\netstandard2.0\Roslynator.Formatting.Analyzers.dll" ^
  --ignore-analyzer-references ^
  --ignored-diagnostics CS1591 ^
  --severity-level info ^
  --culture en ^
  --verbosity n ^
  --file-log "roslynator.log" ^
- --file-log-verbosity diag ^
- --output "diagnostics.xml"
+ --file-log-verbosity diag
 
 pause
