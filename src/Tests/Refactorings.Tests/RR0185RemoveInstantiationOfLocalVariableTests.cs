31714510e5bdbdc60ccac287ed0b68e257c0b977
Josef Pihrt <josef.pihrt@outlook.com>
2021-12-13 18:04:24 +0100

    Add nullable annotation if necessary (RR0185)

---
+++ src/Tests/Refactorings.Tests/RR0185RemoveInstantiationOfLocalVariableTests.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Josef Pihrt and Contributors. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Roslynator.Testing.CSharp;
+using Xunit;
+
+namespace Roslynator.CSharp.Refactorings.Tests
+{
+    public class RR0185RemoveInstantiationOfLocalVariableTests : AbstractCSharpRefactoringVerifier
+    {
+        public override string RefactoringId { get; } = RefactoringIdentifiers.RemoveInstantiationOfLocalVariable;
+
+        [Fact, Trait(Traits.Refactoring, RefactoringIdentifiers.RemoveInstantiationOfLocalVariable)]
+        public async Task Test_WithoutNullableContext()
+        {
+            await VerifyRefactoringAsync(@"
+class C
+{
+    void M()
+    {
+        var x = [||]new string(' ', 1);
+    }
+}
+", @"
+class C
+{
+    void M()
+    {
+        string x = null;
+    }
+}
+", equivalenceKey: EquivalenceKey.Create(RefactoringDescriptors.RemoveInstantiationOfLocalVariable));
+        }
+
+        [Fact, Trait(Traits.Refactoring, RefactoringIdentifiers.RemoveInstantiationOfLocalVariable)]
+        public async Task Test_WithNullableContext()
+        {
+            await VerifyRefactoringAsync(@"
+#nullable enable annotations
+
+class C
+{
+    void M()
+    {
+        var x = [||]new string(' ', 1);
+    }
+}
+", @"
+#nullable enable annotations
+
+class C
+{
+    void M()
+    {
+        string? x = null;
+    }
+}
+", equivalenceKey: EquivalenceKey.Create(RefactoringDescriptors.RemoveInstantiationOfLocalVariable));
+        }
+    }
+}
