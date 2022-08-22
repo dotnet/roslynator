c4bedaad8e881b4af6321bff057dabfa613d88a1
Josef Pihrt <josef.pihrt@outlook.com>
2022-08-22 15:14:29 +0200

    Add CSharpNameGenerator

--- src/Analyzers.CodeFixes/CSharp/CodeFixes/UsePatternMatchingInsteadOfIsAndCastCodeFixProvider.cs
+++ src/Analyzers.CodeFixes/CSharp/CodeFixes/UsePatternMatchingInsteadOfIsAndCastCodeFixProvider.cs
@@ -111,10 +111,7 @@ namespace Roslynator.CSharp.CodeFixes
 
             string name = NameGenerator.CreateName(typeSymbol, firstCharToLower: true) ?? DefaultNames.Variable;
 
-            name = NameGenerator.Default.EnsureUniqueLocalName(name, semanticModel, node.SpanStart, cancellationToken: cancellationToken) ?? DefaultNames.Variable;
-
-            if (SyntaxFacts.GetKeywordKind(name) != SyntaxKind.None)
-                name = "@" + name;
+            name = CSharpNameGenerator.Default.EnsureUniqueLocalName(name, semanticModel, node.SpanStart, cancellationToken: cancellationToken) ?? DefaultNames.Variable;
 
             IsPatternExpressionSyntax isPatternExpression = IsPatternExpression(
                 isInfo.Expression,
