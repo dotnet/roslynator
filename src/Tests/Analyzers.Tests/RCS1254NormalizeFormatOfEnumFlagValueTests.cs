dcf48a8762c9a2175989978392f137e03e22e29f
Josef Pihrt <josef.pihrt@outlook.com>
2022-11-17 12:01:48 +0100

    Update tests

--- src/Tests/Analyzers.Tests/RCS1254NormalizeFormatOfEnumFlagValueTests.cs
+++ src/Tests/Analyzers.Tests/RCS1254NormalizeFormatOfEnumFlagValueTests.cs
@@ -8,60 +8,232 @@ using Xunit;
 
 namespace Roslynator.CSharp.Analysis.Tests
 {
-    public class RCS1255UseEnumFieldExplicitlyTests : AbstractCSharpDiagnosticVerifier<UseEnumFieldExplicitlyAnalyzer, CastExpressionCodeFixProvider>
+    public class RCS1254NormalizeFormatOfEnumFlagValueTests : AbstractCSharpDiagnosticVerifier<NormalizeFormatOfEnumFlagValueAnalyzer, EnumMemberDeclarationCodeFixProvider>
     {
-        public override DiagnosticDescriptor Descriptor { get; } = DiagnosticRules.UseEnumFieldExplicitly;
+        public override DiagnosticDescriptor Descriptor { get; } = DiagnosticRules.NormalizeFormatOfEnumFlagValue;
 
-        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseEnumFieldExplicitly)]
-        public async Task Test()
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.NormalizeFormatOfEnumFlagValue)]
+        public async Task Test_DecimalToShift()
         {
             await VerifyDiagnosticAndFixAsync(@"
-using System.Text.RegularExpressions;
+using System;
 
-class C
+[Flags]
+enum Foo
 {
-    void M()
-    {
-        var x = [|(RegexOptions)1|];
-    }
+    _ = 0,
+    A = 1,
+    B = [|2|],
+    AB = 3,
+    C = ([|4|]),
 }
 ", @"
-using System.Text.RegularExpressions;
+using System;
 
-class C
+[Flags]
+enum Foo
 {
-    void M()
-    {
-        var x = RegexOptions.IgnoreCase;
-    }
+    _ = 0,
+    A = 1,
+    B = 1 << 1,
+    AB = 3,
+    C = (1 << 2),
+}
+", options: Options.AddConfigOption(ConfigOptionKeys.EnumFlagValueStyle, ConfigOptionValues.EnumFlagValueStyle_ShiftOperator));
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.NormalizeFormatOfEnumFlagValue)]
+        public async Task Test_ShiftToDecimal()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System;
+
+[Flags]
+enum Foo
+{
+    _ = 0,
+    A = 1,
+    B = [|1 << 1|],
+    AB = 3,
+    C = ([|1 << 2|]),
+}
+", @"
+using System;
+
+[Flags]
+enum Foo
+{
+    _ = 0,
+    A = 1,
+    B = 2,
+    AB = 3,
+    C = (4),
+}
+", options: Options.AddConfigOption(ConfigOptionKeys.EnumFlagValueStyle, ConfigOptionValues.EnumFlagValueStyle_DecimalNumber));
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.NormalizeFormatOfEnumFlagValue)]
+        public async Task Test_HexadecimalToShift()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System;
+
+[Flags]
+enum Foo
+{
+    _ = 0,
+    A = 1,
+    B = [|0x2|],
+    C = [|0X4|],
+}
+", @"
+using System;
+
+[Flags]
+enum Foo
+{
+    _ = 0,
+    A = 1,
+    B = 1 << 1,
+    C = 1 << 2,
+}
+", options: Options.AddConfigOption(ConfigOptionKeys.EnumFlagValueStyle, ConfigOptionValues.EnumFlagValueStyle_ShiftOperator));
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.NormalizeFormatOfEnumFlagValue)]
+        public async Task Test_HexadecimalToDecimal()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System;
+
+[Flags]
+enum Foo
+{
+    _ = 0,
+    A = 1,
+    B = [|0x2|],
+    C = [|0X4|],
+}
+", @"
+using System;
+
+[Flags]
+enum Foo
+{
+    _ = 0,
+    A = 1,
+    B = 2,
+    C = 4,
+}
+", options: Options.AddConfigOption(ConfigOptionKeys.EnumFlagValueStyle, ConfigOptionValues.EnumFlagValueStyle_DecimalNumber));
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.NormalizeFormatOfEnumFlagValue)]
+        public async Task Test_BinaryToShift()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System;
+
+[Flags]
+enum Foo
+{
+    _ = 0,
+    A = 1,
+    B = [|0b10|],
+    C = [|0B100|],
+}
+", @"
+using System;
+
+[Flags]
+enum Foo
+{
+    _ = 0,
+    A = 1,
+    B = 1 << 1,
+    C = 1 << 2,
+}
+", options: Options.AddConfigOption(ConfigOptionKeys.EnumFlagValueStyle, ConfigOptionValues.EnumFlagValueStyle_ShiftOperator));
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.NormalizeFormatOfEnumFlagValue)]
+        public async Task Test_BinaryToDecimal()
+        {
+            await VerifyDiagnosticAndFixAsync(@"
+using System;
+
+[Flags]
+enum Foo
+{
+    _ = 0,
+    A = 1,
+    B = [|0b10|],
+    C = [|0B100|],
+}
+", @"
+using System;
+
+[Flags]
+enum Foo
+{
+    _ = 0,
+    A = 1,
+    B = 2,
+    C = 4,
+}
+", options: Options.AddConfigOption(ConfigOptionKeys.EnumFlagValueStyle, ConfigOptionValues.EnumFlagValueStyle_DecimalNumber));
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.NormalizeFormatOfEnumFlagValue)]
+        public async Task TestNoDiagnostic_WithoutFlags()
+        {
+            await VerifyNoDiagnosticAsync(@"
+enum Foo
+{
+    _ = 0,
+    A = 1,
+    B = 2,
+    C = 4,
+}
+", options: Options.AddConfigOption(ConfigOptionKeys.EnumFlagValueStyle, ConfigOptionValues.EnumFlagValueStyle_ShiftOperator));
+        }
+
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.NormalizeFormatOfEnumFlagValue)]
+        public async Task TestNoDiagnostic_BitShift()
+        {
+            await VerifyNoDiagnosticAsync(@"
+using System;
+
+[Flags]
+enum Foo
+{
+    _ = 0,
+    A = 1,
+    B = 1 << 1,
 }
 ");
         }
 
-        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.UseEnumFieldExplicitly)]
-        public async Task Test_Flags()
+        [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.NormalizeFormatOfEnumFlagValue)]
+        public async Task TestNoDiagnostic_CombinedValue()
         {
-            await VerifyDiagnosticAndFixAsync(@"
-using System.Text.RegularExpressions;
+            await VerifyNoDiagnosticAsync(@"
+using System;
 
-class C
+[Flags]
+enum Foo
 {
-    void M()
-    {
-        var x = [|(RegexOptions)7|];
-    }
+    _ = 0,
+    A = 1,
+    B = 1 << 1,
+    C = 1 << 2,
+    D = 1 << 3,
+    AB = 3,
+    AC = 1 | 4,
+    AD = 0b1001,
+    X = int.MaxValue,
 }
-", @"
-using System.Text.RegularExpressions;
-
-class C
-{
-    void M()
-    {
-        var x = RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.ExplicitCapture;
-    }
-}
-");
+", options: Options.AddConfigOption(ConfigOptionKeys.EnumFlagValueStyle, ConfigOptionValues.EnumFlagValueStyle_ShiftOperator));
         }
     }
 }
