5bc8295ee880a1ea8c585b1d77ddb749c2f15733
Josef Pihrt <josef.pihrt@outlook.com>
2022-11-19 20:19:18 +0100

    Add missing usings

--- src/CommandLine/Options/ListVisualStudioCommandLineOptions.cs
+++ src/CommandLine/Options/ListVisualStudioCommandLineOptions.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Josef Pihrt and Contributors. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using CommandLine;
+
 namespace Roslynator.CommandLine;
 
 #if DEBUG
--- src/CommandLine/Program.cs
+++ src/CommandLine/Program.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using CommandLine;
 using CommandLine.Text;
