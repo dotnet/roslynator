f951aad6a80b5fa1cbd1465049c08ee04e031722
Josef Pihrt <josef.pihrt@outlook.com>
2023-04-15 14:27:39 +0200

    update

--- tools/cli_fix.ps1
+++ tools/cli_fix.ps1
@@ -21,5 +21,4 @@ roslynator fix "..\src\Roslynator.sln" `
     --verbosity d `
     --file-log "roslynator.log" `
     --file-log-verbosity diag `
-    --diagnostic-fix-map "RCS1155=Roslynator.RCS1155.OrdinalIgnoreCase" `
-    --file-banner " Copyright (c) Josef Pihrt and Contributors. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information."
+    --diagnostic-fix-map "RCS1155=Roslynator.RCS1155.OrdinalIgnoreCase"
