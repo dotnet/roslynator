0588776a772f778106ef4c0539d1bed7fac1725a
Josef Pihrt <josef.pihrt@outlook.com>
2023-07-18 01:51:19 +0200

    update

--- tools/generate_metadata.ps1
+++ tools/generate_metadata.ps1
@@ -1,4 +1,4 @@
 dotnet restore "../src/Tools/Tools.sln" --force
 dotnet build "../src/Tools/Tools.sln" --no-restore /p:"Configuration=Release,Deterministic=true,TreatWarningsAsErrors=true,WarningsNotAsErrors=1591" /m
 
-& "../src/Tools/MetadataGenerator/bin/Release/net7.0/Roslynator.MetadataGenerator" "../src" "../docs2"
+& "../src/Tools/MetadataGenerator/bin/Release/net7.0/Roslynator.MetadataGenerator" "../src" "../docs"
