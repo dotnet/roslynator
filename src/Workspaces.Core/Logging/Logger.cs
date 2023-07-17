49cd9ab7ca1f78810e3f290106d8b8c454f34ce2
Josef Pihrt <josef.pihrt@outlook.com>
2023-07-17 16:49:02 +0200

    update

--- src/Workspaces.Core/Logging/Logger.cs
+++ src/Workspaces.Core/Logging/Logger.cs
@@ -275,20 +275,18 @@ internal static class Logger
     {
         var colors = new ConsoleColors(color);
 
-        WriteLine(exception.Message, colors, verbosity);
+        WriteLine(exception.ToString(), colors, verbosity);
 
         if (exception is AggregateException aggregateException)
             WriteInnerExceptions(aggregateException, "");
-#if DEBUG
-        WriteLine(exception.ToString());
-#endif
+
         void WriteInnerExceptions(AggregateException aggregateException, string indent)
         {
             indent += "  ";
 
             foreach (Exception innerException in aggregateException.InnerExceptions)
             {
-                WriteLine(indent + "Inner exception: " + innerException.Message, colors, verbosity);
+                WriteLine(indent + "Inner exception: " + innerException.ToString(), colors, verbosity);
 
                 if (innerException is AggregateException aggregateException2)
                     WriteInnerExceptions(aggregateException2, indent);
--- tools/generate_ref_docs.ps1
+++ tools/generate_ref_docs.ps1
@@ -1,8 +1,8 @@
-$roslynatorExe="../src/CommandLine/bin/Release/net7.0/Roslynator"
-$rootDirectoryUrl="../../docs/api/"
-
 dotnet restore "../src/CommandLine.sln" -v minimal /m
 dotnet build "../src/CommandLine.sln" --no-restore -c Release -v minimal /m
+
+$roslynatorExe="../src/CommandLine/bin/Release/net7.0/Roslynator"
+# $rootDirectoryUrl="../../docs/api/"
 
 & $roslynatorExe generate-doc generate_ref_docs.sln `
  --properties Configuration=Release `
@@ -13,49 +13,49 @@ dotnet build "../src/CommandLine.sln" --no-restore -c Release -v minimal /m
  --ignored-common-parts content `
  --max-derived-types 10
 
-& $roslynatorExe list-symbols generate_ref_docs.sln `
- --properties Configuration=Release `
- --visibility public `
- --depth member `
- --ignored-parts containing-namespace assembly-attributes `
- --output "../docs/api.txt"
+# & $roslynatorExe list-symbols generate_ref_docs.sln `
+#  --properties Configuration=Release `
+#  --visibility public `
+#  --depth member `
+#  --ignored-parts containing-namespace assembly-attributes `
+#  --output "../docs/api.txt"
 
-& $roslynatorExe generate-doc-root generate_ref_docs.sln `
- --properties Configuration=Release `
- --projects Core `
- -o "../src/Core/README.md" `
- --host docusaurus `
- --heading "Roslynator.Core" `
- --root-directory-url $rootDirectoryUrl
+# & $roslynatorExe generate-doc-root generate_ref_docs.sln `
+#  --properties Configuration=Release `
+#  --projects Core `
+#  -o "../src/Core/README.md" `
+#  --host docusaurus `
+#  --heading "Roslynator.Core" `
+#  --root-directory-url $rootDirectoryUrl
 
-& $roslynatorExe generate-doc-root generate_ref_docs.sln `
- --properties Configuration=Release `
- --projects CSharp `
- -o "../src/CSharp/README.md" `
- --host docusaurus `
- --heading "Roslynator.CSharp" `
- --root-directory-url $rootDirectoryUrl
+# & $roslynatorExe generate-doc-root generate_ref_docs.sln `
+#  --properties Configuration=Release `
+#  --projects CSharp `
+#  -o "../src/CSharp/README.md" `
+#  --host docusaurus `
+#  --heading "Roslynator.CSharp" `
+#  --root-directory-url $rootDirectoryUrl
 
-& $roslynatorExe generate-doc-root generate_ref_docs.sln `
- --properties Configuration=Release `
- --projects Workspaces.Core `
- -o "../src/Workspaces.Core/README.md" `
- --host docusaurus `
- --heading "Roslynator.CSharp.Workspaces" `
- --root-directory-url $rootDirectoryUrl
+# & $roslynatorExe generate-doc-root generate_ref_docs.sln `
+#  --properties Configuration=Release `
+#  --projects Workspaces.Core `
+#  -o "../src/Workspaces.Core/README.md" `
+#  --host docusaurus `
+#  --heading "Roslynator.CSharp.Workspaces" `
+#  --root-directory-url $rootDirectoryUrl
 
-& $roslynatorExe generate-doc-root generate_ref_docs.sln `
- --properties Configuration=Release `
- --projects CSharp.Workspaces `
- -o "../src/CSharp.Workspaces/README.md" `
- --host docusaurus `
- --heading "Roslynator.CSharp.Workspaces" `
- --root-directory-url $rootDirectoryUrl
+# & $roslynatorExe generate-doc-root generate_ref_docs.sln `
+#  --properties Configuration=Release `
+#  --projects CSharp.Workspaces `
+#  -o "../src/CSharp.Workspaces/README.md" `
+#  --host docusaurus `
+#  --heading "Roslynator.CSharp.Workspaces" `
+#  --root-directory-url $rootDirectoryUrl
 
-& $roslynatorExe generate-doc-root generate_ref_docs.sln `
- --properties Configuration=Release `
- --projects Testing.Common Testing.CSharp Testing.CSharp.Xunit Testing.CSharp.MSTest `
- -o "../src/Tests/README.md" `
- --host docusaurus `
- --heading "Roslynator Testing Framework" `
- --root-directory-url $rootDirectoryUrl
+# & $roslynatorExe generate-doc-root generate_ref_docs.sln `
+#  --properties Configuration=Release `
+#  --projects Testing.Common Testing.CSharp Testing.CSharp.Xunit Testing.CSharp.MSTest `
+#  -o "../src/Tests/README.md" `
+#  --host docusaurus `
+#  --heading "Roslynator Testing Framework" `
+#  --root-directory-url $rootDirectoryUrl
