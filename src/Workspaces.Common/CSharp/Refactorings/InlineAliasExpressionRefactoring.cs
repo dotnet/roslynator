f40287cf86920c64c9a80ce4bc51713f26cad474
James <jameshargreaves12@gmail.com>
2023-08-07 20:06:52 +0000

    fix and test

--- src/Workspaces.Common/CSharp/Refactorings/InlineAliasExpressionRefactoring.cs
+++ src/Workspaces.Common/CSharp/Refactorings/InlineAliasExpressionRefactoring.cs
@@ -68,6 +68,20 @@ internal static class InlineAliasExpressionRefactoring
 
         public CancellationToken CancellationToken { get; }
 
+        public override SyntaxNode VisitAliasQualifiedName(AliasQualifiedNameSyntax node)
+        {
+            IAliasSymbol aliasSymbol = SemanticModel.GetAliasInfo(node.Alias, CancellationToken);
+            if (SymbolEqualityComparer.Default.Equals(aliasSymbol, AliasSymbol) 
+                && Replacement is NameSyntax replacementName)
+            {
+                return SyntaxFactory.QualifiedName(replacementName, node.Name)
+                    .WithTriviaFrom(node)
+                    .WithSimplifierAnnotation();
+            }
+
+            return base.VisitAliasQualifiedName(node);
+        }
+
         public override SyntaxNode VisitIdentifierName(IdentifierNameSyntax node)
         {
             IAliasSymbol aliasSymbol = SemanticModel.GetAliasInfo(node, CancellationToken);
