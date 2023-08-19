710e5ff7e313d36619ffa9d23b7e28435a2e48f3
Josef Pihrt <josef.pihrt@outlook.com>
2023-08-19 21:54:31 +0200

    update

--- tools/generate_api_list.ps1
+++ tools/generate_api_list.ps1
@@ -1,9 +1,4 @@
-$roslynatorExe="../src/CommandLine/bin/Debug/net7.0/roslynator"
-
-dotnet restore "../src/CommandLine.sln" -v minimal /m
-dotnet build "../src/CommandLine.sln" --no-restore -c Debug -v minimal /m
-
-& $roslynatorExe list-symbols generate_ref_docs.sln `
+roslynator list-symbols generate_ref_docs.sln `
  --properties Configuration=Release `
  --visibility public `
  --depth member `
