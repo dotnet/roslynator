8952a0e4055df3c6ce4d487dc2feff61df3d89c6
Josef Pihrt <josef.pihrt@outlook.com>
2023-08-24 16:29:05 +0200

    Revert "update"
    This reverts commit 33de4053e74bb0663ab9de6665a643b4d9f45ada.

--- src/CommandLine/Options/MSBuildCommandLineOptions.cs
+++ src/CommandLine/Options/MSBuildCommandLineOptions.cs
@@ -11,13 +11,13 @@ public abstract class MSBuildCommandLineOptions : BaseCommandLineOptions
 {
     [Option(
         longName: "include",
-        HelpText = "Space separated listt of glob patterns to include files, folders or projects.",
+        HelpText = "Space separated list of glob patterns to include files/folders.",
         MetaValue = "<GLOB>")]
     public IEnumerable<string> Include { get; set; }
 
     [Option(
         longName: "exclude",
-        HelpText = "Space separated list of glob patterns to exclude files, folders or projects.",
+        HelpText = "Space separated list of glob patterns to exclude files/folders.",
         MetaValue = "<GLOB>")]
     public IEnumerable<string> Exclude { get; set; }
 
