a6fb6574356e7275952763121b10d2b6181bd12b
Josef Pihrt <josef.pihrt@outlook.com>
2023-08-26 18:59:15 +0200

    update

--- tools/generate_metadata.ps1
+++ tools/generate_metadata.ps1
@@ -1,5 +1,5 @@
 dotnet restore "../src/Tools/Tools.sln" --force
-dotnet build "../src/Tools/Tools.sln" --no-restore /p:"Configuration=Release,Deterministic=true,TreatWarningsAsErrors=true,WarningsNotAsErrors=1591" /m
+dotnet build "../src/Tools/Tools.sln" --no-restore /p:"Configuration=Release,Deterministic=true,TreatWarningsAsErrors=true,WarningsNotAsErrors=`"1591,RS1025,RS1026`"" /m
 
 if(!$?) { Read-Host; Exit }
 
