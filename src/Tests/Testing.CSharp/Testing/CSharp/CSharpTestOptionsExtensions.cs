d51d711dac118c44320a974a709eae59b369b2c5
Josef Pihrt <josef.pihrt@outlook.com>
2023-10-01 17:14:39 +0200

    update

--- src/Tests/Testing.CSharp/Testing/CSharp/CSharpTestOptionsExtensions.cs
+++ src/Tests/Testing.CSharp/Testing/CSharp/CSharpTestOptionsExtensions.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) .NET Foundation and Contributors. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Roslynator.Testing.CSharp;
 
