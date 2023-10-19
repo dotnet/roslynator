7bc89125ea3793993a9e47f89872daf212bebe57
Josef Pihrt <josef@pihrt.net>
2023-10-19 10:36:46 +0200

    Update script (#1228)

--- tools/generate_ref_docs.ps1
+++ tools/generate_ref_docs.ps1
@@ -1,12 +1,12 @@
+dotnet restore generate_ref_docs.sln -v minimal /m
+dotnet build generate_ref_docs.sln --no-restore -c Release -v minimal /m
+
 dotnet restore "../src/CommandLine.sln" -v minimal /m
 dotnet build "../src/CommandLine.sln" --no-restore -c Release -v minimal /m
 
-$roslynatorExe="../src/CommandLine/bin/Release/net7.0/Roslynator"
-$rootDirectoryUrl="build/ref"
-
-& $roslynatorExe generate-doc generate_ref_docs.sln `
+& "../src/CommandLine/bin/Release/net7.0/Roslynator" generate-doc generate_ref_docs.sln `
  --properties Configuration=Release `
- -o $rootDirectoryUrl `
+ -o "build/ref" `
  --host docusaurus `
  --heading "Roslynator .NET API Reference" `
  --group-by-common-namespace `
