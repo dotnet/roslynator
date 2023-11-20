695c81484e5049a1310661a7c5abe07db73b067a
jakubreznak <56341176+jakubreznak@users.noreply.github.com>
2023-11-21 00:19:42 +0100

    Added test for RCS1196 (#1235)

---
+++ src/Tests/Analyzers.Tests/RCS1196CallExtensionMethodAsInstanceMethodTests.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) .NET Foundation and Contributors. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Roslynator.CSharp.CodeFixes;
+using Roslynator.Testing.CSharp;
+using Xunit;
+
+namespace Roslynator.CSharp.Analysis.Tests;
+
+public class RCS1196CallExtensionMethodAsInstanceMethodTests : AbstractCSharpDiagnosticVerifier<CallExtensionMethodAsInstanceMethodAnalyzer, InvocationExpressionCodeFixProvider>
+{
+    public override DiagnosticDescriptor Descriptor { get; } = DiagnosticRules.CallExtensionMethodAsInstanceMethod;
+
+    [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.CallExtensionMethodAsInstanceMethod)]
+    public async Task Test()
+    {
+        await VerifyDiagnosticAndFixAsync(@"
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class C
+{
+    void M(List<int> items)
+    {
+        var x = [|Enumerable.Select(items, f => f.ToString())|];
+    }
+}
+", @"
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class C
+{
+    void M(List<int> items)
+    {
+        var x = items.Select(f => f.ToString());
+    }
+}
+");
+    }
+}
---
+++ src/Tests/Refactorings.Tests/RR0016CallExtensionMethodAsInstanceMethodTests.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) .NET Foundation and Contributors. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Roslynator.Testing.CSharp;
+using Xunit;
+
+namespace Roslynator.CSharp.Refactorings.Tests;
+
+public class RR0016CallExtensionMethodAsInstanceMethodTests : AbstractCSharpRefactoringVerifier
+{
+    public override string RefactoringId { get; } = RefactoringIdentifiers.CallExtensionMethodAsInstanceMethod;
+
+    [Fact, Trait(Traits.Refactoring, RefactoringIdentifiers.CallExtensionMethodAsInstanceMethod)]
+    public async Task Test()
+    {
+        await VerifyRefactoringAsync(@"
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class C
+{
+    void M(List<int> items)
+    {
+        var x = Enumerable.Select[||](items, f => f.ToString());
+    }
+}
+", @"
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class C
+{
+    void M(List<int> items)
+    {
+        var x = items.Select(f => f.ToString());
+    }
+}
+", equivalenceKey: EquivalenceKey.Create(RefactoringId));
+    }
+}
