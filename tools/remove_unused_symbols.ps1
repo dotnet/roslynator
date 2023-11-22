8ed8883a1cb358d3677894f34d04d8c6d58fdfb6
Josef Pihrt <josef.pihrt@outlook.com>
2023-11-23 00:50:12 +0100

    update

--- tools/remove_unused_symbols.ps1
+++ tools/remove_unused_symbols.ps1
@@ -1,4 +1,4 @@
 #dotnet tool install -g roslynator.dotnet.cli
 
-roslynator find-symbols "$PSScriptRoot/../src/Roslynator.sln" `
+roslynator find-symbol "$PSScriptRoot/../src/Roslynator.sln" `
  --symbol-kind type --unused --remove --exclude CommandLine/Orang/** --without-attribute System.ObsoleteAttribute
