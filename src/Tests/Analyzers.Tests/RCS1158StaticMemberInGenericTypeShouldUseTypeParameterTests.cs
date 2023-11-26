0bc6f1cc38dace92741cc62baf696eef61a952d8
Josef Pihrt <josef@pihrt.net>
2023-11-26 01:46:38 +0100

    Fix analyzer RCS1158 (#1288)

---
+++ src/Tests/Analyzers.Tests/RCS1158StaticMemberInGenericTypeShouldUseTypeParameterTests.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) .NET Foundation and Contributors. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Roslynator.Testing;
+using Roslynator.Testing.CSharp;
+using Xunit;
+
+namespace Roslynator.CSharp.Analysis.Tests;
+
+public class RCS1158StaticMemberInGenericTypeShouldUseTypeParameterTests : AbstractCSharpDiagnosticVerifier<StaticMemberInGenericTypeShouldUseTypeParameterAnalyzer, EmptyCodeFixProvider>
+{
+    public override DiagnosticDescriptor Descriptor { get; } = DiagnosticRules.StaticMemberInGenericTypeShouldUseTypeParameter;
+
+    [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.StaticMemberInGenericTypeShouldUseTypeParameter)]
+    public async Task TestNoDiagnostic_Property()
+    {
+        await VerifyNoDiagnosticAsync(@"
+public sealed class C<T, T2> where T : IFoo<T2>
+{
+    public static T2 Name { get; } = T.Name;
+}
+
+public interface IFoo<T>
+{
+    public static abstract T Name { get; }
+}");
+    }
+
+    [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.StaticMemberInGenericTypeShouldUseTypeParameter)]
+    public async Task TestNoDiagnostic_Field()
+    {
+        await VerifyNoDiagnosticAsync(@"
+public sealed class C<T> where T : IFoo
+{
+    public static string Name = typeof(T).Name;
+}
+
+public interface IFoo
+{
+    public static abstract string Name { get; }
+}");
+    }
+}
