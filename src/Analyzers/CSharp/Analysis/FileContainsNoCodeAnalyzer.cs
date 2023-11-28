5c97ccaf4c14713241f5b22c769432f60196c7d4
Josef Pihrt <josef.pihrt@outlook.com>
2023-11-28 21:09:05 +0100

    update

--- src/Analyzers/CSharp/Analysis/FileContainsNoCodeAnalyzer.cs
+++ src/Analyzers/CSharp/Analysis/FileContainsNoCodeAnalyzer.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright (c) .NET Foundation and Contributors. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Text;
-using System;
 
 namespace Roslynator.CSharp.Analysis;
 
