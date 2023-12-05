9257ad2a8d59b5df1fb85049c41706a8ae47a0e1
Josef Pihrt <josef.pihrt@outlook.com>
2023-12-05 16:04:16 +0100

    update

--- src/Common/CSharp/Extensions/CommonCSharpExtensions.cs
+++ src/Common/CSharp/Extensions/CommonCSharpExtensions.cs
@@ -1,23 +1,13 @@
 ﻿// Copyright (c) .NET Foundation and Contributors. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
 namespace Roslynator.CSharp;
 
 internal static class CommonCSharpExtensions
 {
-#pragma warning disable IDE0060, RCS1175
     public static bool SupportsCollectionExpression(this CSharpCompilation compilation)
-#pragma warning restore IDE0060, RCS1175
     {
-        Version version = typeof(SyntaxNode).Assembly.GetName().Version;
-
-        return version.Major > 4
-            || (version.Major == 4 && version.Minor >= 8);
-
-        //TODO: bump Roslyn to 4.8.0
-        //return compilation.LanguageVersion >= LanguageVersion.CSharp12;
+        return (int)compilation.LanguageVersion >= 1200;
     }
 }
