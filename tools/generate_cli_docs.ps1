60c753971579f9e7b47851ab52c41b1febca1dad
Josef Pihrt <josef.pihrt@outlook.com>
2023-12-05 14:45:52 +0100

    update

--- tools/generate_cli_docs.ps1
+++ tools/generate_cli_docs.ps1
@@ -1,7 +1,7 @@
 dotnet restore "$PSScriptRoot/../src/CommandLine.DocumentationGenerator/CommandLine.DocumentationGenerator.csproj" 
 
 dotnet build "$PSScriptRoot/../src/CommandLine.DocumentationGenerator/CommandLine.DocumentationGenerator.csproj" -c Release -v minimal `
- /p:Deterministic=true,TreatWarningsAsErrors=true,WarningsNotAsErrors="1591,RS1024,RS1025,RS1026" `
+ /p:Deterministic=true,TreatWarningsAsErrors=true,WarningsNotAsErrors="1573,1591,RS1024,RS1025,RS1026" `
 
  if(!$?) { Read-Host; Exit }
 
