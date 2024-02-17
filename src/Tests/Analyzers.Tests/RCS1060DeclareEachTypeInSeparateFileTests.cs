3f1a3662f4313b6c35ffae72399bbfbaf3d37ba3
Josef Pihrt <josef@pihrt.net>
2024-02-17 19:24:06 +0100

    Fix RCS1060 (#1401)

---
+++ src/Tests/Analyzers.Tests/RCS1060DeclareEachTypeInSeparateFileTests.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) .NET Foundation and Contributors. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Roslynator.CSharp.CodeFixes;
+using Roslynator.Testing.CSharp;
+using Xunit;
+
+namespace Roslynator.CSharp.Analysis.Tests;
+
+public class RCS1060DeclareEachTypeInSeparateFileTests : AbstractCSharpDiagnosticVerifier<DeclareEachTypeInSeparateFileAnalyzer, ExtractMemberToNewDocumentCodeFixProvider>
+{
+    public override DiagnosticDescriptor Descriptor => DiagnosticRules.DeclareEachTypeInSeparateFile;
+
+    [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.DeclareEachTypeInSeparateFile)]
+    public async Task Test_Namespace()
+    {
+        await VerifyDiagnosticAndFixAsync("""
+namespace N
+{
+    public class [|C1|]
+    {
+    }
+
+    public class [|C2|]
+    {
+    }
+}
+""", """
+namespace N
+{
+    public class C2
+    {
+    }
+}
+""");
+    }
+
+    [Fact, Trait(Traits.Analyzer, DiagnosticIdentifiers.DeclareEachTypeInSeparateFile)]
+    public async Task Test_FileScopedNamespace()
+    {
+        await VerifyDiagnosticAndFixAsync("""
+namespace N;
+
+public class [|C1|]
+{
+}
+
+public class [|C2|]
+{
+}
+""", """
+namespace N;
+
+public class C2
+{
+}
+""");
+    }
+}
