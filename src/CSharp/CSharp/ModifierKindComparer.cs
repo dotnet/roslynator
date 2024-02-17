02b455c0c7db759e1ce659d8abbba393cd981451
Josef Pihrt <josef.pihrt@outlook.com>
2024-02-17 15:55:46 +0100

    update

--- src/CSharp/CSharp/ModifierKindComparer.cs
+++ src/CSharp/CSharp/ModifierKindComparer.cs
@@ -22,47 +22,49 @@ internal class ModifierKindComparer : IComparer<SyntaxKind>
             case SyntaxKind.NewKeyword:
                 return 0;
             case SyntaxKind.PublicKeyword:
-                return 1;
-            case SyntaxKind.PrivateKeyword:
-                return 2;
-            case SyntaxKind.ProtectedKeyword:
-                return 3;
-            case SyntaxKind.InternalKeyword:
-                return 4;
-            case SyntaxKind.ConstKeyword:
-                return 5;
-            case SyntaxKind.StaticKeyword:
-                return 6;
-            case SyntaxKind.VirtualKeyword:
-                return 7;
-            case SyntaxKind.SealedKeyword:
-                return 8;
-            case SyntaxKind.OverrideKeyword:
-                return 9;
-            case SyntaxKind.AbstractKeyword:
                 return 10;
-            case SyntaxKind.ReadOnlyKeyword:
-                return 11;
-            case SyntaxKind.ExternKeyword:
-                return 12;
-            case SyntaxKind.UnsafeKeyword:
-                return 13;
-            case SyntaxKind.FixedKeyword:
-                return 14;
-            case SyntaxKind.VolatileKeyword:
-                return 15;
-            case SyntaxKind.AsyncKeyword:
-                return 16;
-            case SyntaxKind.PartialKeyword:
-                return 17;
-            case SyntaxKind.ThisKeyword:
-                return 18;
-            case SyntaxKind.RefKeyword:
-                return 19;
-            case SyntaxKind.OutKeyword:
+            case SyntaxKind.PrivateKeyword:
                 return 20;
+            case SyntaxKind.ProtectedKeyword:
+                return 30;
+            case SyntaxKind.InternalKeyword:
+                return 40;
+            case SyntaxKind.FileKeyword:
+                return 50;
+            case SyntaxKind.ConstKeyword:
+                return 60;
+            case SyntaxKind.StaticKeyword:
+                return 70;
+            case SyntaxKind.VirtualKeyword:
+                return 80;
+            case SyntaxKind.SealedKeyword:
+                return 90;
+            case SyntaxKind.OverrideKeyword:
+                return 100;
+            case SyntaxKind.AbstractKeyword:
+                return 110;
+            case SyntaxKind.ReadOnlyKeyword:
+                return 120;
+            case SyntaxKind.ExternKeyword:
+                return 130;
+            case SyntaxKind.UnsafeKeyword:
+                return 140;
+            case SyntaxKind.FixedKeyword:
+                return 150;
+            case SyntaxKind.VolatileKeyword:
+                return 160;
+            case SyntaxKind.AsyncKeyword:
+                return 170;
+            case SyntaxKind.PartialKeyword:
+                return 180;
+            case SyntaxKind.ThisKeyword:
+                return 190;
+            case SyntaxKind.RefKeyword:
+                return 200;
+            case SyntaxKind.OutKeyword:
+                return 210;
             case SyntaxKind.InKeyword:
-                return 21;
+                return 220;
             default:
                 {
                     Debug.Fail($"unknown modifier '{kind}'");
