a1dc23cad3cd7d80b074f922a2bbb6210b99dd03
Benjamin Brienen <Benjamin.Brienen@outlook.com>
2024-09-15 00:10:51 +0200

    remove unused using

--- src/Tests/Analyzers.Tests/RCS1077OptimizeLinqMethodCallTests.cs
+++ src/Tests/Analyzers.Tests/RCS1077OptimizeLinqMethodCallTests.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) .NET Foundation and Contributors. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Roslynator.CSharp.CodeFixes;
