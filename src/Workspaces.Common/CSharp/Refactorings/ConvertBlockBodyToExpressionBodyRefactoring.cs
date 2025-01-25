abd2c8a53770bd6cf7b68257055ff9bbcd4fe6d7
Josef Pihrt <josef@pihrt.net>
2025-01-25 14:02:30 +0100

    Update src/Workspaces.Common/CSharp/Refactorings/ConvertBlockBodyToExpressionBodyRefactoring.cs

--- src/Workspaces.Common/CSharp/Refactorings/ConvertBlockBodyToExpressionBodyRefactoring.cs
+++ src/Workspaces.Common/CSharp/Refactorings/ConvertBlockBodyToExpressionBodyRefactoring.cs
@@ -193,7 +193,7 @@ internal static class ConvertBlockBodyToExpressionBodyRefactoring
 
         if (newLinePosition == NewLinePosition.After)
         {
-            var arrowToken = CSharpUtility.GetExpressionBody(newNode).ArrowToken;
+            ArrowExpressionClauseSyntax arrowToken = CSharpUtility.GetExpressionBody(newNode).ArrowToken;
             var annotation = new SyntaxAnnotation();
             newNode = newNode.ReplaceToken(arrowToken, arrowToken.WithAdditionalAnnotations(annotation));
             document = await document.ReplaceNodeAsync(node, newNode, cancellationToken).ConfigureAwait(false);
