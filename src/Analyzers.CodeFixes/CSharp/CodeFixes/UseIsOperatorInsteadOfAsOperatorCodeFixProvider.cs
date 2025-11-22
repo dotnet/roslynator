24d08d1389babb47e287e96fc485dbef8fdd57e1
Josef Pihrt <josef@pihrt.net>
2025-11-22 20:36:23 +0100

    Add tests for RCS1172 (#1708)

--- src/Analyzers.CodeFixes/CSharp/CodeFixes/UseIsOperatorInsteadOfAsOperatorCodeFixProvider.cs
+++ src/Analyzers.CodeFixes/CSharp/CodeFixes/UseIsOperatorInsteadOfAsOperatorCodeFixProvider.cs
@@ -2,13 +2,17 @@
 
 using System.Collections.Immutable;
 using System.Composition;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Roslynator.CodeFixes;
 using Roslynator.CSharp.Refactorings;
+using Roslynator.CSharp.Syntax;
+using static Roslynator.CSharp.CSharpFactory;
 
 namespace Roslynator.CSharp.CodeFixes;
 
@@ -38,9 +42,33 @@ public sealed class UseIsOperatorInsteadOfAsOperatorCodeFixProvider : BaseCodeFi
 
         CodeAction codeAction = CodeAction.Create(
             "Use 'is' operator",
-            ct => UseIsOperatorInsteadOfAsOperatorRefactoring.RefactorAsync(context.Document, node, ct),
+            ct => RefactorAsync(context.Document, node, ct),
             GetEquivalenceKey(diagnostic));
 
         context.RegisterCodeFix(codeAction, diagnostic);
+    }
+
+    private static Task<Document> RefactorAsync(
+        Document document,
+        SyntaxNode node,
+        CancellationToken cancellationToken)
+    {
+        NullCheckExpressionInfo nullCheck = SyntaxInfo.NullCheckExpressionInfo(node);
+
+        AsExpressionInfo asExpressionInfo = SyntaxInfo.AsExpressionInfo(nullCheck.Expression);
+
+        ExpressionSyntax newNode = IsExpression(
+            asExpressionInfo.Expression,
+            SyntaxFactory.Token(asExpressionInfo.OperatorToken.LeadingTrivia, SyntaxKind.IsKeyword, asExpressionInfo.OperatorToken.TrailingTrivia),
+            asExpressionInfo.Type);
+
+        if (nullCheck.IsCheckingNull)
+            newNode = LogicalNotExpression(newNode.WithoutTrivia().Parenthesize()).WithTriviaFrom(newNode);
+
+        newNode = newNode
+            .Parenthesize()
+            .WithFormatterAnnotation();
+
+        return document.ReplaceNodeAsync(node, newNode, cancellationToken);
     }
 }
